c6c95b324b8c0b9608b262682c354a0d794ca7fe
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-07-28 00:22:46 +0400

    Initial

---
+++ packages/common-angular/src/typeorm-placeholder.ts
@@ -0,0 +1,14 @@
+export function Entity(options?: any): Function
+{
+	return () => {};
+}
+
+export function Column(options?: any): Function
+{
+	return () => {};
+}
+
+export function PrimaryColumn(options?: any): Function
+{
+	return () => {};
+}
