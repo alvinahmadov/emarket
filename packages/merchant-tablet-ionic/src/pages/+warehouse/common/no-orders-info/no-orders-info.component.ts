df6bc0c419f14ca626916356ba88a531ee78b63d
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-07-28 00:23:22 +0400

    Initial

---
+++ packages/merchant-tablet-ionic/src/pages/+warehouse/common/no-orders-info/no-orders-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+	           selector: 'no-orders-info',
+	           templateUrl: 'no-orders-info.component.html',
+	           styleUrls: ['./no-orders-info.component.scss'],
+           })
+export class NoOrdersInfoComponent
+{
+	@Input()
+	filterMode: string;
+	
+	@Output()
+	toggleOrderContainer: EventEmitter<boolean> = new EventEmitter();
+}
