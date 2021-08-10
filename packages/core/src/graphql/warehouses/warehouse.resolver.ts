e505292158ba02113a61038ccef5506b26e0bb18
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-08-10 08:00:22 +0400

    Revert "Reformat"
    This reverts commit 7176d537b959e0f3ff328a61fa41f58a66e2b556.

--- packages/core/src/graphql/warehouses/warehouse.resolver.ts
+++ packages/core/src/graphql/warehouses/warehouse.resolver.ts
@@ -1,15 +1,6 @@
 import { Mutation, Query, ResolveProperty, Resolver } from '@nestjs/graphql';
-import { first }                                      from 'rxjs/operators';
-import IGeoLocation, {
-	IGeoLocationCreateObject,
-	ILocation
-}                                                     from '@modules/server.common/interfaces/IGeoLocation';
 import { default as IWarehouse }                      from '@modules/server.common/interfaces/IWarehouse';
-import User                                           from '@modules/server.common/entities/User';
-import GeoLocation                                    from '@modules/server.common/entities/GeoLocation';
 import Warehouse                                      from '@modules/server.common/entities/Warehouse';
-import { IWarehouseRegistrationInput }                from '@modules/server.common/routers/IWarehouseRouter';
-import Utils                                          from '@modules/server.common/utils';
 import {
 	WarehousesCarriersService,
 	WarehousesOrdersService,
@@ -18,9 +9,18 @@ import {
 	WarehousesProductsService
 }                                                     from '../../services/warehouses';
 import { DevicesService }                             from '../../services/devices';
-import { GeoLocationsWarehousesService }              from '../../services/geo-locations';
-import { UsersService }                               from '../../services/users';
 import { OrdersService }                              from '../../services/orders';
+import { UsersService }                               from '../../services/users';
+import { GeoLocationsWarehousesService }              from '../../services/geo-locations';
+import { first }                                      from 'rxjs/operators';
+import { IWarehouseRegistrationInput }                from '@modules/server.common/routers/IWarehouseRouter';
+import IGeoLocation, {
+	IGeoLocationCreateObject,
+	ILocation
+}                                                     from '@modules/server.common/interfaces/IGeoLocation';
+import User                                           from '@modules/server.common/entities/User';
+import Utils                                          from '@modules/server.common/utils';
+import GeoLocation                                    from '@modules/server.common/entities/GeoLocation';
 
 @Resolver('Warehouse')
 export class WarehouseResolver
@@ -133,22 +133,18 @@ export class WarehouseResolver
 		start.setHours(0, 0, 0, 0);
 		end.setHours(23, 59, 59, 999);
 		
-		const users = await this._ordersService
-		                        .Model
-		                        .find({
-			                              ...isDeletedFlag,
-			                              _createdAt: { $gte: start, $lt: end }
-		                              })
+		const users = await this._ordersService.Model.find({
+			                                                   ...isDeletedFlag,
+			                                                   _createdAt: { $gte: start, $lt: end }
+		                                                   })
 		                        .distinct('user._id')
 		                        .lean()
 		                        .exec();
 		
-		const storesIds: string[] = await this._ordersService
-		                                      .Model
-		                                      .find({
-			                                            ...isDeletedFlag,
-			                                            _createdAt: { $gte: start, $lt: end }
-		                                            })
+		const storesIds: string[] = await this._ordersService.Model.find({
+			                                                                 ...isDeletedFlag,
+			                                                                 _createdAt: { $gte: start, $lt: end }
+		                                                                 })
 		                                      .distinct('warehouse')
 		                                      .lean()
 		                                      .exec();
@@ -157,17 +153,13 @@ export class WarehouseResolver
 			total: users.length,
 			perStore: storesIds.map(async(storeId) =>
 			                        {
-				                        const usersIds: string[] =
-						                        await this._ordersService
-						                                  .Model
-						                                  .find(
-								                                  {
-									                                  ...isDeletedFlag,
-									                                  'user._id':
-											                                  { $in: users.map((u) => u._id) },
-									                                  warehouse: storeId
-								                                  }
-						                                  ).distinct('user._id');
+				                        const usersIds: string[] = await this._ordersService.Model.find(
+						                        {
+							                        ...isDeletedFlag,
+							                        'user._id': { $in: users.map((u) => u._id) },
+							                        warehouse: storeId
+						                        }
+				                        ).distinct('user._id');
 				                        return {
 					                        storeId,
 					                        customersCount: usersIds.length
@@ -189,10 +181,7 @@ export class WarehouseResolver
 	@Query('warehouse')
 	async getWarehouse(_, { id }: { id: string })
 	{
-		return this._warehousesService
-		           .get(id)
-		           .pipe(first())
-		           .toPromise();
+		return this._warehousesService.get(id).pipe(first()).toPromise();
 	}
 	
 	@Query()
@@ -229,9 +218,7 @@ export class WarehouseResolver
 	@Query()
 	async getCountOfMerchants()
 	{
-		return this._warehousesService
-		           .Model
-		           .find({ isDeleted: { $eq: false } })
+		return this._warehousesService.Model.find({ isDeleted: { $eq: false } })
 		           .countDocuments()
 		           .exec();
 	}
@@ -245,11 +232,9 @@ export class WarehouseResolver
 			}: { searchName: string; geoLocation: IGeoLocation }
 	)
 	{
-		const count = await this._warehousesService
-		                        .Model
-		                        .find({
-			                              name: { $regex: searchName, $options: 'i' }
-		                              })
+		const count = await this._warehousesService.Model.find({
+			                                                       name: { $regex: searchName, $options: 'i' }
+		                                                       })
 		                        .countDocuments()
 		                        .exec();
 		
@@ -277,15 +262,6 @@ export class WarehouseResolver
 	}
 	
 	@Mutation()
-	async registerWarehouse(
-			_,
-			{ registerInput }: { registerInput: IWarehouseRegistrationInput }
-	)
-	{
-		return this._warehousesService.register(registerInput);
-	}
-	
-	@Mutation()
 	async warehouseLogin(
 			_,
 			{ username, password }: { username: string; password: string }
@@ -307,6 +283,15 @@ export class WarehouseResolver
 	)
 	{
 		return this._warehousesService.updateGeoLocation(storeId, geoLocation);
+	}
+	
+	@Mutation()
+	async registerWarehouse(
+			_,
+			{ registerInput }: { registerInput: IWarehouseRegistrationInput }
+	)
+	{
+		return this._warehousesService.register(registerInput);
 	}
 	
 	@Mutation()
