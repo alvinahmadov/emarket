f262fb2b2820c856b75e575fa10ac04d1f236bca
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-08-16 08:53:29 +0400

    Refactor: Move graphql queries and mutations to separate file to make their edition easy

--- packages/merchant-tablet-ionic/src/services/products-category.service.ts
+++ packages/merchant-tablet-ionic/src/services/products-category.service.ts
@@ -1,35 +1,27 @@
 import { Injectable }   from '@angular/core';
 import { Apollo }       from 'apollo-angular';
-import gql              from 'graphql-tag';
 import { Observable }   from 'rxjs';
 import { map, share }   from 'rxjs/operators';
 import ProductsCategory from '@modules/server.common/entities/ProductsCategory';
+import { GQLQueries }   from '@modules/server.common/utilities/graphql';
 
 @Injectable()
 export class ProductsCategoryService
 {
 	constructor(private readonly apollo: Apollo) {}
-
-    getCategories(): Observable<ProductsCategory[]>
-    {
-        return this.apollo
-		.watchQuery<{ productsCategories: ProductsCategory[] }>({
-                                                                    query: gql`
-                                                                        query allCategories {
-                                                                            productsCategories {
-                                                                                id
-                                                                                name {
-                                                                                    locale
-                                                                                    value
-                                                                                }
-                                                                            }
-                                                                        }
-			                                                        `,
-			                                                        pollInterval: 1000,
-                                                                })
-		.valueChanges.pipe(
-				map((res) => res.data.productsCategories),
-				share()
-		);
-    }
+	
+	getCategories(): Observable<ProductsCategory[]>
+	{
+		return this.apollo
+		           .watchQuery<{
+			           productsCategories: ProductsCategory[]
+		           }>({
+			              query: GQLQueries.ProductCategoryAll,
+			              pollInterval: 1000,
+		              })
+		           .valueChanges.pipe(
+						map((res) => res.data.productsCategories),
+						share()
+				);
+	}
 }
--- packages/merchant-tablet-ionic/src/services/promotion.service.ts
+++ packages/merchant-tablet-ionic/src/services/promotion.service.ts
@@ -1,108 +1,69 @@
-import { Injectable }             from '@angular/core';
-import { Apollo }                 from 'apollo-angular';
-import { IPromotionCreateObject } from '@modules/server.common/interfaces/IPromotion';
-import gql                        from 'graphql-tag';
-import { map, share }             from 'rxjs/operators';
-import { Observable }             from 'rxjs';
+import { Injectable }               from '@angular/core';
+import { Apollo }                   from 'apollo-angular';
+import { IPromotionCreateObject }   from '@modules/server.common/interfaces/IPromotion';
+import { GQLMutations, GQLQueries } from '@modules/server.common/utilities/graphql';
+import { map, share }               from 'rxjs/operators';
+import { Observable }               from 'rxjs';
 
 @Injectable()
 export class PromotionService
 {
 	constructor(private readonly apollo: Apollo) {}
-
-    getAll(findInput: { warehouse: string }): Observable<any>
-    {
-        return this.apollo
-		.query<any>({
-                        query: gql`
-                            query allPromotions($findInput: PromotionsFindInput) {
-                                promotions(findInput: $findInput) {
-                                    _id
-                                    title {
-                                        locale
-                                        value
-                                    }
-                                    description {
-                                        locale
-                                        value
-                                    }
-                                    active
-                                    activeFrom
-                                    activeTo
-                                    image
-                                    promoPrice
-                                    purchasesCount
-                                    warehouseId
-                                    productId
-                                }
-                            }
-			            `,
-			            variables: { findInput },
-                    })
-		.pipe(
-				map((result) => result.data || []),
-				share()
-		);
-    }
-
-    create(promotion: IPromotionCreateObject)
-    {
-        return this.apollo
-		.mutate<{ promotion: IPromotionCreateObject }>({
-                                                           mutation: gql`
-                                                               mutation CreatePromotion($promotion: PromotionInput) {
-                                                                   createPromotion(createInput: $promotion) {
-                                                                       _id
-                                                                   }
-                                                               }
-			                                               `,
-			                                               variables: {
-				                                               promotion,
-			                                               },
-                                                       })
-		.pipe(
-				map((result) => result.data),
-				share()
-		);
-    }
-
-    update(id: String, promotion: IPromotionCreateObject)
-    {
-        return this.apollo
-		.mutate<{ promotion: IPromotionCreateObject }>({
-                                                           mutation: gql`
-                                                               mutation UpdatePromotion(
-                                                                   $id: String
-                                                                   $promotion: PromotionInput
-                                                               ) {
-                                                                   updatePromotion(id: $id, updateInput: $promotion) {
-                                                                       _id
-                                                                   }
-                                                               }
-			                                               `,
-			                                               variables: {
-				                                               id,
-				                                               promotion,
-			                                               },
-                                                       })
-		.pipe(
-				map((result) => result.data),
-				share()
-		);
-    }
-
-    removeByIds(ids: string[])
-    {
-        return this.apollo.mutate({
-                                      mutation: gql`
-                                          mutation RemoveByIds($ids: [String!]!) {
-                                              removePromotionsByIds(ids: $ids) {
-                                                  ok
-                                                  n
-                                              }
-                                          }
-			                          `,
-			                          variables: { ids },
-                                  });
-    }
+	
+	getAll(findInput: { warehouse: string }): Observable<any>
+	{
+		return this.apollo
+		           .query<any>({
+			                       query: GQLQueries.PromotionAll,
+			                       variables: { findInput },
+		                       })
+		           .pipe(
+				           map((result) => result.data || []),
+				           share()
+		           );
+	}
+	
+	create(promotion: IPromotionCreateObject)
+	{
+		return this.apollo
+		           .mutate<{
+			           promotion: IPromotionCreateObject
+		           }>({
+			              mutation: GQLMutations.PromotionCreate,
+			              variables: {
+				              promotion,
+			              },
+		              })
+		           .pipe(
+				           map((result) => result.data),
+				           share()
+		           );
+	}
+	
+	update(id: String, promotion: IPromotionCreateObject)
+	{
+		return this.apollo
+		           .mutate<{
+			           promotion: IPromotionCreateObject
+		           }>({
+			              mutation: GQLMutations.PromotionUpdate,
+			              variables: {
+				              id,
+				              promotion,
+			              },
+		              })
+		           .pipe(
+				           map((result) => result.data),
+				           share()
+		           );
+	}
+	
+	removeByIds(ids: string[])
+	{
+		return this.apollo
+		           .mutate({
+			                   mutation: GQLMutations.PromotionRemoveByIds,
+			                   variables: { ids },
+		                   });
+	}
 }
