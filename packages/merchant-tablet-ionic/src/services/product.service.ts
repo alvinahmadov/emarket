e7f317e474e4b798baff5760c12caa40c4cf8555
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-08-16 09:00:06 +0400

    Refactor: Move graphql queries and mutations to separate file to make their edition easy

--- packages/merchant-tablet-ionic/src/services/product.service.ts
+++ packages/merchant-tablet-ionic/src/services/product.service.ts
@@ -1,32 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Apollo }     from 'apollo-angular';
-import gql            from 'graphql-tag';
-import { map, share } from 'rxjs/operators';
-import Product        from '@modules/server.common/entities/Product';
+import { Injectable }   from '@angular/core';
+import { Apollo }       from 'apollo-angular';
+import { map, share }   from 'rxjs/operators';
+import Product          from '@modules/server.common/entities/Product';
+import { GQLMutations } from '@modules/server.common/utilities/graphql'
 
 @Injectable()
 export class ProductService
 {
 	constructor(private readonly apollo: Apollo) {}
-
-    save(product: Product)
-    {
-        return this.apollo
-		.mutate<{ product: Product }>({
-                                          mutation: gql`
-                                              mutation SaveProduct($product: ProductSaveInput!) {
-                                                  saveProduct(product: $product) {
-                                                      id
-                                                  }
-                                              }
-			                              `,
-			                              variables: {
-				                              product,
-			                              },
-                                      })
-		.pipe(
-				map((result) => result.data.product),
-				share()
-		);
-    }
+	
+	save(product: Product)
+	{
+		return this.apollo
+		           .mutate<{
+			           product: Product
+		           }>(
+				           {
+					           mutation: GQLMutations.WarehouseSaveProduct,
+					           variables: {
+						           product,
+					           },
+				           }
+		           ).pipe(
+						map((result) => result.data.product),
+						share()
+				);
+	}
 }
