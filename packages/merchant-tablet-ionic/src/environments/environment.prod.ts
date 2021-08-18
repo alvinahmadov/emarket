bb3764d9c659d38b13de8d0ceac55a619a6aa63e
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-08-19 00:23:13 +0400

    Delete autogenerated files from git

--- packages/merchant-tablet-ionic/src/environments/environment.prod.ts
+++ packages/merchant-tablet-ionic/src/environments/environment.prod.ts
@@ -7,45 +7,50 @@
 import { Environment } from './model';
 
 export const environment: Environment = {
-	production: false,
-
-	GQL_ENDPOINT: 'http://localhost:5555/graphql',
-	GQL_SUBSCRIPTIONS_ENDPOINT: 'ws://localhost:5050/subscriptions',
-	SERVICES_ENDPOINT: 'http://localhost:5500',
-	HTTPS_SERVICES_ENDPOINT: 'https://localhost:5501',
-
-	APP_VERSION: '0.2.0',
-
-	API_FILE_UPLOAD_URL: 'https://api.cloudinary.com/v1_1/alvindre/upload',
-
-	DEFAULT_LOGIN_USERNAME: 'hut_pizza',
-	DEFAULT_LOGIN_PASSWORD: '123456',
-
-	LOGIN_LOGO: 'assets/imgs/ever-logo.svg',
-	NO_INTERNET_LOGO: 'assets/imgs/logo.png',
-
-	COMPANY_NAME: 'Ever Co. LTD',
-	APP_NAME: 'Ever Merchant',
-
-	GOOGLE_MAPS_API_KEY: '',
-
-	GOOGLE_ANALYTICS_API_KEY: '',
-	FAKE_UUID: 'c2360292-3b42-456d-ac37-1cbd9429d4d1',
-	MIXPANEL_API_KEY: '',
-
-	MAP_MERCHANT_ICON_LINK:
-		'http://maps.google.com/mapfiles/kml/pal3/icon21.png',
-
-	MAP_USER_ICON_LINK: 'http://maps.google.com/mapfiles/kml/pal3/icon48.png',
-
-	MAP_CARRIER_ICON_LINK:
-		'http://maps.google.com/mapfiles/kml/pal4/icon54.png',
-
-	DEFAULT_LANGUAGE: 'en',
-
-	// For maintenance micro service
-	SETTINGS_APP_TYPE: 'merchant-tablet',
-	SETTINGS_MAINTENANCE_API_URL: '',
+  production: false,
+  
+  GQL_ENDPOINT: 'http://localhost:5555/graphql',
+  GQL_SUBSCRIPTIONS_ENDPOINT: 'ws://localhost:5050/subscriptions',
+  SERVICES_ENDPOINT: 'http://localhost:5500',
+  HTTPS_SERVICES_ENDPOINT: 'https://localhost:5501',
+  
+  APP_VERSION: '0.2.0',
+  
+  CLOUDINARY_SIGNED_UPLOAD_PRESET: 'emarket-products-images',
+  CLOUDINARY_UNSIGNED_UPLOAD_PRESET: 'emarket-products-images',
+  CLOUDINARY_UPLOAD_URL: 'https://api.cloudinary.com/v1_1/alvindre/upload',
+  API_FILE_UPLOAD_URL: 'https://api.cloudinary.com/v1_1/alvindre/upload',
+  
+  DEFAULT_LOGIN_USERNAME: 'user',
+  DEFAULT_LOGIN_PASSWORD: '12345',
+  
+  LOGIN_LOGO: 'assets/imgs/ever-logo.svg',
+  NO_INTERNET_LOGO: 'assets/imgs/logo.png',
+  
+  COMPANY_NAME: 'Company',
+  APP_NAME: 'Merchant',
+  
+  GOOGLE_MAPS_API_KEY: '',
+  
+  GOOGLE_ANALYTICS_API_KEY: '',
+  FAKE_UUID: 'c2360292-3b42-456d-ac37-1cbd9429d4d1',
+  MIXPANEL_API_KEY: '',
+  
+  WAREHOUSE_PASSWORD_BCRYPT_SALT_ROUNDS: 12,
+  
+  MAP_MERCHANT_ICON_LINK: 'https://maps.google.com/mapfiles/kml/pal3/icon21.png',
+  
+  MAP_USER_ICON_LINK: 'https://maps.google.com/mapfiles/kml/pal3/icon48.png',
+  
+  MAP_CARRIER_ICON_LINK: 'https://maps.google.com/mapfiles/kml/pal4/icon54.png',
+  
+  DEFAULT_LANGUAGE: 'ru-RU',
+  AVAILABLE_LOCALES: 'en-US|ru-RU',
+  
+  // For maintenance micro service.
+  SETTINGS_APP_TYPE: 'merchant-tablet',
+  SETTINGS_MAINTENANCE_API_URL: ''
+  
 };
 
 /*
@@ -55,3 +60,4 @@ export const environment: Environment = {
  * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
