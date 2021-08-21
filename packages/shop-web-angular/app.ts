d09ca2e74a5c90635432b627048cf54105539c0d
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-08-21 14:11:46 +0400

    Add host

--- packages/shop-web-angular/app.ts
+++ packages/shop-web-angular/app.ts
@@ -3,10 +3,11 @@ import path        from 'path';
 import serveStatic from 'serve-static';
 import { env }     from './scripts/env';
 
+const host: string = env.HOST;
 const port: number = env.PORT;
 
 connect()
 		.use(serveStatic(path.join(__dirname, '../../../../www')))
-		.listen(port);
+		.listen(port, host);
 
-console.log(`listening on ${port}`);
+console.log(`Listening on ${host}:${port}`);
