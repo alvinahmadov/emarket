df4c05ae08db3738baa9a61b19be6dfcf628779f
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-09-05 21:12:36 +0400

    Rename User to Customer and add chat classes

--- packages/common-angular/src/routers/routers.module.ts
+++ packages/common-angular/src/routers/routers.module.ts
@@ -1,33 +1,37 @@
 import { NgModule }                    from '@angular/core';
 import { CommonLibModule }             from '../lib';
-import { CarrierOrdersRouter }         from './carrier-orders-router.service';
-import { InviteRequestRouter }         from './invite-request-router.service';
-import { ProductRouter }               from './product-router.service';
-import { InviteRouter }                from './invite-router.service';
-import { GeoLocationWarehousesRouter } from './geo-location-warehouses-router.service';
-import { GeoLocationProductsRouter }   from './geo-location-products-router.service';
 import { CarrierRouter }               from './carrier-router.service';
+import { CarrierOrdersRouter }         from './carrier-orders-router.service';
+import { ConversationsRouter }         from './conversation-router.service';
+import { ConversationMessagesRouter }  from './conversation-messages-router.service';
+import { CustomerRouter }              from './customer-router.service';
+import { CustomerAuthRouter }          from './customer-auth-router.service';
+import { CustomerOrdersRouter }        from './customer-orders-router.service';
+import { CustomerProductsRouter }      from './customer-products-router.service';
 import { DeviceRouter }                from './device-router.service';
+import { GeoLocationRouter }           from './geo-location-router.service';
 import { GeoLocationOrdersRouter }     from './geo-location-orders-router.service';
+import { GeoLocationProductsRouter }   from './geo-location-products-router.service';
+import { GeoLocationWarehousesRouter } from './geo-location-warehouses-router.service';
+import { InviteRouter }                from './invite-router.service';
+import { InviteRequestRouter }         from './invite-request-router.service';
 import { OrderRouter }                 from './order-router.service';
-import { UserRouter }                  from './user-router.service';
+import { ProductRouter }               from './product-router.service';
 import { WarehouseCarriersRouter }     from './warehouse-carriers-router.service';
 import { WarehouseProductsRouter }     from './warehouse-products-router.service';
 import { WarehouseRouter }             from './warehouse-router.service';
 import { WarehouseAuthRouter }         from './warehouse-auth-router.service';
-import { UserProductsRouter }          from './user-products-router.service';
 import { WarehouseOrdersRouter }       from './warehouse-orders-router.service';
-import { UserOrdersRouter }            from './user-orders-router.service';
-import { UserAuthRouter }              from './user-auth-router.service';
-import { GeoLocationRouter }           from './geo-location-router.service';
 
 @NgModule({
-	          imports: [CommonLibModule],
-	          exports: [],
+	          imports:      [CommonLibModule],
+	          exports:      [],
 	          declarations: [],
-	          providers: [
+	          providers:    [
 		          CarrierOrdersRouter,
 		          CarrierRouter,
+		          ConversationsRouter,
+		          ConversationMessagesRouter,
 		          DeviceRouter,
 		          GeoLocationOrdersRouter,
 		          GeoLocationProductsRouter,
@@ -37,15 +41,15 @@ import { GeoLocationRouter }           from './geo-location-router.service';
 		          InviteRouter,
 		          OrderRouter,
 		          ProductRouter,
-		          UserOrdersRouter,
-		          UserRouter,
-		          UserAuthRouter,
+		          CustomerOrdersRouter,
+		          CustomerRouter,
+		          CustomerAuthRouter,
 		          WarehouseCarriersRouter,
 		          WarehouseRouter,
 		          WarehouseAuthRouter,
 		          WarehouseProductsRouter,
 		          WarehouseOrdersRouter,
-		          UserProductsRouter,
+		          CustomerProductsRouter,
 	          ],
           })
 export class RoutersModule {}
