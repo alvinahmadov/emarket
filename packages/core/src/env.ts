4d9a38a86becb965170895d6a6094f6f406ed7d4
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-09-07 04:06:25 +0400

    Refactor: Add vkontakte login/register support, add jwt expiration variable, add OAuth service callbacks Rename

--- packages/core/src/env.ts
+++ packages/core/src/env.ts
@@ -15,7 +15,7 @@ export type Env = Readonly<{
 	CONNECTION_TIMEOUT: number;
 	
 	HTTPS_SERVICES_ENDPOINT: string;
-	HTTP_SERVICES_ENDPOINT: string;
+	SERVICES_ENDPOINT: string;
 	GQL_ENDPOINT: string;
 	GQL_SUBSCRIPTIONS_ENDPOINT: string;
 	
@@ -50,17 +50,24 @@ export type Env = Readonly<{
 	
 	GOOGLE_APP_ID: string;
 	GOOGLE_APP_SECRET: string;
+	GOOGLE_APP_CALLBACK: string;
 	
 	YANDEX_APP_ID: string;
 	YANDEX_APP_SECRET: string;
+	YANDEX_APP_CALLBACK: string;
 	
 	FACEBOOK_APP_ID: string;
 	FACEBOOK_APP_SECRET: string;
+	FACEBOOK_APP_CALLBACK: string;
+	
+	VKONTAKTE_APP_ID: string;
+	VKONTAKTE_APP_SECRET: string;
+	VKONTAKTE_APP_CALLBACK: string;
 	
 	AVAILABLE_LOCALES: string;
 	
 	JWT_SECRET: string;
-	JWT_EXPIRES: number;
+	JWT_EXPIRES: string;
 	
 	ADMIN_PASSWORD_BCRYPT_SALT_ROUNDS: number;
 	WAREHOUSE_PASSWORD_BCRYPT_SALT_ROUNDS: number;
@@ -77,9 +84,9 @@ export type Env = Readonly<{
 	
 	FAKE_INVITE_CODE: number;
 	
-	FAKE_USERNAME: string;
-	FAKE_PASSWORD: string;
-	FAKE_EMAIL: string;
+	FAKE_MERCHANT_NAME: string;
+	FAKE_MERCHANT_PASSWORD: string;
+	FAKE_MERCHANT_EMAIL: string;
 	
 	ARCGIS_CLIENT_ID: string;
 	ARCGIS_CLIENT_SECRET: string;
@@ -104,22 +111,22 @@ export const env: Env = cleanEnv(
 			              }) as ValidatorSpec<Env['NODE_ENV']>,
 			
 			WEB_CONCURRENCY: num({ default: 1 }),
-			WEB_MEMORY: num({ default: 2048 }),
+			WEB_MEMORY:      num({ default: 2048 }),
 			
 			CONNECTION_TIMEOUT: num({ default: 30 }),
 			
-			HTTPS_SERVICES_ENDPOINT: url({ default: 'https://localhost:5501' }),
-			HTTP_SERVICES_ENDPOINT: url({ default: 'http://localhost:5500' }),
-			GQL_ENDPOINT: url({ default: 'http://localhost:5555/graphql' }),
+			HTTPS_SERVICES_ENDPOINT:    url({ default: 'https://localhost:5501' }),
+			SERVICES_ENDPOINT:          url({ default: 'http://localhost:5500' }),
+			GQL_ENDPOINT:               url({ default: 'http://localhost:5555/graphql' }),
 			GQL_SUBSCRIPTIONS_ENDPOINT: url({ default: 'ws://localhost:5050/subscriptions' }),
 			
 			HTTPS_CERT_PATH: str({ default: 'certificates/https/cert.pem' }),
-			HTTPS_KEY_PATH: str({ default: 'certificates/https/key.pem' }),
+			HTTPS_KEY_PATH:  str({ default: 'certificates/https/key.pem' }),
 			
 			LOGS_PATH: str({ default: './tmp/logs' }),
 			
-			DB_URI: str({ default: 'http://localhost/emarketdb' }),
-			DB_POOL_SIZE: num({ default: 50 }),
+			DB_URI:             str({ default: 'http://localhost/emarketdb' }),
+			DB_POOL_SIZE:       num({ default: 50 }),
 			DB_CONNECT_TIMEOUT: num({ default: 40000 }),
 			
 			STRIPE_SECRET_KEY: str({ default: '' }),
@@ -135,36 +142,43 @@ export const env: Env = cleanEnv(
 			URBAN_AIRSHIP_KEY: str({ default: '' }),
 			URBAN_AIRSHIP_SECRET: str({ default: '' }),
 			
-			AWS_ACCESS_KEY_ID: str({ default: '' }),
+			AWS_ACCESS_KEY_ID:     str({ default: '' }),
 			AWS_SECRET_ACCESS_KEY: str({ default: '' }),
 			
 			KEYMETRICS_MACHINE_NAME: str({ default: '' }),
-			KEYMETRICS_SECRET_KEY: str({ default: '' }),
-			KEYMETRICS_PUBLIC_KEY: str({ default: '' }),
+			KEYMETRICS_SECRET_KEY:   str({ default: '' }),
+			KEYMETRICS_PUBLIC_KEY:   str({ default: '' }),
 			
-			GOOGLE_APP_ID: str({ default: '' }),
-			GOOGLE_APP_SECRET: str({ default: '' }),
+			GOOGLE_APP_ID:       str({ default: '' }),
+			GOOGLE_APP_SECRET:   str({ default: '' }),
+			GOOGLE_APP_CALLBACK: str({ default: '/auth/google/callback' }),
 			
-			YANDEX_APP_ID: str({ default: '' }),
-			YANDEX_APP_SECRET: str({ default: '' }),
+			YANDEX_APP_ID:       str({ default: '' }),
+			YANDEX_APP_SECRET:   str({ default: '' }),
+			YANDEX_APP_CALLBACK: str({ default: '/auth/yandex/callback' }),
 			
-			FACEBOOK_APP_ID: str({ default: '' }),
-			FACEBOOK_APP_SECRET: str({ default: '' }),
+			FACEBOOK_APP_ID:       str({ default: '' }),
+			FACEBOOK_APP_SECRET:   str({ default: '' }),
+			FACEBOOK_APP_CALLBACK: str({ default: '/auth/facebook' }),
+			
+			VKONTAKTE_APP_ID:       str({ default: '' }),
+			VKONTAKTE_APP_SECRET:   str({ default: '' }),
+			VKONTAKTE_APP_CALLBACK: str({ default: '/auth/facebook' }),
 			
 			AVAILABLE_LOCALES: str({ default: '' }),
 			
-			JWT_SECRET: str({ default: 'default' }),
-			JWT_EXPIRES: num({ default: 3600 }),
+			JWT_SECRET:  str({ default: 'default' }),
+			JWT_EXPIRES: str({ default: '2 days' }),
 			
 			ADMIN_PASSWORD_BCRYPT_SALT_ROUNDS: num({
-				                                       desc: 'Used for passwords encryption, recommended value: 12',
+				                                       desc:    'Used for passwords encryption, recommended value: 12',
 				                                       docs:
-						                                       'https://security.stackexchange.com/questions/17207/recommended-of-rounds-for-bcrypt',
+				                                                'https://security.stackexchange.com/questions/17207/recommended-of-rounds-for-bcrypt',
 				                                       default: 12
 			                                       }),
 			
 			WAREHOUSE_PASSWORD_BCRYPT_SALT_ROUNDS: num({
-				                                           desc: 'Used for passwords encryption, recommended value: 12',
+				                                           desc:    'Used for passwords encryption, recommended value: 12',
 				                                           docs:
 						                                           'https://security.stackexchange.com/questions/17207/recommended-of-rounds-for-bcrypt',
 				                                           default: 12
@@ -184,27 +198,27 @@ export const env: Env = cleanEnv(
 				                                      default: 10
 			                                      }),
 			
-			SETTING_INVITES_ENABLED: bool({ default: false }),
+			SETTING_INVITES_ENABLED:                  bool({ default: false }),
 			SETTINGS_REGISTRATIONS_REQUIRED_ON_START: bool({ default: false }),
-			ADMIN_PASSWORD_RESET: bool({ default: false }),
-			FAKE_DATA_GENERATOR: bool({ default: false }),
+			ADMIN_PASSWORD_RESET:                     bool({ default: false }),
+			FAKE_DATA_GENERATOR:                      bool({ default: false }),
 			
-			DEFAULT_ADMIN_MAIL: str({ default: 'admin@mail.com' }),
+			DEFAULT_ADMIN_MAIL:     str({ default: 'admin@mail.com' }),
 			DEFAULT_ADMIN_PASSWORD: str({ default: 'admin' }),
 			
-			FAKE_USERNAME: str({ default: 'user' }),
-			FAKE_PASSWORD: str({ default: '12345' }),
-			FAKE_EMAIL: str({ default: 'user@mail.com' }),
+			FAKE_MERCHANT_NAME:     str({ default: 'merchant' }),
+			FAKE_MERCHANT_PASSWORD: str({ default: '12345' }),
+			FAKE_MERCHANT_EMAIL:    str({ default: 'merchant@mail.com' }),
 			
 			FAKE_INVITE_CODE: num({ default: 0 }),
 			
-			ARCGIS_CLIENT_ID: str({ default: '' }),
+			ARCGIS_CLIENT_ID:     str({ default: '' }),
 			ARCGIS_CLIENT_SECRET: str({ default: '' }),
-			IP_STACK_API_KEY: str({ default: '' }),
-			LOG_LEVEL: str({
-				               choices: ['trace', 'debug', 'info', 'warn', 'error', 'fatal'],
-				               default: 'error'
-			               }),
+			IP_STACK_API_KEY:     str({ default: '' }),
+			LOG_LEVEL:            str({
+				                          choices: ['trace', 'debug', 'info', 'warn', 'error', 'fatal'],
+				                          default: 'error'
+			                          }),
 			APOLLO_KEY: str({
 				                desc:
 						                'Apollo Engine Key (optional, see https://www.apollographql.com/docs/platform/schema-registry)',
