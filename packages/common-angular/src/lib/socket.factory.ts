77a5c06fb5134e3471ae24b5122ec31f59e6b252
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-09-07 05:08:57 +0400

    Add explicit type

--- packages/common-angular/src/lib/socket.factory.ts
+++ packages/common-angular/src/lib/socket.factory.ts
@@ -1,10 +1,13 @@
 import { Socket, SOCKET_IO } from './socket.service';
 import { Inject }            from '@angular/core';
-import _io                   from 'socket.io-client';
 
 export class SocketFactory
 {
-	constructor(@Inject(SOCKET_IO) private readonly io: typeof _io) {}
+	constructor(
+			@Inject(SOCKET_IO)
+			private readonly io: SocketIOClientStatic
+	)
+	{}
 	
 	build(socketUrl: string): Socket
 	{
