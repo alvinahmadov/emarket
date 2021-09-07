01ba12aa2d609423e8e5b6720dd0340e73d9a9db
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-09-07 05:09:53 +0400

    Refactor

--- packages/common-angular/src/lib/router/routers.service.ts
+++ packages/common-angular/src/lib/router/routers.service.ts
@@ -17,15 +17,20 @@ export class RoutersService
 	
 	constructor()
 	{
-		/**
-		 * Here We get observable of routers, then we map it to observable of connectionStatus observables,
-		 * Each time router registered we combineLatest it's isSpecificErrorObs observable with the isAnyErrorObs
-		 * observable which initializes with the value of Observable.of(false)
-		 * By checking if any of the observables next's true, the result of the combineLatest becomes the new isAnyErrorObs.
-		 * At the end we do mergeAll, and forget about the fact there is any dependency on the routers observable by that.
-		 * Then comes publishReplay and refCount which makes sure we won't have to run this whole crazy code
-		 * each time some one subscribes isConnectionProblem.
-		 */
+		this._init();
+	}
+	
+	/**
+	 * Here We get observable of routers, then we map it to observable of connectionStatus observables,
+	 * Each time router registered we combineLatest it's isSpecificErrorObs observable with the isAnyErrorObs
+	 * observable which initializes with the value of Observable.of(false)
+	 * By checking if any of the observables next's true, the result of the combineLatest becomes the new isAnyErrorObs.
+	 * At the end we do mergeAll, and forget about the fact there is any dependency on the routers observable by that.
+	 * Then comes publishReplay and refCount which makes sure we won't have to run this whole crazy code
+	 * each time some one subscribes isConnectionProblem.
+	 */
+	private _init()
+	{
 		this.isConnectionProblem = this.sockets.pipe(
 				map((socket) =>
 				    {
