6148f9bd1ba6626d2e17c6b14fcf1daea5f36a3c
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-09-07 05:04:37 +0400

    Refactor: Remove unused languages

--- packages/common/src/interfaces/ILanguage.ts
+++ packages/common/src/interfaces/ILanguage.ts
@@ -1,22 +1,16 @@
 // TODO: add other locales
-type ILanguage = 'he-IL' | 'en-US' | 'ru-RU' | 'bg-BG' | 'es-ES';
+type ILanguage = 'en-US' | 'ru-RU';
 
 export default ILanguage;
 
 export enum LanguageCodesEnum
 {
 	ENGLISH = 'en-US',
-	HEBREW = 'he-IL',
 	RUSSIAN = 'ru-RU',
-	BULGARIAN = 'bg-BG',
-	SPANISH = 'es-ES',
 }
 
 export enum LanguagesEnum
 {
-	ENGLISH = 'ENGLISH',
-	HEBREW = 'HEBREW',
-	RUSSIAN = 'RUSSIAN',
-	BULGARIAN = 'BULGARIAN',
-	SPANISH = 'SPANISH',
+	ENGLISH = 'English',
+	RUSSIAN = 'Русский',
 }
