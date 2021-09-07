45a121d5baf61d83d9c73b2b656fcc85bd2a0aca
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-09-07 04:20:19 +0400

    Rename: User to Customer

--- packages/core/src/services/users/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './UsersService';
-export * from './SocialStrategiesService';
-export * from './SocialRegisterService';
-export * from './UsersOrdersService';
-export * from './UsersProductsService';
-export * from './UserCommandService';
-export * from './UsersAuthService';
