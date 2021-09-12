d3087f30207d06f9a1380f048def3beb28f84685
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-09-12 08:45:51 +0400

    Refactor: Move duplicated code to common utilities

--- packages/common/src/utilities/common.ts
+++ packages/common/src/utilities/common.ts
@@ -65,6 +65,38 @@ namespace CommonUtils
 		return `${d}${m}${year}-${order.orderNumber}`;
 	}
 	
+	export function getTotalDeliveryTime(order: Order): string
+	{
+		const start = order.createdAt;
+		
+		const end = new Date(order.deliveryTime);
+		
+		let delta = Math.abs(start.getTime() - end.getTime()) / 1000;
+		
+		const days = Math.floor(delta / 86400);
+		delta -= days * 86400;
+		
+		const hours = Math.floor(delta / 3600) % 24;
+		delta -= hours * 3600;
+		
+		const minutes = Math.floor(delta / 60) % 60;
+		delta -= minutes * 60;
+		
+		const seconds = delta % 60;
+		let secondsStr = seconds.toString();
+		secondsStr = secondsStr.substring(0, secondsStr.indexOf('.'));
+		
+		let h = '0' + hours;
+		h = h.substr(-2);
+		let min = '0' + minutes;
+		min = min.substr(-2);
+		let sec = '0' + secondsStr;
+		sec = sec.substr(-2);
+		
+		return `${days !== 0 ? days + 'days ' : ''}
+            ${h} : ${min} : ${sec}`;
+	}
+	
 	export function validateEmail(email: string): boolean
 	{
 		const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
