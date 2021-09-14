1d4ec9f3fa05db766772a47771d29e436de4a2b7
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-09-14 11:00:21 +0400

    Format and add import shortcuts

--- packages/admin-web-angular/src/app/pages/+customers/+customer/customer.module.ts
+++ packages/admin-web-angular/src/app/pages/+customers/+customer/customer.module.ts
@@ -1,44 +1,44 @@
 import { NgModule }                      from '@angular/core';
 import { CommonModule }                  from '@angular/common';
 import { RouterModule }                  from '@angular/router';
-import { ToasterModule }                 from 'angular2-toaster';
+import { FormsModule }                   from '@angular/forms';
+import { NbButtonModule }                from '@nebular/theme';
 import { TranslateModule }               from '@ngx-translate/core';
+import { NgSelectModule }                from '@ng-select/ng-select';
+import { ToasterModule }                 from 'angular2-toaster';
 import { Ng2SmartTableModule }           from 'ng2-smart-table';
 import { HighlightModule }               from 'ngx-highlightjs';
-import { ThemeModule }                   from '../../../@theme';
+import { CustomerOrdersTableModule }     from '@app/@shared/render-component/customer-orders-table/customer-orders-table.module';
+import { CustomerProductsTableModule }   from '@app/@shared/render-component/customer-products-table/customer-products-table.module';
+import { RenderComponentsModule }        from '@app/@shared/render-component/render-components.module';
+import { CustomerWarehousesTableModule } from '@app/@shared/warehouse/customer-warehouses-table/customer-warehouses-table.module';
+import { WarehouseMutationModule }       from '@app/@shared/warehouse/warehouse-mutation';
+import { WarehouseOrderModalModule }     from '@app/@shared/warehouse/+warehouse-order-modal/warehouse-order-modal.module';
+import { ThemeModule }                   from '@app/@theme';
 import { CustomerComponent }             from './customer.component';
 import { CustomerInfoComponent }         from './ea-customer-info/ea-customer-info.component';
 import { CustomerProductsComponent }     from './ea-customer-products/ea-customer-products/ea-customer-products.component';
 import { CustomerLocationComponent }     from './ea-customer-location/ea-customer-location.component';
 import { CustomerStoresComponent }       from './ea-customer-stores/ea-customer-stores.component';
-import { CustomerOrdersTableModule }     from '../../../@shared/render-component/customer-orders-table/customer-orders-table.module';
-import { CustomerProductsTableModule }   from '../../../@shared/render-component/customer-products-table/customer-products-table.module';
-import { RenderComponentsModule }        from '../../../@shared/render-component/render-components.module';
-import { CustomerWarehousesTableModule } from '../../../@shared/warehouse/customer-warehouses-table/customer-warehouses-table.module';
-import { WarehouseMutationModule }       from '../../../@shared/warehouse/warehouse-mutation';
-import { WarehouseOrderModalModule }     from '../../../@shared/warehouse/+warehouse-order-modal/warehouse-order-modal.module';
 import { CustomerOrdersModule }          from './ea-customer-orders/ea-customer-orders.module';
 import { CustomerMetricsComponent }      from './ea-customer-metrics/ea-customer-metrics.component';
-import { NgSelectModule }                from '@ng-select/ng-select';
-import { FormsModule }                   from '@angular/forms';
-import { NbButtonModule }                from '@nebular/theme';
 
 const routes = [
 	{
-		path: '',
+		path:      '',
 		component: CustomerComponent,
 	},
 	{
-		path: 'edit',
+		path:         'edit',
 		loadChildren: () =>
-				import('./+customer-edit/customer-edit.module').then(
-						(m) => m.CustomerEditModule
-				),
+				              import('./+customer-edit/customer-edit.module').then(
+						              (m) => m.CustomerEditModule
+				              ),
 	},
 ];
 
 @NgModule({
-	          imports: [
+	          imports:         [
 		          CommonModule,
 		          Ng2SmartTableModule,
 		          ThemeModule,
@@ -57,7 +57,7 @@ const routes = [
 		          FormsModule,
 		          NbButtonModule,
 	          ],
-	          declarations: [
+	          declarations:    [
 		          CustomerComponent,
 		          CustomerLocationComponent,
 		          CustomerInfoComponent,
