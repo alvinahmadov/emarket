a906d8978dd3fc7d53dd5ce23e6023729419977a
alvinahmadov <alvin.dev.ahmadov@yandex.com>
2021-09-14 10:22:36 +0400

    Rename, Reorder

--- packages/admin-web-angular/src/app/pages/+customers/customers.component.ts
+++ packages/admin-web-angular/src/app/pages/+customers/customers.component.ts
@@ -1,27 +1,24 @@
 import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { Router }                              from '@angular/router';
-import { LocalDataSource }                     from 'ng2-smart-table';
-import { UsersService }                        from '../../@core/data/users.service';
-import { OrdersService }                       from '../../@core/data/orders.service';
-import User                                    from '@modules/server.common/entities/User';
-import Order                                   from '@modules/server.common/entities/Order';
-import { UserMutationComponent }               from '../../@shared/user/user-mutation';
-import { ToasterService }                      from 'angular2-toaster';
 import { TranslateService }                    from '@ngx-translate/core';
-import { Observable, forkJoin }                from 'rxjs';
-import _                                       from 'lodash';
 import { NgbModal }                            from '@ng-bootstrap/ng-bootstrap';
-import { Subject }                             from 'rxjs';
+import { Observable, Subject, forkJoin }       from 'rxjs';
 import { takeUntil, first }                    from 'rxjs/operators';
-import { getCountryName }                      from '@modules/server.common/entities/GeoLocation';
-import { CountryRenderComponent }              from './+invites/country-render/country-render.component';
-import { RedirectNameComponent }               from '../../@shared/render-component/name-redirect/name-redirect.component';
-import { CustomerImageComponent }              from '../../@shared/render-component/customer-table/customer-table/customer-image.component';
-import { NotifyService }                       from '../../@core/services/notify/notify.service';
-import { CustomerOrdersNumberComponent }       from '../../@shared/render-component/customer-table/customer-orders-number/customer-orders-number.component';
-import { CustomerEmailComponent }              from '../../@shared/render-component/customer-email/customer-email.component';
-import { CustomerPhoneComponent }              from '../../@shared/render-component/customer-phone/customer-phone.component';
+import { LocalDataSource }                     from 'ng2-smart-table';
+import Customer                                from '@modules/server.common/entities/Customer';
+import Order                                   from '@modules/server.common/entities/Order';
+import { getDefaultCountryName }               from '@modules/server.common/data/countries';
+import { CustomersService }                    from '@app/@core/data/customers.service';
+import { OrdersService }                       from '@app/@core/data/orders.service';
+import { NotifyService }                       from '@app/@core/services/notify/notify.service';
+import { RedirectNameComponent }               from '@app/@shared/render-component/name-redirect/name-redirect.component';
+import { CustomerImageComponent }              from '@app/@shared/render-component/customer-table/customer-table/customer-image.component';
+import { CustomerOrdersNumberComponent }       from '@app/@shared/render-component/customer-table/customer-orders-number/customer-orders-number.component';
+import { CustomerEmailComponent }              from '@app/@shared/render-component/customer-email/customer-email.component';
+import { CustomerPhoneComponent }              from '@app/@shared/render-component/customer-phone/customer-phone.component';
+import { UserMutationComponent }               from '@app/@shared/user/user-mutation';
 import { BanConfirmComponent }                 from '@app/@shared/user/ban-confirm';
+import { CountryRenderComponent }              from './+invites/country-render/country-render.component';
 
 export interface CustomerViewModel
 {
@@ -40,28 +37,28 @@ export interface CustomerViewModel
 const perPage = 7;
 
 @Component({
-	           selector: 'ea-customers',
+	           selector:    'ea-customers',
+	           styleUrls:   ['/customers.component.scss'],
 	           templateUrl: './customers.component.html',
-	           styleUrls: ['/customers.component.scss'],
            })
 export class CustomersComponent implements AfterViewInit, OnDestroy
 {
 	static noInfoSign = '';
 	public loading: boolean;
 	public showBanLoading = false;
-	protected customers: User[] = [];
+	protected customers: Customer[] = [];
 	protected orders: Order[] = [];
-	protected settingsSmartTable: object;
-	protected sourceSmartTable = new LocalDataSource();
+	public settingsSmartTable: object;
+	public sourceSmartTable = new LocalDataSource();
 	private ngDestroy$ = new Subject<void>();
 	private _selectedCustomers: CustomerViewModel[] = [];
 	private dataCount: number;
-	private $users;
+	private $customers;
 	
 	constructor(
 			private readonly _router: Router,
 			private readonly _ordersService: OrdersService,
-			private readonly _usersService: UsersService,
+			private readonly _customersService: CustomersService,
 			private readonly _modalService: NgbModal,
 			private readonly _translateService: TranslateService,
 			private readonly _notifyService: NotifyService
@@ -88,21 +85,21 @@ export class CustomersComponent implements AfterViewInit, OnDestroy
 		return this._selectedCustomers.length === 1;
 	}
 	
-	public get isUserBanned()
+	public get isCustomerBanned()
 	{
 		return (
 				this._selectedCustomers[0] && this._selectedCustomers[0].isBanned
 		);
 	}
 	
-	protected get hasSelectedCustomers(): boolean
+	public get hasSelectedCustomers(): boolean
 	{
 		return this._selectedCustomers.length > 0;
 	}
 	
 	ngAfterViewInit()
 	{
-		this._addCustomHTMLElements();
+		CustomersComponent._addCustomHTMLElements();
 		this._applyTranslationOnSmartTable();
 		this.smartTableChange();
 		this._loadDataSmartTable();
@@ -116,33 +113,33 @@ export class CustomersComponent implements AfterViewInit, OnDestroy
 	
 	protected selectUser(ev)
 	{
-		const userId: string = ev.data.id;
-		this._router.navigate(['/customers/list' + userId]);
+		const customerId: string = ev.data.id;
+		this._router.navigate(['/customers/list' + customerId]);
 	}
 	
-	protected showCreateUserModal()
+	public showCreateUserModal()
 	{
 		this._modalService.open(UserMutationComponent, {
-			size: 'lg',
-			container: 'nb-layout',
+			size:        'lg',
+			container:   'nb-layout',
 			windowClass: 'ng-custom',
-			backdrop: 'static',
+			backdrop:    'static',
 		});
 	}
 	
-	protected selectCustomerTmp(ev)
+	public selectCustomerTmp(ev)
 	{
 		this._selectedCustomers = ev.selected;
 	}
 	
-	protected deleteSelectedRows()
+	public deleteSelectedRows()
 	{
 		const idsForDelete: string[] = this._selectedCustomers.map((w) => w.id);
 		
 		try
 		{
 			this.loading = true;
-			this._usersService
+			this._customersService
 			    .removeByIds(idsForDelete)
 			    .pipe(first())
 			    .toPromise();
@@ -163,9 +160,9 @@ export class CustomersComponent implements AfterViewInit, OnDestroy
 		}
 	}
 	
-	protected banSelectedRows()
+	public banSelectedRows()
 	{
-		if(this.isUserBanned)
+		if(this.isCustomerBanned)
 		{
 			this.showUnbanPopup();
 		}
@@ -178,18 +175,18 @@ export class CustomersComponent implements AfterViewInit, OnDestroy
 	private showUnbanPopup()
 	{
 		const modal = this._modalService.open(BanConfirmComponent, {
-			size: 'lg',
-			container: 'nb-layout',
+			size:        'lg',
+			container:   'nb-layout',
 			windowClass: 'ng-custom',
-			backdrop: 'static',
+			backdrop:    'static',
 		});
 		modal.componentInstance.user = this._selectedCustomers[0];
 		modal.result
 		     .then(async(user) =>
 		           {
 			           this.showBanLoading = true;
-			           await this._usersService.unbanUser(user.id);
-			           this._loadDataSmartTable();
+			           await this._customersService.unbanCustomer(user.id);
+			           await this._loadDataSmartTable();
 			           this.showBanLoading = false;
 			           this._notifyService.success(`${user.name} is unbanned!`);
 		           })
@@ -199,18 +196,18 @@ export class CustomersComponent implements AfterViewInit, OnDestroy
 	private showBanPopup()
 	{
 		const modal = this._modalService.open(BanConfirmComponent, {
-			size: 'lg',
-			container: 'nb-layout',
+			size:        'lg',
+			container:   'nb-layout',
 			windowClass: 'ng-custom',
-			backdrop: 'static',
+			backdrop:    'static',
 		});
 		modal.componentInstance.user = this._selectedCustomers[0];
 		modal.result
 		     .then(async(user) =>
 		           {
 			           this.showBanLoading = true;
-			           await this._usersService.banUser(user.id);
-			           this._loadDataSmartTable();
+			           await this._customersService.banCustomer(user.id);
+			           await this._loadDataSmartTable();
 			           this.showBanLoading = false;
 			           this._notifyService.success(`${user.name} is banned!`);
 		           })
@@ -247,56 +244,56 @@ export class CustomersComponent implements AfterViewInit, OnDestroy
 				 ]) =>
 				{
 					this.settingsSmartTable = {
-						actions: false,
+						actions:    false,
 						selectMode: 'multi',
-						columns: {
-							images: {
-								title: image,
-								class: 'customer-image',
-								type: 'custom',
-								renderComponent: CustomerImageComponent,
+						columns:    {
+							images:    {
+								title:                   image,
+								class:                   'customer-image',
+								type:                    'custom',
+								renderComponent:         CustomerImageComponent,
 								onComponentInitFunction: (instance) =>
-								{
-									instance.redirectPage = 'customers/list';
-								},
-								filter: false,
+								                         {
+									                         instance.redirectPage = 'customers/list';
+								                         },
+								filter:                  false,
 							},
-							name: {
-								title: name,
-								type: 'custom',
-								renderComponent: RedirectNameComponent,
+							name:      {
+								title:                   name,
+								type:                    'custom',
+								renderComponent:         RedirectNameComponent,
 								onComponentInitFunction: (instance) =>
-								{
-									instance.redirectPage = 'customers/list';
-								},
+								                         {
+									                         instance.redirectPage = 'customers/list';
+								                         },
 							},
-							email: {
-								title: email,
-								type: 'custom',
+							email:     {
+								title:           email,
+								type:            'custom',
 								renderComponent: CustomerEmailComponent,
 							},
-							phone: {
-								title: phone,
-								type: 'custom',
+							phone:     {
+								title:           phone,
+								type:            'custom',
 								renderComponent: CustomerPhoneComponent,
 							},
-							country: {
-								title: country,
+							country:   {
+								title:  country,
 								editor: {
-									type: 'custom',
+									type:      'custom',
 									component: CountryRenderComponent,
 								},
 							},
-							city: { title: city },
-							address: { title: address },
+							city:      { title: city },
+							address:   { title: address },
 							ordersQty: {
-								title: ordersQty,
-								type: 'custom',
+								title:           ordersQty,
+								type:            'custom',
 								renderComponent: CustomerOrdersNumberComponent,
-								filter: false,
+								filter:          false,
 							},
 						},
-						pager: {
+						pager:      {
 							display: true,
 							perPage,
 						},
@@ -307,79 +304,81 @@ export class CustomersComponent implements AfterViewInit, OnDestroy
 	
 	private async _loadDataSmartTable(page = 1)
 	{
-		if(this.$users)
+		if(this.$customers)
 		{
-			await this.$users.unsubscribe();
+			await this.$customers.unsubscribe();
 		}
-		let users: User[] = [];
+		let customers: Customer[] = [];
 		
 		const loadData = async() =>
 		{
-			const usersOrders = await this._ordersService.getUsersOrdersCountInfo(
-					users.map((u) => u.id)
-			);
+			const customerIds = customers.map((u) => u?.id);
 			
-			let usersVM = users.map((user) =>
-			                        {
-				                        const userOrders = usersOrders.find(
-						                        (res) => res['id'] === user.id
-				                        );
-				
-				                        return {
-					                        id: user.id,
-					                        image: user.image || CustomersComponent.noInfoSign,
-					                        name:
-							                        user.firstName && user.lastName
-							                        ? `${user.firstName} ${user.lastName}`
-							                        : user.firstName
-							                          ? user.firstName
-							                          : user.lastName
-							                            ? user.lastName
-							                            : user.id,
-					                        email: user.email || CustomersComponent.noInfoSign,
-					                        phone: user.phone || CustomersComponent.noInfoSign,
-					                        country:
-							                        getCountryName(user.geoLocation.countryId).trim() ||
-							                        CustomersComponent.noInfoSign,
-					                        city:
-							                        user.geoLocation.city || CustomersComponent.noInfoSign,
-					                        address: `st. ${
-							                        user.geoLocation.streetAddress ||
-							                        CustomersComponent.noInfoSign
-					                        }, hse. № ${
-							                        user.geoLocation.house || CustomersComponent.noInfoSign
-					                        }`,
-					                        ordersQty: userOrders ? userOrders.ordersCount : 0,
-					                        isBanned: user.isBanned,
-				                        };
-			                        });
+			const customersOrders = await this._ordersService
+			                                  .getUsersOrdersCountInfo(customerIds);
+			
+			let customersVM = customers.map(
+					(customer) =>
+					{
+						const customerOrders = customersOrders.find(
+								(res) => res['id'] === customer.id
+						);
+						
+						return {
+							id:        customer.id,
+							image:     customer.avatar || CustomersComponent.noInfoSign,
+							name:
+							           customer.firstName && customer.lastName
+							           ? `${customer.firstName} ${customer.lastName}`
+							           : customer.firstName
+							             ? customer.firstName
+							             : customer.lastName
+							               ? customer.lastName
+							               : customer.id,
+							email:     customer.email || CustomersComponent.noInfoSign,
+							phone:     customer.phone || CustomersComponent.noInfoSign,
+							country:
+							           getDefaultCountryName(customer.geoLocation.countryId).trim() ||
+							           CustomersComponent.noInfoSign,
+							city:
+							           customer.geoLocation.city || CustomersComponent.noInfoSign,
+							address:   `st. ${
+									customer.geoLocation.streetAddress ||
+									CustomersComponent.noInfoSign
+							}, hse. № ${
+									customer.geoLocation.house || CustomersComponent.noInfoSign
+							}`,
+							ordersQty: customerOrders ? customerOrders.ordersCount : 0,
+							isBanned:  customer.isBanned,
+						};
+					});
 			
 			await this.loadDataCount();
 			
 			if(this.showOnlyBanned)
 			{
-				usersVM = usersVM.filter((user) => user.isBanned);
+				customersVM = customersVM.filter((user) => user.isBanned);
 			}
 			
-			const usersData = new Array(this.dataCount);
+			const customersData = new Array(this.dataCount);
 			
-			usersData.splice(perPage * (page - 1), perPage, ...usersVM);
-			await this.sourceSmartTable.load(usersData);
+			customersData.splice(perPage * (page - 1), perPage, ...customersVM);
+			await this.sourceSmartTable.load(customersData);
 		};
 		
-		// We call two times 'loadData'
+		// We call two times 'loadData_()'
 		// This is need because in every change on one of them the server emit and we want to receive it
-		this.$users = this._usersService
-		                  .getUsers({
-			                            skip: perPage * (page - 1),
-			                            limit: perPage,
-		                            })
-		                  .pipe(takeUntil(this.ngDestroy$))
-		                  .subscribe(async(u: User[]) =>
-		                             {
-			                             users = u;
-			                             await loadData();
-		                             });
+		this.$customers = this._customersService
+		                      .getCustomers({
+			                                    skip:  perPage * (page - 1),
+			                                    limit: perPage,
+		                                    })
+		                      .pipe(takeUntil(this.ngDestroy$))
+		                      .subscribe(async(c: Customer[]) =>
+		                                 {
+			                                 customers = c;
+			                                 await loadData();
+		                                 });
 	}
 	
 	private _applyTranslationOnSmartTable()
@@ -393,7 +392,7 @@ export class CustomersComponent implements AfterViewInit, OnDestroy
 	}
 	
 	// This is just workaround to show some search icon on smart table, in the future maybe we must find better solution.
-	private _addCustomHTMLElements(): any
+	private static _addCustomHTMLElements(): any
 	{
 		document.querySelector(
 				'tr.ng2-smart-filters > th:nth-child(1)'
@@ -417,6 +416,6 @@ export class CustomersComponent implements AfterViewInit, OnDestroy
 	
 	private async loadDataCount()
 	{
-		this.dataCount = await this._usersService.getCountOfUsers();
+		this.dataCount = await this._customersService.getCountOfCustomers();
 	}
 }
