92a92f42958d9351f97df079bd9285ed0a6d93bf
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-06 22:01:07 +0400

    Initial

---
+++ packages/common/src/entities/Conversation.ts
@@ -0,0 +1,52 @@
+import { Entity, Column }                           from 'typeorm';
+import { ModelName, DBObject, Schema, Types }       from '../@pyro/db';
+import IConversation, { IConversationCreateObject } from '../interfaces/IConversation';
+
+/**
+ * Represent Device (e.g. Table or Browser, used by someone to access one of apps)
+ *
+ * @class Device
+ * @extends {DBObject<IDeviceRawObject, IDeviceCreateObject>}
+ * @implements {IDeviceRawObject}
+ */
+@ModelName('Conversation')
+@Entity({ name: 'conversations' })
+class Conversation extends DBObject<IConversation, IConversationCreateObject>
+		implements IConversation
+{
+	/**
+	 * ChannelID used for chat
+	 *
+	 * @type {(string | null)}
+	 * @memberof Device
+	 */
+	@Schema({ type: String, required: true })
+	@Column()
+	channelId: string;
+	
+	/**
+	 * Platform of the device (browser, ios, android, etc)
+	 *
+	 * @type {IPlatform}
+	 * @memberof Device
+	 */
+	@Types.String('browser')
+	@Column()
+	type: string;
+	
+	/**
+	 * Language for chat
+	 *
+	 * @type {ILanguage}
+	 * @memberof Device
+	 */
+	@Types.String('ru-RU')
+	@Column()
+	locale: string;
+	
+	@Types.Boolean(false)
+	@Column()
+	isDeleted: boolean;
+}
+
+export default Conversation;
