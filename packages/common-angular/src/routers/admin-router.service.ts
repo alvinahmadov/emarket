4ad53eba891ceda83d0a0389bb81d24c6cec8c46
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-07 01:05:17 +0400

    Initial

---
+++ packages/common-angular/src/routers/admin-router.service.ts
@@ -0,0 +1,46 @@
+import { Observable }                            from 'rxjs';
+import { map }                                   from 'rxjs/operators';
+import { Injectable }                            from '@angular/core';
+import IAdmin                                    from '@modules/server.common/interfaces/IAdmin';
+import Admin                                     from '@modules/server.common/entities/Admin';
+import IAdminRouter,
+{ IAdminLoginResponse, IAdminRegistrationInput } from '@modules/server.common/routers/IAdminRouter';
+import { Router, RouterFactory }                 from '../lib/router';
+
+@Injectable()
+export class AdminRouter implements IAdminRouter
+{
+	private readonly router: Router;
+	
+	constructor(routerFactory: RouterFactory)
+	{
+		this.router = routerFactory.create('admin');
+	}
+	
+	public get(id: string): Observable<Admin | null>
+	{
+		return this.router
+		           .runAndObserve<Admin>('get', id)
+		           .pipe(map((admin) => this._adminFactory(admin)));
+	}
+	
+	public login(email: string, password: string): Promise<IAdminLoginResponse | null>
+	{
+		return this.router.run<IAdminLoginResponse>('register', email, password);
+	}
+	
+	public register(input: IAdminRegistrationInput): Promise<Admin>
+	{
+		return Promise.reject("Method not allowed");
+	}
+	
+	public updateById(id: Admin["id"], updateObject: Partial<IAdmin>): Promise<Admin>
+	{
+		return Promise.reject("Method not allowed");
+	}
+	
+	protected _adminFactory(admin: IAdmin)
+	{
+		return admin == null ? null : new Admin(admin);
+	}
+}
