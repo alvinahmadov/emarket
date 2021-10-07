506fe3e80468373839d08c4e650c41cab9053b8e
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-07 16:30:03 +0400

    Reformat and add getters

--- packages/admin-web-angular/src/app/@shared/render-component/customer-phone/customer-phone.component.ts
+++ packages/admin-web-angular/src/app/@shared/render-component/customer-phone/customer-phone.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-	           styleUrls: ['./customer-phone.component.scss'],
+	           styleUrls:   ['./customer-phone.component.scss'],
 	           templateUrl: './customer-phone.component.html',
            })
 export class CustomerPhoneComponent
 {
 	@Input()
-	rowData: any;
+	public rowData: any;
 	
 	constructor() {}
+	
+	public get phone(): string
+	{
+		return this.rowData['phone'];
+	}
+	
+	public get hasPhone(): boolean
+	{
+		return this.rowData['phone'].length > 0;
+	}
 }
