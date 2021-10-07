855490635a8a5d9458ac5392c574313fa172c488
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-07 16:27:40 +0400

    Reformat and add getters

--- packages/admin-web-angular/src/app/@shared/render-component/name-redirect/name-redirect.component.ts
+++ packages/admin-web-angular/src/app/@shared/render-component/name-redirect/name-redirect.component.ts
@@ -3,21 +3,32 @@ import { ViewCell }                 from 'ng2-smart-table';
 import { Router }                   from '@angular/router';
 
 @Component({
-	           styleUrls: ['name-redirect.component.scss'],
+	           styleUrls:   ['name-redirect.component.scss'],
 	           templateUrl: './name-redirect.component.html',
            })
 export class RedirectNameComponent implements ViewCell, OnInit
 {
-	value: string | number;
-	redirectPage: string;
+	public value: string | number;
+	public redirectPage: string;
+	
 	@Input()
-	rowData: any;
+	public rowData: any;
 	
 	constructor(private readonly router: Router) {}
 	
-	ngOnInit() {}
+	public ngOnInit() {}
 	
-	redirect()
+	public get name(): string
+	{
+		return this.rowData['name'];
+	}
+	
+	public get isBanned(): boolean
+	{
+		return this.rowData['isBanned'];
+	}
+	
+	public redirect()
 	{
 		if(this.redirectPage)
 		{
