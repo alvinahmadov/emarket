4777a9326b50526b6564bf37a1f78362544fa0b9
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-08 04:08:02 +0400

    Format and add language selection configuration

--- packages/admin-web-angular/src/app/pages/pages.component.ts
+++ packages/admin-web-angular/src/app/pages/pages.component.ts
@@ -2,17 +2,35 @@ import { Component } from '@angular/core';
 
 import { TranslateService } from '@ngx-translate/core';
 import { NbMenuItem }       from '@nebular/theme';
+import { Store }            from '@app/@core/data/store.service';
+import { environment }      from 'environments/environment.prod';
 
 @Component({
-	           selector: 'ea-pages',
+	           selector:    'ea-pages',
 	           templateUrl: './pages.component.html',
            })
 export class PagesComponent
 {
 	menu: NbMenuItem[];
 	
-	constructor(private translate: TranslateService)
+	constructor(
+			private translate: TranslateService,
+			private storage: Store
+	)
 	{
+		if(this.storage.locale)
+		{
+			this.translate.use(this.storage.locale);
+		}
+		else if(this.translate.currentLang)
+		{
+			this.storage.locale = this.translate.currentLang;
+		}
+		else
+		{
+			this.storage.locale = environment.DEFAULT_LANGUAGE;
+			this.translate.use(environment.DEFAULT_LANGUAGE);
+		}
 		this.initialize();
 		this._applyTranslationOnSmartTable();
 	}
@@ -21,74 +39,74 @@ export class PagesComponent
 	{
 		this.menu = [
 			{
-				title: this.getTranslation('MENU_VIEW.DASHBOARD'),
-				icon: 'pie-chart-outline',
-				link: '/dashboard',
+				title:     this.getTranslation('MENU_VIEW.DASHBOARD'),
+				icon:      'pie-chart-outline',
+				link:      '/dashboard',
 				pathMatch: 'prefix',
 			},
 			{
-				title: this.getTranslation('MENU_VIEW.STORES'),
-				icon: 'home-outline',
-				link: '/stores',
+				title:     this.getTranslation('MENU_VIEW.STORES'),
+				icon:      'home-outline',
+				link:      '/stores',
 				pathMatch: 'prefix',
 			},
 			{
-				title: this.getTranslation('MENU_VIEW.PRODUCTS.PRODUCTS'),
-				icon: 'shopping-cart-outline',
-				link: '/products',
+				title:    this.getTranslation('MENU_VIEW.PRODUCTS.PRODUCTS'),
+				icon:     'shopping-cart-outline',
+				link:     '/products',
 				children: [
 					{
 						title: this.getTranslation(
 								'MENU_VIEW.PRODUCTS.MANAGEMENT'
 						),
-						link: '/products/list',
+						link:  '/products/list',
 					},
 					{
-						title: this.getTranslation(
+						title:     this.getTranslation(
 								'MENU_VIEW.PRODUCTS.CATEGORIES'
 						),
-						link: '/products/categories',
+						link:      '/products/categories',
 						pathMatch: 'prefix',
 					},
 				],
 			},
 			{
-				title: this.getTranslation('MENU_VIEW.CUSTOMERS.CUSTOMERS'),
-				icon: 'person-outline',
-				link: '/customers',
+				title:    this.getTranslation('MENU_VIEW.CUSTOMERS.CUSTOMERS'),
+				icon:     'person-outline',
+				link:     '/customers',
 				children: [
 					{
 						title: this.getTranslation(
 								'MENU_VIEW.CUSTOMERS.MANAGEMENT'
 						),
-						link: '/customers/list',
+						link:  '/customers/list',
 					},
 					{
-						title: this.getTranslation(
+						title:     this.getTranslation(
 								'MENU_VIEW.CUSTOMERS.INVITES'
 						),
-						link: '/customers/invites',
+						link:      '/customers/invites',
 						pathMatch: 'prefix',
-					},
+					}
 				],
 			},
 			{
-				title: this.getTranslation('MENU_VIEW.CARRIERS'),
-				icon: 'car-outline',
-				link: '/carriers',
+				title:     this.getTranslation('MENU_VIEW.CARRIERS'),
+				icon:      'car-outline',
+				link:      '/carriers',
 				pathMatch: 'prefix',
 			},
 			{
-				title: this.getTranslation('MENU_VIEW.SIMULATION'),
-				icon: 'star-outline',
-				link: '/simulation',
+				title:     this.getTranslation('MENU_VIEW.SIMULATION'),
+				icon:      'star-outline',
+				link:      '/simulation',
 				pathMatch: 'prefix',
-				home: true,
+				home:      true,
 			},
 			{
-				title: this.getTranslation('MENU_VIEW.SETUP'),
-				icon: 'settings-2-outline',
-				link: '/setup',
+				title:     this.getTranslation('MENU_VIEW.SETUP'),
+				icon:      'settings-2-outline',
+				link:      '/setup',
 				pathMatch: 'prefix',
 			},
 		];
