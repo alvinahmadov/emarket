9c25e9dbe6f736767f740f6505e6cdcf6782d964
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-10 14:43:38 +0400

    Remove unused code Move core util functions to common utilities

--- packages/core/src/pm2bootstrap.ts
+++ packages/core/src/pm2bootstrap.ts
@@ -1,16 +1,14 @@
-import pm2, { StartOptions } from 'pm2';
-import { createLogger }      from './helpers/Log';
-import { getHostAndPort }    from './utils';
-import { env }               from './env';
+import pm2, { StartOptions }     from 'pm2';
+import { CommonUtils as Common } from '@modules/server.common/utilities';
+import { createLogger }          from './helpers/Log';
+import { env }                   from './env';
 
 const mode = env.isProd ? 'production' : 'development';
-
-const log = createLogger({ name: 'PM2' });
 
 const MACHINE_NAME = process.env.KEYMETRICS_MACHINE_NAME;
 const PRIVATE_KEY = process.env.KEYMETRICS_SECRET_KEY;
 const PUBLIC_KEY = process.env.KEYMETRICS_PUBLIC_KEY;
-const [HOST, PORT] = getHostAndPort(env.SERVICES_ENDPOINT)
+const [HOST, PORT] = Common.getHostAndPort(env.SERVICES_ENDPOINT)
 const appName = process.env.PM2_APP_NAME || 'EMarket';
 const isProd = env.isProd;
 const instances = env.WEB_CONCURRENCY;
@@ -18,19 +16,19 @@ const maxMemory = env.WEB_MEMORY;
 const processName = 'MarketApi'
 
 const startOptions: StartOptions = {
-	script: './build/src/main.js',
-	name: processName,
-	exec_mode: 'fork',
-	instances: instances,
+	script:             './build/src/main.js',
+	name:               processName,
+	exec_mode:          'fork',
+	instances:          instances,
 	max_memory_restart: maxMemory + 'M',
-	watch: env.isDev,
-	env: {
-		NODE_ENV: isProd ? 'production' : 'development',
-		PORT: PORT.toString() || "5500",
-		HOST: HOST || "http://localhost",
+	watch:              env.isDev,
+	env:                {
+		NODE_ENV:                isProd ? 'production' : 'development',
+		PORT:                    PORT.toString() || "5500",
+		HOST:                    HOST || "http://localhost",
 		KEYMETRICS_MACHINE_NAME: MACHINE_NAME,
-		KEYMETRICS_PUBLIC: PUBLIC_KEY,
-		KEYMETRICS_SECRET: PRIVATE_KEY,
+		KEYMETRICS_PUBLIC:       PUBLIC_KEY,
+		KEYMETRICS_SECRET:       PRIVATE_KEY,
 	},
 };
 
