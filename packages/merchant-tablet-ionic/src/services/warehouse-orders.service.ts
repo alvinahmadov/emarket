cea9add13e5e8d753137814ef1f3d3f8f4aad431
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-11 23:37:00 +0400

    Reorder Reformat Add explicit types and access specifiers

--- packages/merchant-tablet-ionic/src/services/warehouse-orders.service.ts
+++ packages/merchant-tablet-ionic/src/services/warehouse-orders.service.ts
@@ -1,29 +1,34 @@
 import { Injectable }               from '@angular/core';
 import { Apollo }                   from 'apollo-angular';
 import { Observable }               from 'rxjs';
-import { IOrderCreateInput }        from '@modules/server.common/routers/IWarehouseOrdersRouter';
-import Order                        from '@modules/server.common/entities/Order';
 import { map }                      from 'rxjs/operators';
 import IPagingOptions               from '@modules/server.common/interfaces/IPagingOptions';
+import Order                        from '@modules/server.common/entities/Order';
+import { IOrderCreateInput }        from '@modules/server.common/routers/IWarehouseOrdersRouter';
 import { GQLMutations, GQLQueries } from '@modules/server.common/utilities/graphql';
+
+export interface IRemovedUserOrdersResponse
+{
+	num?: number
+	modified?: number
+}
 
 @Injectable()
 export class WarehouseOrdersService
 {
-	constructor(private readonly _apollo: Apollo)
-	{ }
+	constructor(private readonly _apollo: Apollo) {}
 	
-	createOrder(createInput: IOrderCreateInput): Observable<Order>
+	public createOrder(createInput: IOrderCreateInput): Observable<Order>
 	{
 		return this._apollo
 		           .mutate({
-			                   mutation: GQLMutations.StoreOrdersMakeOrder,
+			                   mutation:  GQLMutations.StoreOrdersMakeOrder,
 			                   variables: { createInput },
 		                   })
-		           .pipe(map((result: any) => result.data.createOrder));
+		           .pipe(map((result: any) => result.data['createOrder']));
 	}
 	
-	getStoreOrdersTableData(
+	public getStoreOrdersTableData(
 			storeId: string,
 			pagingOptions?: IPagingOptions,
 			status?: string
@@ -31,20 +36,20 @@ export class WarehouseOrdersService
 	{
 		return this._apollo
 		           .watchQuery({
-			                       query: GQLQueries.StoreOrdersTableData,
+			                       query:        GQLQueries.WarehouseOrdersTableData,
 			                       pollInterval: 2000,
-			                       variables: { storeId, pagingOptions, status },
+			                       variables:    { storeId, pagingOptions, status },
 		                       })
 		           .valueChanges.pipe(
 						map((res) => res.data['getStoreOrdersTableData'])
 				);
 	}
 	
-	async getCountOfStoreOrders(storeId: string, status?: string)
+	public async getCountOfStoreOrders(storeId: string, status?: string): Promise<number>
 	{
 		const res = await this._apollo
 		                      .query({
-			                             query: GQLQueries.StoreOrdersCount,
+			                             query:     GQLQueries.WarehouseOrdersCount,
 			                             variables: { storeId, status },
 		                             })
 		                      .toPromise();
@@ -52,23 +57,23 @@ export class WarehouseOrdersService
 		return res.data['getCountOfStoreOrders'];
 	}
 	
-	async removeUserOrders(storeId: string, userId: string)
+	public async removeCustomerOrders(storeId: string, customerId: string): Promise<IRemovedUserOrdersResponse>
 	{
 		const res = await this._apollo
 		                      .query({
-			                             query: GQLQueries.StoreOrdersRemoveOrders,
-			                             variables: { storeId, userId },
+			                             query:     GQLQueries.WarehouseOrdersRemoveOrders,
+			                             variables: { storeId, customerId },
 		                             })
 		                      .toPromise();
 		
-		return res.data['removeUserOrders'];
+		return res.data['removeCustomerOrders'];
 	}
 	
-	async getOrdersInDelivery(storeId: string)
+	public async getOrdersInDelivery(storeId: string): Promise<Order[]>
 	{
 		const res = await this._apollo
 		                      .query({
-			                             query: GQLQueries.OrdersInDelivery,
+			                             query:     GQLQueries.WarehouseOrdersInDelivery,
 			                             variables: { storeId },
 		                             })
 		                      .toPromise();
