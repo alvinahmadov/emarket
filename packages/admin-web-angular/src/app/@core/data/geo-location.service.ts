e3ebc54d5ee7a11e0f3ef9cb7ea70f7190c3f254
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-11 23:39:23 +0400

    Reorder Reformat Add explicit types and access specifiers Add poll interval static constant

--- packages/admin-web-angular/src/app/@core/data/geo-location.service.ts
+++ packages/admin-web-angular/src/app/@core/data/geo-location.service.ts
@@ -1,36 +1,36 @@
 import { Injectable } from '@angular/core';
-import GeoLocation    from '@modules/server.common/entities/GeoLocation';
 import { Apollo }     from 'apollo-angular';
-import ProductInfo    from '@modules/server.common/entities/ProductInfo';
 import { Observable } from 'rxjs';
 import { map, share } from 'rxjs/operators';
+import GeoLocation    from '@modules/server.common/entities/GeoLocation';
+import ProductInfo    from '@modules/server.common/entities/ProductInfo';
 import { GQLQueries } from '@modules/server.common/utilities/graphql';
 
 @Injectable()
 export class GeoLocationService
 {
+	private static readonly pollInteral: number = 10000;
+	
 	constructor(private readonly apollo: Apollo) {}
 	
-	getGeoLocationProducts(
-			geoLocation: GeoLocation
-	): Observable<ProductInfo[]>
+	public getGeoLocationProducts(geoLocation: GeoLocation): Observable<ProductInfo[]>
 	{
 		return this.apollo
 		           .watchQuery<{
 			           geoLocationProducts: ProductInfo[]
 		           }>({
-			              query: GQLQueries.GeoLocationProduct,
-			              variables: {
+			              query:        GQLQueries.GeoLocationProduct,
+			              variables:    {
 				              geoLocation: {
-					              countryId: geoLocation.countryId,
-					              city: geoLocation.city,
-					              postcode: geoLocation.postcode,
+					              countryId:     geoLocation.countryId,
+					              city:          geoLocation.city,
+					              postcode:      geoLocation.postcode,
 					              streetAddress: geoLocation.streetAddress,
-					              house: geoLocation.house,
-					              loc: geoLocation.loc,
+					              house:         geoLocation.house,
+					              loc:           geoLocation.loc,
 				              },
 			              },
-			              pollInterval: 2000,
+			              pollInterval: GeoLocationService.pollInteral,
 		              })
 		           .valueChanges.pipe(
 						map((res) =>
