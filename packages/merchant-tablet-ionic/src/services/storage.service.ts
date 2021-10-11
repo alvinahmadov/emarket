9bb005ac5cffa57fff5902f45fc694910f2c441d
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-11 23:12:01 +0400

    Reformat Reorder imports Remove commented lines Add merchant id storage data

---
+++ packages/merchant-tablet-ionic/src/services/storage.service.ts
@@ -0,0 +1,153 @@
+import { Injectable }          from '@angular/core';
+import { TranslateService }    from '@ngx-translate/core';
+import Customer                from '@modules/server.common/entities/Customer';
+import Device                  from '@modules/server.common/entities/Device';
+import Warehouse               from '@modules/server.common/entities/Warehouse';
+import { WarehouseRouter }     from '@modules/client.common.angular2/routers/warehouse-router.service';
+import { WarehouseAuthRouter } from '@modules/client.common.angular2/routers/warehouse-auth-router.service';
+
+@Injectable()
+export class Storage
+{
+	constructor(
+			private readonly translate: TranslateService,
+			private readonly warehouseRouter: WarehouseRouter,
+			private readonly warehouseAuthRouter: WarehouseAuthRouter
+	)
+	{}
+	
+	public get token(): string | null
+	{
+		
+		return localStorage.getItem('token') || null;
+	}
+	
+	public set token(token: string)
+	{
+		if(token == null)
+		{
+			localStorage.removeItem('token');
+		}
+		else
+		{
+			localStorage.setItem('token', token);
+		}
+	}
+	
+	public get warehouseId(): string | null
+	{
+		return localStorage.getItem('_warehouseId') || null;
+	}
+	
+	public set warehouseId(id: Warehouse['id'] | null)
+	{
+		if(id == null)
+		{
+			localStorage.removeItem('_warehouseId');
+		}
+		else
+		{
+			localStorage.setItem('_warehouseId', id);
+		}
+	}
+	
+	public get merchantId(): string | null
+	{
+		return localStorage.getItem('_merchantId') || null;
+	}
+	
+	public set merchantId(id: Customer['id'] | null)
+	{
+		if(id == null)
+		{
+			localStorage.removeItem('_merchantId');
+		}
+		else
+		{
+			localStorage.setItem('_merchantId', id);
+		}
+	}
+	
+	public get deviceId(): string | null
+	{
+		return localStorage.getItem('_deviceId') || null;
+	}
+	
+	public set deviceId(id: Device['id'] | null)
+	{
+		if(id == null)
+		{
+			localStorage.removeItem('_deviceId');
+		}
+		else
+		{
+			localStorage.setItem('_deviceId', id);
+		}
+	}
+	
+	public get platform(): string | null
+	{
+		return localStorage.getItem('_platform') || null;
+	}
+	
+	public set platform(type: string | null)
+	{
+		if(type == null)
+		{
+			localStorage.removeItem('_platform');
+		}
+		else
+		{
+			localStorage.setItem('_platform', type);
+		}
+	}
+	
+	public get language(): string
+	{
+		return localStorage.getItem('_language') || null;
+	}
+	
+	public set language(language: string)
+	{
+		if(language == null)
+		{
+			localStorage.removeItem('_language');
+		}
+		else
+		{
+			localStorage.setItem('_language', language);
+		}
+		
+		this.translate.use(language);
+	}
+	
+	public get maintenanceMode(): string | null
+	{
+		return localStorage.getItem('maintenanceMode') || null;
+	}
+	
+	public get serverConnection()
+	{
+		return localStorage.getItem('serverConnection');
+	}
+	
+	public set serverConnection(val: string)
+	{
+		localStorage.setItem('serverConnection', val);
+	}
+	
+	public isLogged()
+	{
+		return this.warehouseAuthRouter.isAuthenticated(this.token)
+	}
+	
+	public clearMaintenanceMode()
+	{
+		localStorage.removeItem('maintenanceMode');
+	}
+	
+	public clear()
+	{
+		localStorage.clear();
+	}
+}
