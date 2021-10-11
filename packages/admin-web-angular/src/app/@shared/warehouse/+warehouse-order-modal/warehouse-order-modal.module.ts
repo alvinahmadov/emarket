d4543076674a7e6332f5f476782bc9e47c096920
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 03:58:43 +0400

    Add order comment component

--- packages/admin-web-angular/src/app/@shared/warehouse/+warehouse-order-modal/warehouse-order-modal.module.ts
+++ packages/admin-web-angular/src/app/@shared/warehouse/+warehouse-order-modal/warehouse-order-modal.module.ts
@@ -1,16 +1,21 @@
 import { NgModule }                        from '@angular/core';
 import { CommonModule }                    from '@angular/common';
-import { Ng2SmartTableModule }             from 'ng2-smart-table';
-import { ThemeModule }                     from '../../../@theme';
-import { WarehouseOrderModalComponent }    from './warehouse-order-modal.component';
-import { WarehouseOrderInputComponent }    from './warehouse-order-input.component';
 import { TranslateModule }                 from '@ngx-translate/core';
 import { NbSpinnerModule, NbButtonModule } from '@nebular/theme';
+import { Ng2SmartTableModule }             from 'ng2-smart-table';
+import { ThemeModule }                     from '@app/@theme';
+import { WarehouseOrderModalComponent }    from './warehouse-order-modal.component';
+import { WarehouseOrderInputComponent }    from './warehouse-order-input.component';
+import { MakeOrderCommentComponent }       from './make-order-comment.component';
 
-const COMPONENTS = [WarehouseOrderModalComponent, WarehouseOrderInputComponent];
+const COMPONENTS = [
+	WarehouseOrderModalComponent,
+	WarehouseOrderInputComponent,
+	MakeOrderCommentComponent
+];
 
 @NgModule({
-	          imports: [
+	          imports:         [
 		          CommonModule,
 		          ThemeModule,
 		          Ng2SmartTableModule,
@@ -19,8 +24,12 @@ const COMPONENTS = [WarehouseOrderModalComponent, WarehouseOrderInputComponent];
 		          TranslateModule.forChild(),
 		          NbButtonModule,
 	          ],
-	          declarations: [WarehouseOrderModalComponent, WarehouseOrderInputComponent],
+	          declarations:    [
+		          WarehouseOrderModalComponent,
+		          WarehouseOrderInputComponent,
+		          MakeOrderCommentComponent,
+	          ],
 	          entryComponents: COMPONENTS,
-	          exports: COMPONENTS,
+	          exports:         COMPONENTS,
           })
 export class WarehouseOrderModalModule {}
