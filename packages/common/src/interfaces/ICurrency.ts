2e217aa6616c4c9633239d986305bb7489a3e467
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-11 21:16:19 +0400

    Remove isDeleted from currency interface

--- packages/common/src/interfaces/ICurrency.ts
+++ packages/common/src/interfaces/ICurrency.ts
@@ -6,13 +6,11 @@ export interface ICurrencyCreateObject extends DBCreateObject
 	name?: string;
 	sign?: string;
 	order?: string;
-	isDeleted?: boolean;
 }
 
 interface ICurrency extends ICurrencyCreateObject, DBRawObject
 {
 	_id: PyroObjectId;
-	isDeleted: boolean;
 }
 
 export default ICurrency;
