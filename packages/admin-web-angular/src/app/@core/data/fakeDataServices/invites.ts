b2c0fdd5da9a2b071931a96f42ade081e5a42894
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 00:20:26 +0400

    Add ICoordinate type compatibility

--- packages/admin-web-angular/src/app/@core/data/fakeDataServices/invites.ts
+++ packages/admin-web-angular/src/app/@core/data/fakeDataServices/invites.ts
@@ -1,13 +1,13 @@
+import faker                   from 'faker';
 import { Injectable }          from '@angular/core';
 import { IInviteCreateObject } from '@modules/server.common/interfaces/IInvite';
-import { Country }             from '@modules/server.common/entities/GeoLocation';
-import faker                   from 'faker';
+import Country                 from '@modules/server.common/enums/Country';
 import { environment }         from 'environments/environment';
 
 const NEED_DEFAULT_SETTINGS_MESSAGE =
-		"Can't generate fake data without DEFAULT_LONGITUDE and DEFAULT_LATITUDE";
-const lng = environment['DEFAULT_LONGITUDE'];
-const lat = environment['DEFAULT_LATITUDE'];
+		      "Can't generate fake data without DEFAULT_LONGITUDE and DEFAULT_LATITUDE";
+const lng = environment.DEFAULT_LONGITUDE;
+const lat = environment.DEFAULT_LATITUDE;
 
 @Injectable()
 export default class FakeDataInvites
@@ -18,62 +18,74 @@ export default class FakeDataInvites
 		{
 			const a: IInviteCreateObject = {
 				geoLocation: {
-					city: 'אשדוד',
-					postcode: '77452',
+					city:          'אשדוד',
+					postcode:      '77452',
 					streetAddress: 'העצמאות',
-					house: '38',
-					countryId: 1,
-					loc: {
-						type: 'Point',
-						coordinates: [lng + 0.05, lat - 0.01],
+					house:         '38',
+					countryId:     1,
+					loc:           {
+						type:        'Point',
+						coordinates: {
+							lng: lng + 0.05,
+							lat: lat - 0.01
+						},
 					},
 				},
-				apartment: '3',
+				apartment:   '3',
 			};
 			
 			const b: IInviteCreateObject = {
 				geoLocation: {
-					city: 'אשדוד',
-					postcode: '77452',
+					city:          'אשדוד',
+					postcode:      '77452',
 					streetAddress: 'העצמאות',
-					house: '450',
-					countryId: 1,
-					loc: {
-						type: 'Point',
-						coordinates: [lng + 0.09, lat - 0.1],
+					house:         '450',
+					countryId:     1,
+					loc:           {
+						type:        'Point',
+						coordinates: {
+							lng: lng + 0.09,
+							lat: lat - 0.1
+						},
 					},
 				},
-				apartment: '2',
+				apartment:   '2',
 			};
 			
 			const c: IInviteCreateObject = {
 				geoLocation: {
-					city: 'Rishon LeTsiyon',
-					postcode: '77452',
+					city:          'Rishon LeTsiyon',
+					postcode:      '77452',
 					streetAddress: 'Jabotinsky',
-					house: '180',
-					countryId: 1,
-					loc: {
-						type: 'Point',
-						coordinates: [lng, lat - 0.01],
+					house:         '180',
+					countryId:     1,
+					loc:           {
+						type:        'Point',
+						coordinates: {
+							lng: lng,
+							lat: lat - 0.01
+						},
 					},
 				},
-				apartment: '6',
+				apartment:   '6',
 			};
 			
 			const d: IInviteCreateObject = {
 				geoLocation: {
-					city: 'Rishon LeTsiyon',
-					postcode: '77452',
+					city:          'Rishon LeTsiyon',
+					postcode:      '77452',
 					streetAddress: 'Jabotinsky',
-					house: '190',
-					countryId: 1,
-					loc: {
-						type: 'Point',
-						coordinates: [lng + 0.06, lat],
+					house:         '190',
+					countryId:     1,
+					loc:           {
+						type:        'Point',
+						coordinates: {
+							lng: lng + 0.06,
+							lat: lat
+						},
 					},
 				},
-				apartment: '6',
+				apartment:   '6',
 			};
 			
 			return { a, b, c, d };
@@ -91,18 +103,18 @@ export default class FakeDataInvites
 		{
 			return {
 				geoLocation: {
-					city: faker.address.city(),
-					postcode: faker.address.zipCode(),
-					notes: faker.lorem.text(1),
+					city:          faker.address.city(),
+					postcode:      faker.address.zipCode(),
+					notes:         faker.lorem.text(1),
 					streetAddress: faker.address.streetAddress(),
-					house: faker.random.number(199).toString(),
-					countryId: faker.random.number(1) as Country,
-					loc: {
-						type: 'Point',
-						coordinates: [lng, lat],
+					house:         faker.random.number(199).toString(),
+					countryId:     faker.random.number(1) as Country,
+					loc:           {
+						type:        'Point',
+						coordinates: { lng, lat },
 					},
 				},
-				apartment: faker.random.number(199).toString(),
+				apartment:   faker.random.number(199).toString(),
 			};
 		}
 		else
