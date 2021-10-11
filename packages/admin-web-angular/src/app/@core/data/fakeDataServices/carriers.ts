f22d2eaf9e98aaf7fe6a989a1ad6f60de4ddb991
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-11 23:46:44 +0400

    Reorder Reformat Set loc.coordinates as a ICoordinate object

--- packages/admin-web-angular/src/app/@core/data/fakeDataServices/carriers.ts
+++ packages/admin-web-angular/src/app/@core/data/fakeDataServices/carriers.ts
@@ -16,165 +16,163 @@ export default class FakeDataCarriers
 		mike: ICarrierRegistrationInput;
 		tom: ICarrierRegistrationInput;
 		josh: ICarrierRegistrationInput;
-	}> =
-			         lng && lat
-			         ? {
-						         generate: () =>
-						                   {
-							                   const firstName = faker.name.firstName();
-							
-							                   return {
-								                   carrier:  {
-									                   isDeleted:            false,
-									                   firstName,
-									                   lastName:             faker.name.lastName(),
-									                   status:               faker.random.number({
-										                                                             min: 0,
-										                                                             max: 2,
-									                                                             }) as CarrierStatus,
-									                   phone:                faker.phone.phoneNumber(),
-									                   username:             faker.internet.userName(),
-									                   logo:                 faker.image.avatar(),
-									                   numberOfDeliveries:   faker.random.number({
-										                                                             min: 200,
-										                                                             max: 20000,
-									                                                             }),
-									                   deliveriesCountToday: faker.random.number({
-										                                                             min: 0,
-										                                                             max: 60,
-									                                                             }),
-									                   isSharedCarrier:      true,
-									                   geoLocation:          {
-										                   city:          faker.address.city(),
-										                   postcode:      faker.address.zipCode(),
-										                   streetAddress: faker.address.streetAddress(),
-										                   house:         faker.random.number(199).toString(),
-										                   countryId:     faker.random.number(
-												                   220
-										                   ) as Country,
-										                   loc:           {
-											                   type:        'Point',
-											                   coordinates: [
-												                   FakeDataCarriers.getCloseCoordinate(lng),
-												                   FakeDataCarriers.getCloseCoordinate(lat),
-											                   ],
-										                   },
-									                   },
-								                   },
-								                   password: '123456',
-							                   };
-						                   },
-						
-						         mike: {
-							         carrier:  {
-								         isDeleted:            false,
-								         firstName:            'Mike',
-								         lastName:             'Carr',
-								         status:               CarrierStatus.Online,
-								         phone:                '052-315-2346',
-								         username:             'mike',
-								         logo:                 faker.image.avatar(),
-								         numberOfDeliveries:   faker.random.number({
-									                                                   min: 200,
-									                                                   max: 20000,
-								                                                   }),
-								         deliveriesCountToday: faker.random.number({
-									                                                   min: 0,
-									                                                   max: 60,
-								                                                   }),
-								         isSharedCarrier:      true,
-								         geoLocation:          {
-									         city:          'Ashdod',
-									         postcode:      '77452',
-									         streetAddress: 'HaAtsmaut',
-									         house:         '28',
-									         countryId:     Country.RU,
-									         loc:           {
-										         type:        'Point',
-										         coordinates: [
-											         FakeDataCarriers.getCloseCoordinate(lng),
-											         FakeDataCarriers.getCloseCoordinate(lat),
-										         ],
-									         },
-								         },
-							         },
-							         password: '123456',
-						         },
-						
-						         tom: {
-							         carrier:  {
-								         isDeleted:            false,
-								         firstName:            'Tom',
-								         lastName:             'Bisic',
-								         status:               CarrierStatus.Online,
-								         phone:                '052-311-5711',
-								         username:             'tom',
-								         logo:                 faker.image.avatar(),
-								         numberOfDeliveries:   faker.random.number({
-									                                                   min: 200,
-									                                                   max: 20000,
-								                                                   }),
-								         deliveriesCountToday: faker.random.number({
-									                                                   min: 0,
-									                                                   max: 60,
-								                                                   }),
-								         isSharedCarrier:      true,
-								         geoLocation:          {
-									         city:          'Ashdod',
-									         postcode:      '77452',
-									         streetAddress: 'HaAtsmaut',
-									         house:         '88',
-									         countryId:     Country.RU,
-									         loc:           {
-										         type:        'Point',
-										         coordinates: [
-											         FakeDataCarriers.getCloseCoordinate(lng),
-											         FakeDataCarriers.getCloseCoordinate(lat),
-										         ],
-									         },
-								         },
-							         },
-							         password: '123456',
-						         },
-						
-						         josh: {
-							         carrier:  {
-								         firstName:            'Josh',
-								         lastName:             'Lenon',
-								         status:               CarrierStatus.Online,
-								         phone:                '052-311-5711',
-								         username:             'josh',
-								         logo:                 faker.image.avatar(),
-								         numberOfDeliveries:   faker.random.number({
-									                                                   min: 200,
-									                                                   max: 20000,
-								                                                   }),
-								         deliveriesCountToday: faker.random.number({
-									                                                   min: 0,
-									                                                   max: 60,
-								                                                   }),
-								         isSharedCarrier:      true,
-								         geoLocation:          {
-									         city:          'Ashdod',
-									         postcode:      '77452',
-									         streetAddress: 'HaAtsmaut',
-									         house:         '88',
-									         countryId:     Country.RU,
-									         loc:           {
-										         type:        'Point',
-										         coordinates: [
-											         FakeDataCarriers.getCloseCoordinate(lng),
-											         FakeDataCarriers.getCloseCoordinate(lat),
-										         ],
-									         },
-								         },
-							         },
-							         password: '123456',
-						         },
-					         }
-			         : null;
+	}> = lng && lat
+	     ? {
+				generate: () =>
+				          {
+					          const firstName = faker.name.firstName();
+					          const lastName = faker.name.lastName();
+					
+					          return {
+						          carrier:  {
+							          isDeleted:            false,
+							          firstName:            firstName,
+							          lastName:             lastName,
+							          status:               faker.random.number({
+								                                                    min: 0,
+								                                                    max: 2,
+							                                                    }) as CarrierStatus,
+							          phone:                faker.phone.phoneNumber(),
+							          username:             faker.internet.userName(firstName, lastName),
+							          logo:                 faker.image.avatar(),
+							          numberOfDeliveries:   faker.random.number({
+								                                                    min: 200,
+								                                                    max: 20000,
+							                                                    }),
+							          deliveriesCountToday: faker.random.number({
+								                                                    min: 0,
+								                                                    max: 60,
+							                                                    }),
+							          isSharedCarrier:      true,
+							          geoLocation:          {
+								          city:          faker.address.city(),
+								          postcode:      faker.address.zipCode(),
+								          streetAddress: faker.address.streetAddress(),
+								          house:         faker.random.number(199).toString(),
+								          countryId:     faker.random.number(220) as Country,
+								          loc:           {
+									          type:        'Point',
+									          coordinates: {
+										          lat: FakeDataCarriers.getCloseCoordinate(lat),
+										          lng: FakeDataCarriers.getCloseCoordinate(lng),
+									          },
+								          },
+							          },
+						          },
+						          password: '123456',
+					          };
+				          },
+				
+				mike: {
+					carrier:  {
+						isDeleted:            false,
+						firstName:            'Mike',
+						lastName:             'Carr',
+						status:               CarrierStatus.Online,
+						phone:                '052-315-2346',
+						username:             'mike',
+						logo:                 faker.image.avatar(),
+						numberOfDeliveries:   faker.random.number({
+							                                          min: 200,
+							                                          max: 20000,
+						                                          }),
+						deliveriesCountToday: faker.random.number({
+							                                          min: 0,
+							                                          max: 60,
+						                                          }),
+						isSharedCarrier:      true,
+						geoLocation:          {
+							city:          'Ashdod',
+							postcode:      '77452',
+							streetAddress: 'HaAtsmaut',
+							house:         '28',
+							countryId:     Country.RU,
+							loc:           {
+								type:        'Point',
+								coordinates: {
+									lat: FakeDataCarriers.getCloseCoordinate(lat),
+									lng: FakeDataCarriers.getCloseCoordinate(lng),
+								},
+							},
+						},
+					},
+					password: '123456',
+				},
+				
+				tom: {
+					carrier:  {
+						isDeleted:            false,
+						firstName:            'Tom',
+						lastName:             'Bisic',
+						status:               CarrierStatus.Online,
+						phone:                '052-311-5711',
+						username:             'tom',
+						logo:                 faker.image.avatar(),
+						numberOfDeliveries:   faker.random.number({
+							                                          min: 200,
+							                                          max: 20000,
+						                                          }),
+						deliveriesCountToday: faker.random.number({
+							                                          min: 0,
+							                                          max: 60,
+						                                          }),
+						isSharedCarrier:      true,
+						geoLocation:          {
+							city:          'Ashdod',
+							postcode:      '77452',
+							streetAddress: 'HaAtsmaut',
+							house:         '88',
+							countryId:     Country.RU,
+							loc:           {
+								type:        'Point',
+								coordinates: {
+									lat: FakeDataCarriers.getCloseCoordinate(lat),
+									lng: FakeDataCarriers.getCloseCoordinate(lng),
+								},
+							},
+						},
+					},
+					password: '123456',
+				},
+				
+				josh: {
+					carrier:  {
+						firstName:            'Josh',
+						lastName:             'Lenon',
+						status:               CarrierStatus.Online,
+						phone:                '052-311-5711',
+						username:             'josh',
+						logo:                 faker.image.avatar(),
+						numberOfDeliveries:   faker.random.number({
+							                                          min: 200,
+							                                          max: 20000,
+						                                          }),
+						deliveriesCountToday: faker.random.number({
+							                                          min: 0,
+							                                          max: 60,
+						                                          }),
+						isSharedCarrier:      true,
+						geoLocation:          {
+							city:          'Ashdod',
+							postcode:      '77452',
+							streetAddress: 'HaAtsmaut',
+							house:         '88',
+							countryId:     Country.RU,
+							loc:           {
+								type:        'Point',
+								coordinates: {
+									lat: FakeDataCarriers.getCloseCoordinate(lat),
+									lng: FakeDataCarriers.getCloseCoordinate(lng),
+								},
+							},
+						},
+					},
+					password: '123456',
+				},
+			}
+	     : null;
 	
-	private static getCloseCoordinate(coord)
+	private static getCloseCoordinate(coord): number
 	{
 		const num = Math.floor(Math.random() * (9 + 1));
 		const num2 = Math.floor(Math.random() * (9 + 1));
