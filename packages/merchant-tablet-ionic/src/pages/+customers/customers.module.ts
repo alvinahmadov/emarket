0a00f722c0b4e22b00177701c2ff9d94b60fce4e
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-11 23:36:09 +0400

    Reorder Reformat Rename WarehouseUsersService to WarehouseCustomersService

--- packages/merchant-tablet-ionic/src/pages/+customers/customers.module.ts
+++ packages/merchant-tablet-ionic/src/pages/+customers/customers.module.ts
@@ -1,33 +1,33 @@
-import { NgModule }                 from '@angular/core';
-import { PipesModule }              from '@modules/client.common.angular2/pipes/pipes.module';
-import { ComponentsModule }         from '../../components/components.module';
-import { TranslateModule }          from '@ngx-translate/core';
-import { CustomersPage }            from './customers';
-import { WarehouseUsersService }    from '@modules/client.common.angular2/routers/warehouse-users.service';
-import { Ng2SmartTableModule }      from 'ng2-smart-table';
-import { UserPhoneComponent }       from '../../components/users-table/phone';
-import { AddressComponent }         from '../../components/users-table/address';
-import { OrdersComponent }          from '../../components/users-table/orders';
-import { TotalComponent }           from '../../components/users-table/total';
-import { EmailComponent }           from '../../components/users-table/email';
-import { ImageUserComponent }       from '../../components/users-table/image';
-import { OrdersService }            from '../../../src/services/orders.service';
-import { Routes, RouterModule }     from '@angular/router';
-import { IonicModule }              from '@ionic/angular';
-import { CommonModule }             from '@angular/common';
-import { FormsModule }              from '@angular/forms';
-import { ConfirmDeletePopupModule } from 'components/confirm-delete-popup/confirm-delete-popup.module';
-import { WarehouseOrdersService }   from 'services/warehouse-orders.service';
+import { NgModule }                  from '@angular/core';
+import { TranslateModule }           from '@ngx-translate/core';
+import { Routes, RouterModule }      from '@angular/router';
+import { IonicModule }               from '@ionic/angular';
+import { CommonModule }              from '@angular/common';
+import { FormsModule }               from '@angular/forms';
+import { Ng2SmartTableModule }       from 'ng2-smart-table';
+import { WarehouseCustomersService } from '@modules/client.common.angular2/routers/warehouse-customers.service';
+import { PipesModule }               from '@modules/client.common.angular2/pipes/pipes.module';
+import { CustomersPage }             from './customers';
+import { ConfirmDeletePopupModule }  from '../../components/confirm-delete-popup/confirm-delete-popup.module';
+import { ComponentsModule }          from '../../components/components.module';
+import { UserPhoneComponent }        from '../../components/users-table/phone';
+import { AddressComponent }          from '../../components/users-table/address';
+import { OrdersComponent }           from '../../components/users-table/orders';
+import { TotalComponent }            from '../../components/users-table/total';
+import { EmailComponent }            from '../../components/users-table/email';
+import { ImageUserComponent }        from '../../components/users-table/image';
+import { OrdersService }             from '../../services/orders.service';
+import { WarehouseOrdersService }    from '../../services/warehouse-orders.service';
 
 const routes: Routes = [
 	{
-		path: '',
+		path:      '',
 		component: CustomersPage,
 	},
 ];
 
 @NgModule({
-	          declarations: [
+	          declarations:    [
 		          CustomersPage,
 		          ImageUserComponent,
 		          AddressComponent,
@@ -35,7 +35,7 @@ const routes: Routes = [
 		          TotalComponent,
 		          EmailComponent,
 	          ],
-	          imports: [
+	          imports:         [
 		          PipesModule,
 		          ComponentsModule,
 		          IonicModule,
@@ -54,6 +54,6 @@ const routes: Routes = [
 		          TotalComponent,
 		          EmailComponent,
 	          ],
-	          providers: [WarehouseUsersService, OrdersService, WarehouseOrdersService],
+	          providers:       [OrdersService, WarehouseCustomersService, WarehouseOrdersService],
           })
 export class CustomersPageModule {}
