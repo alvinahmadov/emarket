9b3cd07bacd4fdb5bbd41254376506cf65f450b2
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 03:57:03 +0400

    Add order comment component

---
+++ packages/admin-web-angular/src/app/@shared/warehouse/+warehouse-order-modal/make-order-comment.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ViewCell }                               from 'ng2-smart-table';
+
+@Component({
+	           styles:   [
+		           `
+                       .order-comment-wrapper textarea {
+                           width: 100%;
+                       }
+		           `,
+	           ],
+	           template: `
+		                     <div class="order-comment-wrapper">
+			                     <textarea (blur)="setComment($event)"></textarea>
+		                     </div>
+	                     `,
+           })
+export class MakeOrderCommentComponent implements ViewCell
+{
+	@Input()
+	value;
+	
+	@Input()
+	rowData: any;
+	
+	@Output()
+	comment = new EventEmitter<string>();
+	
+	public get productId(): string
+	{
+		return this.value.productId;
+	}
+	
+	public setComment(e)
+	{
+		this.comment.emit(e.target.value);
+	}
+}
