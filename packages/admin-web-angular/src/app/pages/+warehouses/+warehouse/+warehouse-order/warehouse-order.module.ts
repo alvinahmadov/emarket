806dc00dd7a0f75c4fa5d46f997d0bde32d05873
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 06:31:29 +0400

    Refactor imports

--- packages/admin-web-angular/src/app/pages/+warehouses/+warehouse/+warehouse-order/warehouse-order.module.ts
+++ packages/admin-web-angular/src/app/pages/+warehouses/+warehouse/+warehouse-order/warehouse-order.module.ts
@@ -1,19 +1,21 @@
+// noinspection AngularInvalidImportedOrDeclaredSymbol
+
 import { NgModule }                          from '@angular/core';
 import { CommonModule }                      from '@angular/common';
-import { FormWizardModule }                  from '@ever-co/angular2-wizard';
-import { Ng2SmartTableModule }               from 'ng2-smart-table';
-import { UserFormsModule }                   from '../../../../@shared/user/forms';
-import { LocationFormModule }                from '../../../../@shared/forms/location';
-import { GoogleMapModule }                   from '../../../../@shared/forms/google-map/google-map.module';
-import { WarehouseOrderComponent }           from './warehouse-order.component';
-import { WarehouseOrderCreateUserComponent } from './create-user/warehouse-order-create-user.component';
-import { WarehouseOrderModalModule }         from '../../../../@shared/warehouse/+warehouse-order-modal/warehouse-order-modal.module';
 import { TranslateModule }                   from '@ngx-translate/core';
 import { NbSpinnerModule }                   from '@nebular/theme';
-import { ThemeModule }                       from '../../../../@theme';
+import { FormWizardModule }                  from '@ever-co/angular2-wizard';
+import { Ng2SmartTableModule }               from 'ng2-smart-table';
+import { UserFormsModule }                   from '@app/@shared/user/forms';
+import { LocationFormModule }                from '@app/@shared/forms/location';
+import { GoogleMapModule }                   from '@app/@shared/forms/google-map/google-map.module';
+import { WarehouseOrderModalModule }         from '@app/@shared/warehouse/+warehouse-order-modal/warehouse-order-modal.module';
+import { ThemeModule }                       from '@app/@theme';
+import { WarehouseOrderComponent }           from './warehouse-order.component';
+import { WarehouseOrderCreateUserComponent } from './create-user/warehouse-order-create-user.component';
 
 @NgModule({
-	          imports: [
+	          imports:         [
 		          CommonModule,
 		          ThemeModule,
 		          FormWizardModule,
@@ -25,7 +27,7 @@ import { ThemeModule }                       from '../../../../@theme';
 		          WarehouseOrderModalModule,
 		          NbSpinnerModule,
 	          ],
-	          declarations: [WarehouseOrderComponent, WarehouseOrderCreateUserComponent],
+	          declarations:    [WarehouseOrderComponent, WarehouseOrderCreateUserComponent],
 	          entryComponents: [WarehouseOrderComponent],
           })
 export class WarehouseOrderModule {}
