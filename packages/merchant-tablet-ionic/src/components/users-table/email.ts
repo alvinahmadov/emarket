664f924531c6a4a7eac4389067ee49b03dd10c7c
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 06:38:00 +0400

    User to Customer Reformat

--- packages/merchant-tablet-ionic/src/components/users-table/email.ts
+++ packages/merchant-tablet-ionic/src/components/users-table/email.ts
@@ -1,42 +1,43 @@
 import { Component, OnInit }      from '@angular/core';
 import { ViewCell }               from 'ng2-smart-table';
-import User                       from '@modules/server.common/entities/User';
 import { ModalController }        from '@ionic/angular';
+import Customer                   from '@modules/server.common/entities/Customer';
 import { CustomerEmailPopupPage } from 'pages/+customers/customer-email-popup/customer-email-popup';
 
 @Component({
 	           template: `
-		<div class="text-center">
-			<ion-icon
-				*ngIf="user.email"
-				name="mail"
-				class="mail-icon icon icon-md ion-md-mail"
-				(click)="presentCustomerEmailPopup(user)"
-			>
-			</ion-icon>
-		</div>
-	`,
+		                     <div class="text-center">
+			                     <ion-icon
+					                     *ngIf="customer.email"
+					                     name="mail"
+					                     class="mail-icon icon icon-md ion-md-mail"
+					                     (click)="presentCustomerEmailPopup(customer)"
+			                     >
+			                     </ion-icon>
+		                     </div>
+	                     `,
            })
 export class EmailComponent implements ViewCell, OnInit
 {
-	value: string | number;
-	rowData: any;
-	user: User;
+	public value: string | number;
+	public rowData: any;
+	public customer: Customer;
 	
 	constructor(public modalCtrl: ModalController) {}
 	
-	ngOnInit(): void
+	public ngOnInit(): void
 	{
-		this.user = this.rowData.user;
+		this.customer = this.rowData.customer;
 	}
 	
-	async presentCustomerEmailPopup(user: User)
+	public async presentCustomerEmailPopup(customer: Customer)
 	{
-		const modal = await this.modalCtrl.create({
-			                                          component: CustomerEmailPopupPage,
-			                                          componentProps: { user },
-			                                          cssClass: 'customer-email',
-		                                          });
+		const modal = await this.modalCtrl
+		                        .create({
+			                                component:      CustomerEmailPopupPage,
+			                                componentProps: { customer },
+			                                cssClass:       'customer-email',
+		                                });
 		await modal.present();
 	}
 }
