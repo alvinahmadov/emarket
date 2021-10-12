2b0e3ff05da606528f1bf3459d9313473b6244ad
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 07:36:58 +0400

    Reformat Refactor: Make static function

--- packages/merchant-tablet-ionic/src/components/order/make-order/+warehouse-order-modal/warehouse-order-modal.component.ts
+++ packages/merchant-tablet-ionic/src/components/order/make-order/+warehouse-order-modal/warehouse-order-modal.component.ts
@@ -1,44 +1,37 @@
-import {
-	Component,
-	EventEmitter,
-	Input,
-	OnDestroy,
-	OnInit,
-	Output,
-}                          from '@angular/core';
-import { LocalDataSource } from 'ng2-smart-table';
-import { Subject }         from 'rxjs';
-import { takeUntil }       from 'rxjs/operators';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, } from '@angular/core';
+import { LocalDataSource }                                            from 'ng2-smart-table';
+import { Subject }                                                    from 'rxjs';
+import { takeUntil }                                                  from 'rxjs/operators';
+import { TranslateService }                                           from '@ngx-translate/core';
 
-import { WarehouseProductsRouter }      from '@modules/client.common.angular2/routers/warehouse-products-router.service';
-import WarehouseProduct                 from '@modules/server.common/entities/WarehouseProduct';
-import { WarehouseOrderInputComponent } from './warehouse-order-input.component';
-import { IOrderCreateInputProduct }     from '@modules/server.common/routers/IWarehouseOrdersRouter';
-import { TranslateService }             from '@ngx-translate/core';
-import { ProductLocalesService }        from '@modules/client.common.angular2/locale/product-locales.service';
 import { ILocaleMember }                from '@modules/server.common/interfaces/ILocale';
+import WarehouseProduct                 from '@modules/server.common/entities/WarehouseProduct';
+import { IOrderCreateInputProduct }     from '@modules/server.common/routers/IWarehouseOrdersRouter';
+import { ProductLocalesService }        from '@modules/client.common.angular2/locale/product-locales.service';
+import { WarehouseProductsRouter }      from '@modules/client.common.angular2/routers/warehouse-products-router.service';
+import { WarehouseOrderInputComponent } from './warehouse-order-input.component';
 
 @Component({
-	           selector: 'ea-warehouse-order-modal',
-	           styleUrls: ['./warehouse-order-modal.component.scss'],
+	           selector:    'ea-warehouse-order-modal',
+	           styleUrls:   ['./warehouse-order-modal.component.scss'],
 	           templateUrl: './warehouse-order-modal.component.html',
            })
 export class WarehouseOrderModalComponent implements OnInit, OnDestroy
 {
 	@Input()
-	warehouseId: string;
+	public warehouseId: string;
 	
 	@Input()
-	showOrderAction: boolean = true;
+	public showOrderAction: boolean = true;
 	
 	@Output()
-	makeOrderEmitter = new EventEmitter<IOrderCreateInputProduct[]>();
+	public makeOrderEmitter = new EventEmitter<IOrderCreateInputProduct[]>();
 	
 	@Output()
-	isOrderAllowedEmitter = new EventEmitter<boolean>();
+	public isOrderAllowedEmitter = new EventEmitter<boolean>();
 	
-	settingsSmartTable: object;
-	sourceSmartTable = new LocalDataSource();
+	public settingsSmartTable: object;
+	public sourceSmartTable = new LocalDataSource();
 	
 	private _warehouseProducts: WarehouseProduct[] = [];
 	
@@ -55,52 +48,52 @@ export class WarehouseOrderModalComponent implements OnInit, OnDestroy
 	)
 	{}
 	
-	get TRANSLATE_PREFIXES()
-	{
-		const basePrefix = 'SHARED.WAREHOUSE.ORDER_MODAL';
-		const smartTableTitlesPrefix = 'SMART_TABLE.TITLES';
-		
-		return {
-			MAKE_ORDER: `${basePrefix}.MAKE_ORDER`,
-			ONLY_AVAILABLE: `${basePrefix}.ONLY_AVAILABLE`,
-			ORDER: `${basePrefix}.ORDER`,
-			SMART_TABLE: {
-				TITLES: {
-					IMG: `${basePrefix}.${smartTableTitlesPrefix}.IMG`,
-					PRODUCT: `${basePrefix}.${smartTableTitlesPrefix}.PRODUCT`,
-					PRICE: `${basePrefix}.${smartTableTitlesPrefix}.PRICE`,
-					AVAILABLE: `${basePrefix}.${smartTableTitlesPrefix}.AVAILABLE`,
-					AMOUNT: `${basePrefix}.${smartTableTitlesPrefix}.AMOUNT`,
-				},
-			},
-		};
-	}
-	
-	get canOrder(): boolean
-	{
-		return this._orderProducts.some((product) => product.count > 0);
-	}
-	
-	ngOnInit()
+	public ngOnInit()
 	{
 		this._loadSettingsSmartTable();
 		this._loadWarehouseProducts();
 	}
 	
-	ngOnDestroy()
+	public ngOnDestroy()
 	{
 		this._ngDestroy$.next();
 		this._ngDestroy$.complete();
 	}
 	
-	makeOrder()
+	public get TRANSLATE_PREFIXES(): any
+	{
+		const basePrefix = 'SHARED.WAREHOUSE.ORDER_MODAL';
+		const smartTableTitlesPrefix = 'SMART_TABLE.TITLES';
+		
+		return {
+			MAKE_ORDER:     `${basePrefix}.MAKE_ORDER`,
+			ONLY_AVAILABLE: `${basePrefix}.ONLY_AVAILABLE`,
+			ORDER:          `${basePrefix}.ORDER`,
+			SMART_TABLE:    {
+				TITLES: {
+					IMG:       `${basePrefix}.${smartTableTitlesPrefix}.IMG`,
+					PRODUCT:   `${basePrefix}.${smartTableTitlesPrefix}.PRODUCT`,
+					PRICE:     `${basePrefix}.${smartTableTitlesPrefix}.PRICE`,
+					AVAILABLE: `${basePrefix}.${smartTableTitlesPrefix}.AVAILABLE`,
+					AMOUNT:    `${basePrefix}.${smartTableTitlesPrefix}.AMOUNT`,
+				},
+			},
+		};
+	}
+	
+	public get canOrder(): boolean
+	{
+		return this._orderProducts.some((product) => product.count > 0);
+	}
+	
+	public makeOrder()
 	{
 		this.makeOrderEmitter.emit(
 				this._orderProducts.filter(({ count }) => count > 0)
 		);
 	}
 	
-	toggleAvalableProducts()
+	public toggleAvalableProducts()
 	{
 		if(this._clearAvailableProductsFilter)
 		{
@@ -110,15 +103,15 @@ export class WarehouseOrderModalComponent implements OnInit, OnDestroy
 		{
 			this.sourceSmartTable.setFilter([
 				                                {
-					                                field: 'available',
+					                                field:  'available',
 					                                search: '0',
 					                                filter: (element, valueToCompare) =>
-					                                {
-						                                const regex = /<div class="badge badge-pill badge-secondary">([0-9]+)<\/div>/gm;
-						                                const productCount = +regex.exec(element)[1];
+					                                        {
+						                                        const regex = /<div class="badge badge-pill badge-secondary">([0-9]+)<\/div>/gm;
+						                                        const productCount = +regex.exec(element)[1];
 						
-						                                return productCount > +valueToCompare;
-					                                },
+						                                        return productCount > +valueToCompare;
+					                                        },
 				                                },
 			                                ]);
 		}
@@ -146,7 +139,7 @@ export class WarehouseOrderModalComponent implements OnInit, OnDestroy
 				{
 					return {
 						productId: wp.productId,
-						count: 0,
+						count:     0,
 					};
 				}
 		);
@@ -155,26 +148,26 @@ export class WarehouseOrderModalComponent implements OnInit, OnDestroy
 				(wp: WarehouseProduct) =>
 				{
 					return {
-						img: `
-						<img src="${this._getTranslate(wp.product['images'])}" height="68px"/>
+						img:       `
+						<img src="${this._getTranslate(wp.product['images'])}" height="68px" alt=""/>
 					`,
-						product: `
+						product:   `
 						<span class="float-left">${this._getTranslate(wp.product['title'])}</span>
 					`,
-						price: `${wp.price}$`,
+						price:     `${wp.price}$`,
 						available: `
 						<div class="badge badge-pill badge-secondary">${wp.count}</div>
 					`,
-						amount: { productId: wp.productId, available: wp.count },
+						amount:    { productId: wp.productId, available: wp.count },
 					};
 				}
 		);
 		
 		this.sourceSmartTable.setSort([
 			                              {
-				                              field: 'available',
+				                              field:     'available',
 				                              direction: 'desc',
-				                              compare: this._compareByAvailableProducts,
+				                              compare:   WarehouseOrderModalComponent._compareByAvailableProducts,
 			                              },
 		                              ]);
 		this.sourceSmartTable.load(productsData);
@@ -185,7 +178,16 @@ export class WarehouseOrderModalComponent implements OnInit, OnDestroy
 		return this._productLocaleService.getTranslate(members);
 	}
 	
-	private _compareByAvailableProducts(_, first, second)
+	private _translate(key: string): string
+	{
+		let translationResult = '';
+		
+		this._translateService.get(key).subscribe((res) => translationResult = res);
+		
+		return translationResult;
+	}
+	
+	private static _compareByAvailableProducts(_, first, second)
 	{
 		const regex = /<div class="badge badge-pill badge-secondary">([0-9]+)<\/div>/gm;
 		
@@ -193,19 +195,9 @@ export class WarehouseOrderModalComponent implements OnInit, OnDestroy
 		regex.lastIndex = 0; // to reset the regex
 		const matchSecond = +regex.exec(second)[1];
 		
-		return _ > 0 ? matchFirst - matchSecond : matchSecond - matchFirst;
-	}
-	
-	private _translate(key: string): string
-	{
-		let translationResult = '';
-		
-		this._translateService.get(key).subscribe((res) =>
-		                                          {
-			                                          translationResult = res;
-		                                          });
-		
-		return translationResult;
+		return _ > 0
+		       ? matchFirst - matchSecond
+		       : matchSecond - matchFirst;
 	}
 	
 	private _loadSettingsSmartTable()
@@ -228,63 +220,63 @@ export class WarehouseOrderModalComponent implements OnInit, OnDestroy
 		
 		this.settingsSmartTable = {
 			actions: false,
-			pager: { perPage: 5 },
+			pager:   { perPage: 5 },
 			columns: {
-				img: {
-					title: img,
+				img:       {
+					title:  img,
 					filter: false,
-					type: 'html',
-					width: '50px',
+					type:   'html',
+					width:  '50px',
 				},
-				product: {
+				product:   {
 					title: product,
-					type: 'html',
+					type:  'html',
 				},
-				price: {
-					title: price,
-					filter: false,
+				price:     {
+					title:           price,
+					filter:          false,
 					compareFunction: (_, first, second) =>
-					{
-						const matchFirst = +first.replace('$', '');
-						const matchSecond = +second.replace('$', '');
-						return _ > 0
-						       ? matchFirst - matchSecond
-						       : matchSecond - matchFirst;
-					},
+					                 {
+						                 const matchFirst = +first.replace('$', '');
+						                 const matchSecond = +second.replace('$', '');
+						                 return _ > 0
+						                        ? matchFirst - matchSecond
+						                        : matchSecond - matchFirst;
+					                 },
 				},
 				available: {
-					title: available,
-					type: 'html',
-					filter: false,
-					compareFunction: this._compareByAvailableProducts,
+					title:           available,
+					type:            'html',
+					filter:          false,
+					compareFunction: WarehouseOrderModalComponent._compareByAvailableProducts,
 				},
-				amount: {
-					title: amount,
-					filter: false,
-					type: 'custom',
-					renderComponent: WarehouseOrderInputComponent,
+				amount:    {
+					title:                   amount,
+					filter:                  false,
+					type:                    'custom',
+					renderComponent:         WarehouseOrderInputComponent,
 					onComponentInitFunction: (
-							childInstance: WarehouseOrderInputComponent
-					) =>
-					{
-						childInstance.amount
-						             .pipe(takeUntil(this._ngDestroy$))
-						             .subscribe((count) =>
-						                        {
-							                        const wProduct = this._orderProducts.find(
-									                        ({ productId }) =>
-											                        productId === childInstance.productId
-							                        );
-							                        wProduct.count = count;
+							                         childInstance: WarehouseOrderInputComponent
+					                         ) =>
+					                         {
+						                         childInstance.amount
+						                                      .pipe(takeUntil(this._ngDestroy$))
+						                                      .subscribe((count) =>
+						                                                 {
+							                                                 const wProduct = this._orderProducts.find(
+									                                                 ({ productId }) =>
+											                                                 productId === childInstance.productId
+							                                                 );
+							                                                 wProduct.count = count;
 							
-							                        if(!this.showOrderAction)
-							                        {
-								                        this.isOrderAllowedEmitter.emit(
-										                        this.canOrder
-								                        );
-							                        }
-						                        });
-					},
+							                                                 if(!this.showOrderAction)
+							                                                 {
+								                                                 this.isOrderAllowedEmitter.emit(
+										                                                 this.canOrder
+								                                                 );
+							                                                 }
+						                                                 });
+					                         },
 				},
 			},
 		};
