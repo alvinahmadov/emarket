73da7a175c8d9ccb26a54235ad55f84c9b5b3304
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 04:45:32 +0400

    Add getLocationByIP

--- packages/common/src/routers/IGeoLocationsRouter.ts
+++ packages/common/src/routers/IGeoLocationsRouter.ts
@@ -1,9 +1,13 @@
+import GeoLocation from '../entities/GeoLocation'
+
 interface IGeoLocationsRouter
 {
 	getAddressByCoordinatesUsingArcGIS(
 			lat: number,
 			lng: number
 	): Promise<any | null>;
+	
+	getLocationByIP(clientIp?: string): Promise<GeoLocation | null>
 }
 
 export default IGeoLocationsRouter;
