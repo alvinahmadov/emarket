4d599bca13b21c0b29a52028b4cea879e2919174
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 08:15:34 +0400

    Reformat and add catcher for dev

--- packages/merchant-tablet-ionic/src/pages/pages.module.ts
+++ packages/merchant-tablet-ionic/src/pages/pages.module.ts
@@ -1,101 +1,124 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { environment }          from 'environments/environment';
 import { AuthModuleGuard }      from './+auth/auth.module.guard';
+import { LoginModuleGuard }     from './+login/login.module.guard';
 import { WarehouseModuleGuard } from './+warehouse/warehouse.module.guard';
 import { InfoModuleGuard }      from './+info/info.module.guard';
 
+function catchModuleErr(err)
+{
+	if(!environment.production)
+	{
+		console.error(err);
+	}
+}
+
 const routes: Routes = [
 	{
-		path: 'auth',
-		loadChildren: () =>
-				import('./+auth/auth.module').then((m) => m.AuthPageModule),
-		canLoad: [AuthModuleGuard],
+		path:         'auth',
+		loadChildren: () => import('./+auth/auth.module')
+				.then((m) => m.AuthPageModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [AuthModuleGuard],
 	},
 	{
-		path: 'warehouse',
-		loadChildren: () =>
-				import('./+warehouse/warehouse.module').then(
-						(m) => m.WarehousePageModule
-				),
-		canLoad: [WarehouseModuleGuard],
+		path:         'login',
+		loadChildren: () => import('./+login/login.module')
+				.then((m) => m.LoginPageModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [LoginModuleGuard],
 	},
 	{
-		path: 'language',
-		loadChildren: () =>
-				import('./+language/language.module').then(
-						(m) => m.LanguagePageModule
-				),
-		canLoad: [WarehouseModuleGuard],
+		path:         'warehouse',
+		loadChildren: () => import('./+warehouse/warehouse.module')
+				.then((m) => m.WarehousePageModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [WarehouseModuleGuard],
 	},
 	{
-		path: 'customers',
-		loadChildren: () =>
-				import('./+customers/customers.module').then(
-						(m) => m.CustomersPageModule
-				),
-		canLoad: [WarehouseModuleGuard],
+		path:         'language',
+		loadChildren: () => import('./+language/language.module')
+				.then((m) => m.LanguagePageModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [WarehouseModuleGuard],
 	},
 	{
-		path: 'carriers',
-		loadChildren: () =>
-				import('./+carriers/carriers.module').then(
-						(m) => m.CarrierssPageModule
-				),
-		canLoad: [WarehouseModuleGuard],
+		path:         'customers',
+		loadChildren: () => import('./+customers/customers.module')
+				.then((m) => m.CustomersPageModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [WarehouseModuleGuard],
 	},
 	{
-		path: 'track',
-		loadChildren: () =>
-				import('./+track/track.module').then((m) => m.TrackPageModule),
-		canLoad: [WarehouseModuleGuard],
+		path:         'carriers',
+		loadChildren: () => import('./+carriers/carriers.module')
+				.then((m) => m.CarrierssPageModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [WarehouseModuleGuard],
 	},
 	{
-		path: 'track/:id',
-		loadChildren: () =>
-				import('./+track/track.module').then((m) => m.TrackPageModule),
-		canLoad: [WarehouseModuleGuard],
+		path:         'chats',
+		loadChildren: () => import('./+chats/chats.module')
+				.then((m) => m.ChatsModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [WarehouseModuleGuard],
 	},
 	{
-		path: 'settings',
-		loadChildren: () =>
-				import('./+settings/settings.module').then(
-						(m) => m.SettingsPageModule
-				),
-		canLoad: [WarehouseModuleGuard],
+		path:         'track',
+		loadChildren: () => import('./+track/track.module')
+				.then((m) => m.TrackPageModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [WarehouseModuleGuard],
 	},
 	{
-		path: 'info',
-		loadChildren: () =>
-				import('./+info/info.module').then((m) => m.InfoModule),
-		canLoad: [InfoModuleGuard],
+		path:         'track/:id',
+		loadChildren: () => import('./+track/track.module')
+				.then((m) => m.TrackPageModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [WarehouseModuleGuard],
 	},
 	{
-		path: 'promotions',
-		loadChildren: () =>
-				import('./+promotions/promotion.module').then(
-						(m) => m.PromotionModule
-				),
-		canLoad: [WarehouseModuleGuard],
+		path:         'settings',
+		loadChildren: () => import('./+settings/settings.module')
+				.then((m) => m.SettingsPageModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [WarehouseModuleGuard],
 	},
 	{
-		path: 'errors',
-		loadChildren: () =>
-				import('./+errors/errors.module').then((m) => m.ErrorsModule),
+		path:         'info',
+		loadChildren: () => import('./+info/info.module')
+				.then((m) => m.InfoModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [InfoModuleGuard],
 	},
 	{
-		path: '',
-		pathMatch: 'full',
-		redirectTo: 'warehouse',
+		path:         'promotions',
+		loadChildren: () => import('./+promotions/promotion.module')
+				.then((m) => m.PromotionModule)
+				.catch(err => catchModuleErr(err)),
+		canLoad:      [WarehouseModuleGuard],
+	},
+	{
+		path:         'errors',
+		loadChildren: () => import('./+errors/errors.module')
+				.then((m) => m.ErrorsModule)
+				.catch(err => catchModuleErr(err)),
+	},
+	{
+		path:       '',
+		pathMatch:  'full',
+		redirectTo: 'auth',
 	},
 ];
 
 @NgModule({
-	          imports: [RouterModule.forChild(routes)],
+	          imports:   [RouterModule.forChild(routes)],
 	          providers: [
 		          AuthModuleGuard,
 		          WarehouseModuleGuard,
 		          InfoModuleGuard
 	          ],
-	          exports: [RouterModule],
+	          exports:   [RouterModule],
           })
 export class PagesModule {}
