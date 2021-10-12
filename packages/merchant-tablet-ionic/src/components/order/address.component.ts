071da84a20cfbd81f9a181a4b15fc5b0be182398
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 07:03:40 +0400

    Refactor: User to Customer

--- packages/merchant-tablet-ionic/src/components/order/address.component.ts
+++ packages/merchant-tablet-ionic/src/components/order/address.component.ts
@@ -1,47 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewCell }          from 'ng2-smart-table';
-import User                  from '@modules/server.common/entities/User';
+import Customer              from '@modules/server.common/entities/Customer';
 
 @Component({
-	           styles: [
+	           styles:   [
 		           `
-			h1 {
-				font-weight: normal !importnat;
-			}
-			.address {
-				font-style: italic !important;
-				text-decoration: underline;
-				display: block !important;
-			}
-		`,
+                       h1 {
+                           font-weight: normal !important;
+                       }
+
+                       .address {
+                           font-style: italic !important;
+                           text-decoration: underline;
+                           display: block !important;
+                       }
+		           `,
 	           ],
 	           template: `
-		<strong
-			>{{ user?.geoLocation.city }}
-			<span *ngIf="user?.geoLocation.postcode"
-				>({{ user?.geoLocation.postcode }})</span
-			>
-		</strong>
-		<span *ngIf="user" class="address">{{
-			getStoreFullAddress(user)
-		}}</span>
-	`,
+		                     <strong>
+			                     {{ customer?.geoLocation.city }}
+			                     <span *ngIf="customer?.geoLocation.postcode">
+				           ({{ customer?.geoLocation.postcode }})
+			           </span>
+		                     </strong>
+		                     <span *ngIf="customer" class="address">
+			           {{ getStoreFullAddress(customer) }}
+		           </span>
+	                     `,
            })
 export class AddressComponent implements ViewCell, OnInit
 {
-	value: string | number;
-	rowData: any;
-	user: User;
+	public value: string | number;
+	public rowData: any;
+	public customer: Customer;
 	
-	ngOnInit(): void
+	public ngOnInit(): void
 	{
-		this.user = this.rowData.user;
+		this.customer = this.rowData.customer;
 	}
 	
-	getStoreFullAddress(user: User)
+	public getStoreFullAddress(customer: Customer): string
 	{
-		const geoLocation = user.geoLocation;
-		const fullAddress = `${geoLocation.city}, ${geoLocation.streetAddress} ${geoLocation.house}`;
-		return fullAddress;
+		const geoLocation = customer.geoLocation;
+		return `${geoLocation.city}, ${geoLocation.streetAddress} ${geoLocation.house}`;
 	}
 }
