b6497ecc190d9c3c939b6ac87370da75d5d8081c
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 08:08:46 +0400

    Reformat

--- packages/merchant-tablet-ionic/src/pages/+info/+terms-of-use/terms-of-use.module.ts
+++ packages/merchant-tablet-ionic/src/pages/+info/+terms-of-use/terms-of-use.module.ts
@@ -1,23 +1,23 @@
 import { NgModule }             from '@angular/core';
-import { PipesModule }          from '@modules/client.common.angular2/pipes/pipes.module';
 import { TranslateModule }      from '@ngx-translate/core';
-import { ComponentsModule }     from 'components/components.module';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule }         from '@angular/common';
 import { FormsModule }          from '@angular/forms';
 import { IonicModule }          from '@ionic/angular';
+import { PipesModule }          from '@modules/client.common.angular2/pipes/pipes.module';
+import { ComponentsModule }     from 'components/components.module';
 import { TermsOfUsePage }       from './terms-of-use';
 
 const routes: Routes = [
 	{
-		path: '',
+		path:      '',
 		component: TermsOfUsePage,
 	},
 ];
 
 @NgModule({
 	          declarations: [TermsOfUsePage],
-	          imports: [
+	          imports:      [
 		          TranslateModule.forChild(),
 		          PipesModule,
 		          ComponentsModule,
