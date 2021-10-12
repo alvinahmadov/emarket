7f564dd21d41ec6d6672388b1be2328e101f2de7
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 07:05:16 +0400

    Refactor: Add order comment Reorder imports

--- packages/merchant-tablet-ionic/src/components/order/order.module.ts
+++ packages/merchant-tablet-ionic/src/components/order/order.module.ts
@@ -1,22 +1,23 @@
+import { CommonModule }                  from '@angular/common';
 import { NgModule }                      from '@angular/core';
-import { ChooseCustomerOptionComponent } from './choose-customer-option.component';
-import { MakeOrderComponent }            from './make-order/make-order.component';
-import { SelectAddCustomerComponent }    from './select-add-customer.component';
-import { OrderComponent }                from './order.component';
-import { Ng2SmartTableModule }           from 'ng2-smart-table';
-import { UsersService }                  from '../../services/users.service';
-import { UserMutationModule }            from '../../@shared/user/mutation/user-mutation.module';
-import { AddressComponent }              from './address.component';
-import { MakeOrderInputComponent }       from './make-order/make-order-input.component';
-import { WarehouseOrdersService }        from '../../services/warehouse-orders.service';
+import { FormsModule }                   from '@angular/forms';
 import { TranslateModule }               from '@ngx-translate/core';
 import { IonicModule }                   from '@ionic/angular';
-import { CommonModule }                  from '@angular/common';
-import { FormsModule }                   from '@angular/forms';
+import { Ng2SmartTableModule }           from 'ng2-smart-table';
+import { AddressComponent }              from './address.component';
+import { ChooseCustomerOptionComponent } from './choose-customer-option.component';
+import { MakeOrderComponent }            from './make-order/make-order.component';
+import { OrderComponent }                from './order.component';
 import { OrderTypeComponent }            from './order-type/order-type.component';
+import { MakeOrderInputComponent }       from './make-order/make-order-input.component';
+import { MakeOrderCommentComponent }     from './make-order/order-comment/make-order-comment.component';
+import { SelectAddCustomerComponent }    from './select-add-customer.component';
+import { UserMutationModule }            from '../../@shared/user/mutation/user-mutation.module';
+import { CustomersService }              from '../../services/customers.service';
+import { WarehouseOrdersService }        from '../../services/warehouse-orders.service';
 
 @NgModule({
-	          imports: [
+	          imports:         [
 		          Ng2SmartTableModule,
 		          IonicModule,
 		          CommonModule,
@@ -24,22 +25,28 @@ import { OrderTypeComponent }            from './order-type/order-type.component
 		          UserMutationModule,
 		          TranslateModule.forChild(),
 	          ],
-	          declarations: [
+	          declarations:    [
 		          OrderComponent,
 		          ChooseCustomerOptionComponent,
 		          SelectAddCustomerComponent,
 		          MakeOrderComponent,
 		          MakeOrderInputComponent,
+		          MakeOrderCommentComponent,
 		          OrderTypeComponent,
 		          AddressComponent,
 	          ],
-	          entryComponents: [AddressComponent, MakeOrderInputComponent],
-	          exports: [
+	          entryComponents: [
+		          AddressComponent,
+		          MakeOrderInputComponent,
+		          MakeOrderCommentComponent
+	          ],
+	          exports:         [
 		          OrderComponent,
 		          ChooseCustomerOptionComponent,
 		          SelectAddCustomerComponent,
 		          MakeOrderComponent,
+		          MakeOrderCommentComponent
 	          ],
-	          providers: [UsersService, WarehouseOrdersService],
+	          providers:       [CustomersService, WarehouseOrdersService],
           })
 export class OrderModule {}
