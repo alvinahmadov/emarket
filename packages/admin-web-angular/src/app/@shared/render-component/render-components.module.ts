d2ace457b147ec76d1891c0f6dbac4de38603486
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 04:09:32 +0400

    Reformat

--- packages/admin-web-angular/src/app/@shared/render-component/render-components.module.ts
+++ packages/admin-web-angular/src/app/@shared/render-component/render-components.module.ts
@@ -1,27 +1,29 @@
 import { NgModule }                         from '@angular/core';
+import { CommonModule }                     from '@angular/common';
+import { TranslateModule }                  from '@ngx-translate/core';
+import { MomentModule }                     from 'ngx-moment';
+import { ThemeModule }                      from '@app/@theme';
 import { PriceCountInputComponent }         from './price-countInput/price-countInput.component';
 import { RedirectIdComponent }              from './redirect-id';
-import { CommonModule }                     from '@angular/common';
-import { ThemeModule }                      from '../../@theme';
 import { RedirectNameComponent }            from './name-redirect/name-redirect.component';
+import { RedirectChatComponent }            from './chat-redirect/chat-redirect.component';
 import { CreatedComponent }                 from './created/created.component';
-import { ProductTitleRedirectComponent }    from './product-title-redirect/product-title-redirect.component';
-import { ProductImageRedirectComponent }    from './product-image-redirect/product-image-redirect.component';
-import { ProductCheckboxComponent }         from './product-checkbox/product-checkbox';
-import { MomentModule }                     from 'ngx-moment';
-import { ProductTitleComponent }            from './product-title/product-title.component';
-import { ProductImageComponent }            from './product-image/product-image.component';
 import { CustomerEmailComponent }           from './customer-email/customer-email.component';
 import { CustomerPhoneComponent }           from './customer-phone/customer-phone.component';
 import { CheckboxComponent }                from './customer-orders-table/checkbox/checkbox.component';
+import { ProductTitleRedirectComponent }    from './product-title-redirect/product-title-redirect.component';
+import { ProductImageRedirectComponent }    from './product-image-redirect/product-image-redirect.component';
+import { ProductCheckboxComponent }         from './product-checkbox/product-checkbox';
+import { ProductTitleComponent }            from './product-title/product-title.component';
+import { ProductImageComponent }            from './product-image/product-image.component';
 import { IsAvailableCheckBox }              from './store-product-is-available-checkbox/is-available-checkbox.component';
 import { ProductTakeawayDeliveryComponent } from './product-takeaway-delivery/product-takeaway-delivery.component';
-import { TranslateModule }                  from '@ngx-translate/core';
 
 const COMPONENTS = [
 	PriceCountInputComponent,
 	RedirectIdComponent,
 	RedirectNameComponent,
+	RedirectChatComponent,
 	CreatedComponent,
 	ProductTitleRedirectComponent,
 	ProductImageRedirectComponent,
@@ -36,13 +38,13 @@ const COMPONENTS = [
 ];
 
 @NgModule({
-	          imports: [
+	          imports:         [
 		          CommonModule,
 		          ThemeModule,
 		          MomentModule,
 		          TranslateModule.forChild(),
 	          ],
-	          declarations: COMPONENTS,
+	          declarations:    COMPONENTS,
 	          entryComponents: COMPONENTS,
           })
 export class RenderComponentsModule {}
