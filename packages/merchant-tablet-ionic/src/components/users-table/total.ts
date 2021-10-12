4804193f703f2421e153d5ce3eb49d236065a594
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 06:40:14 +0400

    User to Customer Reformat

--- packages/merchant-tablet-ionic/src/components/users-table/total.ts
+++ packages/merchant-tablet-ionic/src/components/users-table/total.ts
@@ -1,36 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewCell }          from 'ng2-smart-table';
-import User                  from '@modules/server.common/entities/User';
+import { environment }       from '@modules/client.common.angular2/environments/environment';
+import Customer              from '@modules/server.common/entities/Customer';
 import Order                 from '@modules/server.common/entities/Order';
 
 @Component({
 	           template: `
-		<div class="text-center" *ngIf="rowData?.total !== 0">
-			<span>{{ '$' + rowData?.total }}</span>
-			<div></div>
-		</div>
-	`,
+		                     <div class="text-center"
+		                          *ngIf="rowData?.total !== 0"
+		                     >
+			                     <span>{{ rowData.total }} {{ currencySymbol }}</span>
+			                     <div></div>
+		                     </div>
+	                     `,
            })
 export class TotalComponent implements ViewCell, OnInit
 {
-	value: string | number;
-	rowData: any;
-	user: User;
-	orders: Order[];
+	readonly currencySymbol: string = environment.CURRENCY_SYMBOL;
+	public value: string | number;
+	public rowData: any;
+	public customer: Customer;
+	public orders: Order[];
 	
 	constructor() {}
 	
-	ngOnInit(): void
+	public ngOnInit(): void
 	{
-		this.user = this.rowData.user;
+		this.customer = this.rowData.customer;
 		this.orders = this.rowData.allOrders;
 	}
 	
-	getTotalPrice(userId: string)
+	public getTotalPrice(customerId: string): number
 	{
 		const orders = this.orders
 		                   .filter((o: Order) => o.isPaid)
-		                   .filter((o: Order) => o.user.id === userId);
+		                   .filter((o: Order) => o.customer.id === customerId);
 		let totalPrice = 0;
 		if(orders.length > 0)
 		{
