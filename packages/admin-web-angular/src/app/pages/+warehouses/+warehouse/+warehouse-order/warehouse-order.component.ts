2ebd3e81273ce0090ad1d9f748472ffdc50fa7e4
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 06:30:09 +0400

    Refactor

--- packages/admin-web-angular/src/app/pages/+warehouses/+warehouse/+warehouse-order/warehouse-order.component.ts
+++ packages/admin-web-angular/src/app/pages/+warehouses/+warehouse/+warehouse-order/warehouse-order.component.ts
@@ -4,50 +4,52 @@ import {
 	OnInit,
 	OnDestroy,
 	EventEmitter,
-} from '@angular/core';
-
+}                                            from '@angular/core';
 import { ActivatedRoute }                    from '@angular/router';
-import { ToasterService }                    from 'angular2-toaster';
+import { TranslateService }                  from '@ngx-translate/core';
+import { NgbActiveModal }                    from '@ng-bootstrap/ng-bootstrap';
 import { WizardComponent }                   from '@ever-co/angular2-wizard';
+import { ToasterService }                    from 'angular2-toaster';
 import { LocalDataSource }                   from 'ng2-smart-table';
 import { Subject }                           from 'rxjs';
 import { takeUntil, finalize }               from 'rxjs/operators';
-import { UsersService }                      from '../../../../@core/data/users.service';
-import { WarehouseOrdersService }            from '../../../../@core/data/warehouseOrders.service';
-import User                                  from '@modules/server.common/entities/User';
+import Customer                              from '@modules/server.common/entities/Customer';
 import { IOrderCreateInputProduct }          from '@modules/server.common/routers/IWarehouseOrdersRouter';
+import { CustomersService }                  from '@app/@core/data/customers.service';
+import { WarehouseOrdersService }            from '@app/@core/data/warehouseOrders.service';
+import { WarehouseOrderModalComponent }      from '@app/@shared/warehouse/+warehouse-order-modal/warehouse-order-modal.component';
 import { WarehouseOrderCreateUserComponent } from './create-user/warehouse-order-create-user.component';
-import { WarehouseOrderModalComponent }      from '../../../../@shared/warehouse/+warehouse-order-modal/warehouse-order-modal.component';
-import { TranslateService }                  from '@ngx-translate/core';
-import { NgbActiveModal }                    from '@ng-bootstrap/ng-bootstrap';
 
 const perPage = 5;
 
 @Component({
-	           styleUrls: ['./warehouse-order.component.scss'],
+	           styleUrls:   ['./warehouse-order.component.scss'],
 	           templateUrl: './warehouse-order.component.html',
            })
 export class WarehouseOrderComponent implements OnInit, OnDestroy
 {
-	loading: boolean;
+	public loading: boolean;
 	
-	warehouseId: string;
-	titleSelectAdd: string = this._translate(
+	public warehouseId: string;
+	public titleSelectAdd: string = this._translate(
 			this.TRANSLATE_PREFIXES.STEP2.TITLE.SELECT_ADD
 	);
-	isSelectedFromExisting: boolean;
-	showNextButton: boolean = false;
-	isOrderAllowed: boolean = false;
+	public isSelectedFromExisting: boolean;
+	public showNextButton: boolean = false;
+	public isOrderAllowed: boolean = false;
 	
-	settingsSmartTable: object;
-	sourceSmartTable = new LocalDataSource();
+	public settingsSmartTable: object;
+	public sourceSmartTable = new LocalDataSource();
 	
-	createUserEmitter = new EventEmitter<void>();
-	orderFinishedEmitter = new EventEmitter<void>();
+	public createUserEmitter = new EventEmitter<void>();
+	public orderFinishedEmitter = new EventEmitter<void>();
+	
 	@ViewChild('wizardFormStepTwo')
 	public wizardFormStepTwo: WizardComponent;
+	
 	@ViewChild('wizardForm', { static: true })
 	private _wizardForm: WizardComponent;
+	
 	@ViewChild('warehouseOrderCreateUser')
 	private _warehouseOrderCreateUser: WarehouseOrderCreateUserComponent;
 	
@@ -55,7 +57,7 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 	private _warehouseOrderModal: WarehouseOrderModalComponent;
 	
 	private _userIdToOrder: string;
-	private userSelected: any;
+	private customerSelected: any;
 	private _dataIsLoaded: boolean = false;
 	
 	private _noInfoSign = '';
@@ -69,15 +71,15 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 			private readonly _activatedRoute: ActivatedRoute,
 			private readonly _toasterService: ToasterService,
 			private readonly _translateService: TranslateService,
-			private readonly _usersService: UsersService,
+			private readonly _usersService: CustomersService,
 			private readonly _warehouseOrdersService: WarehouseOrdersService
 	)
 	{}
 	
-	get TRANSLATE_PREFIXES()
+	public get TRANSLATE_PREFIXES(): any
 	{
 		const wizardFormPrefix =
-				'WAREHOUSE_VIEW.CREATE_ORDER_MODAL.WIZARD_FORM';
+				      'WAREHOUSE_VIEW.CREATE_ORDER_MODAL.WIZARD_FORM';
 		const step1Prefix = 'STEP1';
 		const step2Prefix = 'STEP2';
 		const step3Prefix = 'STEP3';
@@ -86,33 +88,33 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 			BUTTON_NEXT: `${wizardFormPrefix}.BUTTON_NEXT`,
 			BUTTON_PREV: `${wizardFormPrefix}.BUTTON_PREV`,
 			BUTTON_DONE: `${wizardFormPrefix}.BUTTON_DONE`,
-			STEP1: {
-				TITLE: `${wizardFormPrefix}.${step1Prefix}.TITLE`,
+			STEP1:       {
+				TITLE:                `${wizardFormPrefix}.${step1Prefix}.TITLE`,
 				SELECT_FROM_EXISTING: `${wizardFormPrefix}.${step1Prefix}.SELECT_FROM_EXISTING`,
-				ADD_NEW_CUSTOMER: `${wizardFormPrefix}.${step1Prefix}.ADD_NEW_CUSTOMER`,
+				ADD_NEW_CUSTOMER:     `${wizardFormPrefix}.${step1Prefix}.ADD_NEW_CUSTOMER`,
 			},
-			STEP2: {
-				TITLE: {
-					SELECT_ADD: `${wizardFormPrefix}.${step2Prefix}.TITLE.SELECT_ADD`,
+			STEP2:       {
+				TITLE:       {
+					SELECT_ADD:      `${wizardFormPrefix}.${step2Prefix}.TITLE.SELECT_ADD`,
 					SELECT_CUSTOMER: `${wizardFormPrefix}.${step2Prefix}.TITLE.SELECT_CUSTOMER`,
-					ADD_NEW: `${wizardFormPrefix}.${step2Prefix}.TITLE.ADD_NEW`,
+					ADD_NEW:         `${wizardFormPrefix}.${step2Prefix}.TITLE.ADD_NEW`,
 				},
 				SMART_TABLE: {
 					TITLES: {
 						FULL_NAME: `${wizardFormPrefix}.${step2Prefix}.SMART_TABLE.TITLES.FULL_NAME`,
-						EMAIL: `${wizardFormPrefix}.${step2Prefix}.SMART_TABLE.TITLES.EMAIL`,
-						PHONE: `${wizardFormPrefix}.${step2Prefix}.SMART_TABLE.TITLES.PHONE`,
-						ADDRESS: `${wizardFormPrefix}.${step2Prefix}.SMART_TABLE.TITLES.ADDRESS`,
+						EMAIL:     `${wizardFormPrefix}.${step2Prefix}.SMART_TABLE.TITLES.EMAIL`,
+						PHONE:     `${wizardFormPrefix}.${step2Prefix}.SMART_TABLE.TITLES.PHONE`,
+						ADDRESS:   `${wizardFormPrefix}.${step2Prefix}.SMART_TABLE.TITLES.ADDRESS`,
 					},
 				},
 			},
-			STEP3: {
+			STEP3:       {
 				TITLE: `${wizardFormPrefix}.${step3Prefix}.TITLE`,
 			},
 		};
 	}
 	
-	get isCreateOrderWizardAllowed()
+	public get isCreateOrderWizardAllowed(): boolean
 	{
 		return (
 				this.isSelectedFromExisting ||
@@ -121,17 +123,17 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 		);
 	}
 	
-	get buttonDone()
+	public get buttonDone(): string
 	{
 		return this._translate(this.TRANSLATE_PREFIXES.BUTTON_DONE);
 	}
 	
-	get buttonNext()
+	public get buttonNext(): string
 	{
 		return this._translate(this.TRANSLATE_PREFIXES.BUTTON_NEXT);
 	}
 	
-	get buttonPrevious()
+	public get buttonPrevious(): string
 	{
 		return this._translate(this.TRANSLATE_PREFIXES.BUTTON_PREV);
 	}
@@ -141,26 +143,32 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 		return !this.isSelectedFromExisting;
 	}
 	
-	ngOnInit()
+	public ngOnInit()
 	{
 		this._listenStepWizard();
 		this._getWarehouseIdFromRoute();
 		this.smartTableChange();
 	}
 	
-	onIsOrderAllowed(orderAllowed: boolean)
+	public ngOnDestroy()
+	{
+		this._ngDestroy$.next();
+		this._ngDestroy$.complete();
+	}
+	
+	public onIsOrderAllowed(orderAllowed: boolean)
 	{
 		this.isOrderAllowed = orderAllowed;
 	}
 	
-	onMakeOrder(orderProducts: IOrderCreateInputProduct[])
+	public onMakeOrder(orderProducts: IOrderCreateInputProduct[])
 	{
 		this.loading = true;
 		this._warehouseOrdersService
 		    .createOrder({
-			                 userId: this._userIdToOrder,
+			                 customerId:  this._userIdToOrder,
 			                 warehouseId: this.warehouseId,
-			                 products: orderProducts,
+			                 products:    orderProducts,
 		                 })
 		    .pipe(takeUntil(this._ngDestroy$))
 		    .pipe(finalize(() => this.orderFinishedEmitter.emit()))
@@ -170,7 +178,7 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 					    this.loading = false;
 					    this._toasterService.pop(
 							    `success`,
-							    `User ${this.userSelected.name} finish the order`
+							    `User ${this.customerSelected.name} finish the order`
 					    );
 				    },
 				    () =>
@@ -184,23 +192,23 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 		    );
 	}
 	
-	onNewUser(userData)
+	public onNewUser(userData)
 	{
 		userData.name =
 				(userData.firstName ? userData.firstName : '') +
 				' ' +
 				(userData.lastName ? userData.lastName : '');
-		this.userSelected = userData;
+		this.customerSelected = userData;
 		
 		this._userIdToOrder = userData.id;
 	}
 	
-	onNextStep()
+	public onNextStep()
 	{
 		this._wizardForm.next();
 	}
 	
-	selectExistingCustomer()
+	public selectExistingCustomer()
 	{
 		this.titleSelectAdd = this._translate(
 				this.TRANSLATE_PREFIXES.STEP2.TITLE.SELECT_CUSTOMER
@@ -213,7 +221,7 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 		this.onNextStep();
 	}
 	
-	addNewCustomer()
+	public addNewCustomer()
 	{
 		this.titleSelectAdd = this._translate(
 				this.TRANSLATE_PREFIXES.STEP2.TITLE.ADD_NEW
@@ -225,27 +233,21 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 		this.onNextStep();
 	}
 	
-	selectFromExisting(ev)
+	public selectFromExisting(ev)
 	{
-		this.userSelected = ev.data;
+		this.customerSelected = ev.data;
 		this._userIdToOrder = ev.data.id;
 		this._wizardForm.next();
 	}
 	
-	completeOrder()
+	public completeOrder()
 	{
 		this._warehouseOrderModal.makeOrder();
 	}
 	
-	cancel()
+	public cancel()
 	{
 		this.activeModal.dismiss('canceled');
-	}
-	
-	ngOnDestroy()
-	{
-		this._ngDestroy$.next();
-		this._ngDestroy$.complete();
 	}
 	
 	private _translate(key: string): string
@@ -309,12 +311,12 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 		this.settingsSmartTable = {
 			actions: false,
 			columns: {
-				name: { title: fullName },
-				email: { title: email },
-				phone: { title: phone },
+				name:    { title: fullName },
+				email:   { title: email },
+				phone:   { title: phone },
 				address: { title: address },
 			},
-			pager: {
+			pager:   {
 				display: true,
 				perPage,
 			},
@@ -328,12 +330,12 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 			await this.$users.unsubscribe();
 		}
 		this.$users = this._usersService
-		                  .getUsers({
-			                            skip: perPage * (page - 1),
-			                            limit: perPage,
-		                            })
+		                  .getCustomers({
+			                                skip:  perPage * (page - 1),
+			                                limit: perPage,
+		                                })
 		                  .pipe(takeUntil(this._ngDestroy$))
-		                  .subscribe(async(users: User[]) =>
+		                  .subscribe(async(users: Customer[]) =>
 		                             {
 			                             await this.loadDataCount();
 			                             const formattedData = this._setupDataForSmartTable(users);
@@ -349,17 +351,17 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 		                             });
 	}
 	
-	private _setupDataForSmartTable(users: User[])
+	private _setupDataForSmartTable(users: Customer[])
 	{
-		return users.map((user: User) =>
+		return users.map((user: Customer) =>
 		                 {
 			                 return {
-				                 id: user.id,
-				                 name: `
+				                 id:      user.id,
+				                 name:    `
 					${user.firstName || this._noInfoSign} ${user.lastName || this._noInfoSign}
 				`,
-				                 email: user.email || this._noInfoSign,
-				                 phone: user.phone || this._noInfoSign,
+				                 email:   user.email || this._noInfoSign,
+				                 phone:   user.phone || this._noInfoSign,
 				                 address: user.fullAddress || this._noInfoSign,
 			                 };
 		                 });
@@ -392,6 +394,6 @@ export class WarehouseOrderComponent implements OnInit, OnDestroy
 	
 	private async loadDataCount()
 	{
-		this.dataCount = await this._usersService.getCountOfUsers();
+		this.dataCount = await this._usersService.getCountOfCustomers();
 	}
 }
