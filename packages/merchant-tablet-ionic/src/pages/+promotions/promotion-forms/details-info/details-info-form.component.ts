12029e92c5c9ad30170042bb40642107ee116b07
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-12 07:58:02 +0400

    Reformat

--- packages/merchant-tablet-ionic/src/pages/+promotions/promotion-forms/details-info/details-info-form.component.ts
+++ packages/merchant-tablet-ionic/src/pages/+promotions/promotion-forms/details-info/details-info-form.component.ts
@@ -6,71 +6,89 @@ import {
 }                                             from '@modules/server.common/interfaces/IPromotion';
 
 @Component({
-	           selector: 'details-info-form',
-	           styleUrls: ['./details-info-form.component.scss'],
-	           templateUrl: 'details-info-form.component.html',
+	           selector:    'details-info-form',
+	           styleUrls:   ['./details-info-form.component.scss'],
+	           templateUrl: './details-info-form.component.html',
            })
 export class DetailsInfoFormComponent implements OnInit
 {
 	@Input()
-	readonly form: FormGroup;
+	public readonly form: FormGroup;
 	
 	@Input()
-	promotionData: IPromotion;
+	public promotionData: IPromotion;
 	
-	promotionDetails: Partial<IPromotion>;
+	public promotionDetails: Partial<IPromotion>;
 	
 	constructor() {}
 	
-	ngOnInit(): void
+	public ngOnInit(): void
 	{
+		if(!this.promotionData)
+		{
+			this.promotionData = {
+				_id:            '',
+				_createdAt:     undefined,
+				_updatedAt:     undefined,
+				description:    [],
+				active:         false,
+				activeFrom:     new Date(),
+				activeTo:       undefined,
+				image:          "",
+				product:        undefined,
+				promoPrice:     0,
+				purchasesCount: 0,
+				title:          [],
+				warehouse:      undefined,
+			};
+		}
 		this.promotionDetails =
-				this.promotionData || this._initPromotionDetails();
+				this.promotionData || DetailsInfoFormComponent._initPromotionDetails();
 		
 		this.setValue();
 	}
 	
-	get image()
+	public get image()
 	{
 		return this.form.get('image');
 	}
 	
-	static buildForm(formBuilder: FormBuilder): FormGroup
+	public static buildForm(formBuilder: FormBuilder): FormGroup
 	{
 		return formBuilder.group({
-			                         image: [''],
-			                         active: [true],
+			                         image:          [''],
+			                         active:         [true],
 			                         purchasesCount: [0, Validators.min(0)],
-			                         promoPrice: [0, Validators.min(0)],
+			                         promoPrice:     [0, Validators.min(0)],
 		                         });
 	}
 	
-	getValue()
+	public getValue()
 	{
 		return this.form.value as IPromotionCreateObject;
 	}
 	
-	setValue()
+	public setValue()
 	{
 		if(!this.promotionDetails) return;
 		
 		const promotionFormValue = {
-			image: this.promotionDetails.image || '',
-			active: this.promotionDetails.active || true,
+			image:          this.promotionDetails.image || '',
+			active:         this.promotionDetails.active || true,
 			purchasesCount: this.promotionDetails.purchasesCount || 0,
-			promoPrice: this.promotionData.promoPrice || 0,
+			promoPrice:     this.promotionData.promoPrice || 0,
 		};
 		
 		this.form.patchValue(promotionFormValue);
 	}
 	
-	private _initPromotionDetails()
+	private static _initPromotionDetails(): Partial<IPromotion>
 	{
 		return {
-			image: null,
-			active: true,
+			image:          null,
+			active:         true,
 			purchasesCount: 0,
-			promoPrice: 0,
+			promoPrice:     0,
 		};
 	}
 }
