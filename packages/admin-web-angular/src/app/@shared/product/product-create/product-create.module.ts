fc589d90abafc930ceaa59321a46ac0038d8091b
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-13 20:03:27 +0400

    Disable warning Reformat

--- packages/admin-web-angular/src/app/@shared/product/product-create/product-create.module.ts
+++ packages/admin-web-angular/src/app/@shared/product/product-create/product-create.module.ts
@@ -1,22 +1,22 @@
 import { NgModule }               from '@angular/core';
-import { ThemeModule }            from '../../../@theme/theme.module';
 import { TranslateModule }        from '@ngx-translate/core';
+import { NbSpinnerModule }        from '@nebular/theme';
 import { FormWizardModule }       from '@ever-co/angular2-wizard';
+import { ThemeModule }            from '@app/@theme/theme.module';
 import { ProductCreateComponent } from './product-create.component';
 import { ProductFormsModule }     from '../forms';
-import { NbSpinnerModule }        from '@nebular/theme';
 
+// noinspection AngularInvalidImportedOrDeclaredSymbol
 @NgModule({
-	          imports: [
+	          imports:         [
 		          ThemeModule,
 		          FormWizardModule,
 		          TranslateModule.forChild(),
-		
 		          ProductFormsModule,
 		          NbSpinnerModule,
 	          ],
-	          exports: [ProductCreateComponent],
-	          declarations: [ProductCreateComponent],
+	          exports:         [ProductCreateComponent],
+	          declarations:    [ProductCreateComponent],
 	          entryComponents: [ProductCreateComponent],
           })
 export class ProductCreateModule {}
