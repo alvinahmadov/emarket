98034c3f6cbad6c315ffd5720defd8a970d6147e
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-20 02:05:34 +0400

    Move styles to component Add getters

--- packages/admin-web-angular/src/app/@shared/render-component/customer-orders-table/redirect-store/redirect-store.component.ts
+++ packages/admin-web-angular/src/app/@shared/render-component/customer-orders-table/redirect-store/redirect-store.component.ts
@@ -1,44 +1,62 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ViewCell }                 from 'ng2-smart-table';
-import { Router }                   from '@angular/router';
-import { WarehousesService }        from '../../../../@core/data/warehouses.service';
-import Warehouse                    from '@modules/server.common/entities/Warehouse';
-import { Observable }               from 'rxjs';
+import { Component, Input, OnInit }    from '@angular/core';
+import { Router }                      from '@angular/router';
+import { Observable }                  from 'rxjs';
+import { ViewCell }                    from 'ng2-smart-table';
+import
+{ WarehousesService as StoresService } from '@app/@core/data/warehouses.service';
+import { default as Store }            from '@modules/server.common/entities/Warehouse';
 
 @Component({
-	           styleUrls: ['redirect-store.component.scss'],
+	           styles:      [
+		           `
+                       .warehouse-name {
+                           padding-top: 5px;
+                           font-weight: bold;
+                       }
+
+                       .warehouse-image {
+                           width: 30px;
+                           height: 30px;
+                       }
+		           `
+	           ],
 	           templateUrl: './redirect-store.component.html',
            })
 export class RedirectStoreComponent implements ViewCell, OnInit
 {
-	value: string | number;
+	public value: string | number;
 	
 	@Input()
-	rowData: any;
-	store$: Observable<Warehouse>;
+	public rowData: any;
+	public store$: Observable<Store>;
 	
-	public warehouseStatusText: string;
+	public storeStatusText: string;
 	
 	constructor(
 			private readonly router: Router,
-			private readonly warehousesService: WarehousesService
+			private readonly storesService: StoresService
 	)
 	{}
 	
-	ngOnInit()
+	public ngOnInit()
 	{
-		this.store$ = this.warehousesService.getStoreById(
-				this.rowData.warehouseId
+		this.store$ = this.storesService.getStoreById(
+				this.storeId
 		);
-		this.warehouseStatusText =
+		this.storeStatusText =
 				'STATUS_TEXT.' + this.rowData.warehouseStatusText;
 	}
 	
-	redirect()
+	public get storeId(): string
+	{
+		return this.rowData.warehouseId
+	}
+	
+	public redirect()
 	{
 		if(this.rowData.warehouseId)
 		{
-			this.router.navigate([`stores/${this.rowData.warehouseId}`]);
+			this.router.navigate([`stores/${this.storeId}`]);
 		}
 	}
 }
