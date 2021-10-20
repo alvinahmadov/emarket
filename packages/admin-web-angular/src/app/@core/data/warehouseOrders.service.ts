5ad23f708df7f0fddd464327f056cf731f685f78
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-20 11:19:24 +0400

    Extend service from ApolloService Add explicit types for apollo query/mutation return values Add explicit method return types for services

--- packages/admin-web-angular/src/app/@core/data/warehouseOrders.service.ts
+++ packages/admin-web-angular/src/app/@core/data/warehouseOrders.service.ts
@@ -1,86 +1,87 @@
-import { Injectable }               from '@angular/core';
-import { Apollo }                   from 'apollo-angular';
-import { Observable }               from 'rxjs';
-import { IOrderCreateInput }        from '@modules/server.common/routers/IWarehouseOrdersRouter';
-import Order                        from '@modules/server.common/entities/Order';
-import { map }                      from 'rxjs/operators';
-import 'rxjs/add/operator/map';
-import IPagingOptions               from '@modules/server.common/interfaces/IPagingOptions';
-import { GQLMutations, GQLQueries } from '@modules/server.common/utilities';
+import { Injectable }            from '@angular/core';
+import { Apollo }                from 'apollo-angular';
+import { Observable }            from 'rxjs';
+import { map }                   from 'rxjs/operators';
+import IPagingOptions            from '@modules/server.common/interfaces/IPagingOptions';
+import Order                     from '@modules/server.common/entities/Order';
+import { IOrderCreateInput }     from '@modules/server.common/routers/IWarehouseOrdersRouter';
+import ApolloService             from '@modules/client.common.angular2/services/apollo.service';
+import { GQLMutation, GQLQuery } from 'graphql/definitions';
 
 @Injectable()
-export class WarehouseOrdersService
+export class WarehouseOrdersService extends ApolloService
 {
-	constructor(private readonly _apollo: Apollo) {}
-	
-	createOrder(createInput: IOrderCreateInput): Observable<Order>
+	constructor(apollo: Apollo)
 	{
-		return this._apollo
-		           .mutate({
-			                   mutation: GQLMutations.OrderCreate,
-			                   variables: { createInput },
-		                   })
-		           .pipe(map((result: any) => result.data.createOrder));
+		super(apollo, {
+			serviceName:  "Admin::WarehouseOrdersService",
+			pollInterval: 5000
+		});
 	}
 	
-	getDashboardOrdersChartOrders(storeId: string): Observable<Order[]>
+	public createOrder(createInput: IOrderCreateInput): Observable<Order>
 	{
-		return this._apollo
-		           .watchQuery<{
-			           getDashboardOrdersChartOrders: Order[]
+		return this.apollo
+		           .mutate<{
+			           order: IOrderCreateInput
 		           }>({
-			              query: GQLQueries.OrderDashboardChartOrders,
+			              mutation:  GQLMutation.Store.Order.Create,
+			              variables: { createInput },
+		              })
+		           .pipe(map((result) => <Order>this.factory(result, Order)));
+	}
+	
+	public getDashboardOrdersChartOrders(storeId: string): Observable<Order[]>
+	{
+		return this.apollo
+		           .watchQuery<{
+			           dashboardChartOrders: Order[]
+		           }>({
+			              query:     GQLQuery.Store.Order.GetDashboardChart,
 			              variables: { storeId },
 		              })
 		           .valueChanges
-		           .pipe(
-				           map((res) => res.data.getDashboardOrdersChartOrders)
-		           );
+		           .pipe(map((result) => this.get(result)));
 	}
 	
-	getStoreOrders(storeId: string): Observable<Order[]>
+	public getStoreOrders(storeId: string): Observable<Order[]>
 	{
-		return this._apollo
+		return this.apollo
 		           .watchQuery<{
 			           getStoreOrders: Order[]
 		           }>({
-			              query: GQLQueries.OrderStoreOrders,
-			              pollInterval: 5000,
-			              variables: { storeId },
+			              query:        GQLQuery.Store.Order.GetById,
+			              pollInterval: this.pollInterval,
+			              variables:    { storeId },
 		              })
 		           .valueChanges
-		           .pipe(
-				           map((res) => res.data.getStoreOrders)
-		           );
+		           .pipe(map((result) => this.get(result)));
 	}
 	
-	getStoreOrdersTableData(
+	public getStoreOrdersTableData(
 			storeId: string,
 			pagingOptions?: IPagingOptions,
 			status?: string
 	): Observable<{ orders: Order[] }>
 	{
-		return this._apollo
+		return this.apollo
 		           .watchQuery({
-			                       query: GQLQueries.OrderStoreOrdersTableData,
-			                       pollInterval: 5000,
-			                       variables: { storeId, pagingOptions, status },
+			                       query:        GQLQuery.Store.Order.GetTableData,
+			                       pollInterval: this.pollInterval,
+			                       variables:    { storeId, pagingOptions, status },
 		                       })
 		           .valueChanges
-		           .pipe(
-				           map((res) => res.data['getStoreOrdersTableData'])
-		           );
+		           .pipe(map((result) => this.get(result)));
 	}
 	
-	async getCountOfStoreOrders(storeId: string, status?: string)
+	public async getCountOfStoreOrders(storeId: string, status?: string): Promise<number>
 	{
-		const res = await this._apollo
-		                      .query({
-			                             query: GQLQueries.OrderStoreOrdersCount,
-			                             variables: { storeId, status },
-		                             })
-		                      .toPromise();
-		
-		return res.data['getCountOfStoreOrders'];
+		return this.apollo
+		           .query({
+			                  query:     GQLQuery.Store.Order.GetCount,
+			                  variables: { storeId, status },
+		                  })
+		           .pipe(map((result) => this.get(result)))
+		           .toPromise();
 	}
 }
