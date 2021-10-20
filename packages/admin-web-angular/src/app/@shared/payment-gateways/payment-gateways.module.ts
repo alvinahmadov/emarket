057ab65108634846a16ef9d3ee1f4fb56cfcefa2
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-20 20:02:00 +0400

    Refactor Reformat Reorder imports

--- packages/admin-web-angular/src/app/@shared/payment-gateways/payment-gateways.module.ts
+++ packages/admin-web-angular/src/app/@shared/payment-gateways/payment-gateways.module.ts
@@ -2,20 +2,22 @@ import { NgModule }                 from '@angular/core';
 import { ThemeModule }              from '@app/@theme';
 import { CommonModule }             from '@angular/common';
 import { TranslateModule }          from '@ngx-translate/core';
-import { PaymentGatewaysComponent } from './payment-gateways.component';
-import { StripeGatewayComponent }   from './stripe-gateway/stripe-gateway.component';
-import { PayPalGatewayComponent }   from './paypal-gateway/paypal-gateway.component';
-import { YooMoneyGatewayComponent } from './yoomoney-gateway/yoomoney-gateway.component';
-import { BitpayGatewayComponent }   from './bitpay-gateway/bitpay-gateway.component';
-import { FileUploaderModule }       from '@app/@shared/file-uploader/file-uploader.module';
+import { NbInputModule }            from '@nebular/theme';
 import { CurrenciesService }        from '@app/@core/data/currencies.service';
+import { FileUploaderModule }       from '@app/@shared/file-uploader/file-uploader.module';
+import { PaymentGatewaysComponent } from './payment-gateways.component';
+import { BitpayGatewayComponent }   from './bitpay-gateway/bitpay-gateway.component';
+import { PayPalGatewayComponent }   from './paypal-gateway/paypal-gateway.component';
+import { StripeGatewayComponent }   from './stripe-gateway/stripe-gateway.component';
+import { YooMoneyGatewayComponent } from './yoomoney-gateway/yoomoney-gateway.component';
 
 @NgModule({
-	          imports: [
+	          imports:      [
 		          CommonModule,
 		          ThemeModule,
 		          TranslateModule.forChild(),
 		          FileUploaderModule,
+		          NbInputModule,
 	          ],
 	          declarations: [
 		          PaymentGatewaysComponent,
@@ -24,7 +26,7 @@ import { CurrenciesService }        from '@app/@core/data/currencies.service';
 		          YooMoneyGatewayComponent,
 		          BitpayGatewayComponent
 	          ],
-	          exports: [PaymentGatewaysComponent],
-	          providers: [CurrenciesService],
+	          exports:      [PaymentGatewaysComponent],
+	          providers:    [CurrenciesService],
           })
 export class PaymentGatewaysModule {}
