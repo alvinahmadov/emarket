b7d9ce2f82a52fa63efdd4553d13181ddc5fd639
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-21 09:58:46 +0400

    Refactor: Store to StorageService

--- packages/merchant-tablet-ionic/src/graphql/apollo.config.ts
+++ packages/merchant-tablet-ionic/src/graphql/apollo.config.ts
@@ -4,8 +4,8 @@ import { Apollo, ApolloModule }     from 'apollo-angular';
 import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
 import { InMemoryCache }            from 'apollo-cache-inmemory';
 import { setContext }               from 'apollo-link-context';
-import { Store }                    from '../services/store.service';
-import { environment }              from '../environments/environment';
+import { StorageService }           from 'services/storage.service';
+import { environment }              from 'environments/environment';
 
 @NgModule({
 	          exports: [HttpClientModule, ApolloModule, HttpLinkModule],
@@ -15,7 +15,7 @@ export class GraphQLModule
 	constructor(
 			private readonly apollo: Apollo,
 			private readonly httpLink: HttpLink,
-			private readonly store: Store
+			private readonly storageService: StorageService
 	)
 	{
 		const uri = environment.GQL_ENDPOINT;
@@ -24,7 +24,7 @@ export class GraphQLModule
 		const authLink = setContext((_, { headers }) =>
 		                            {
 			                            // get the authentication token from local storage if it exists
-			                            const token = store.token;
+			                            const token = storageService.token;
 			                            // return the headers to the context so httpLink can read them
 			                            return {
 				                            headers: {
@@ -35,21 +35,21 @@ export class GraphQLModule
 		                            });
 		
 		apollo.create({
-			              link: authLink.concat(http),
+			              link:           authLink.concat(http),
 			              defaultOptions: {
 				              watchQuery: {
 					              fetchPolicy: 'network-only',
 					              errorPolicy: 'ignore',
 				              },
-				              query: {
+				              query:      {
 					              fetchPolicy: 'network-only',
 					              errorPolicy: 'all',
 				              },
-				              mutate: {
+				              mutate:     {
 					              errorPolicy: 'all',
 				              },
 			              },
-			              cache: new InMemoryCache(),
+			              cache:          new InMemoryCache(),
 		              });
 	}
 }
