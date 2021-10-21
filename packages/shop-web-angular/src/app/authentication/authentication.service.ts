6c561b3b10c30214aac1978996013e327d745f26
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-21 23:21:23 +0400

    Rename variable

--- packages/shop-web-angular/src/app/authentication/authentication.service.ts
+++ packages/shop-web-angular/src/app/authentication/authentication.service.ts
@@ -1,13 +1,13 @@
-import { Injectable }   from '@angular/core';
-import { InviteRouter } from '@modules/client.common.angular2/routers/invite-router.service';
-import { UserRouter }   from '@modules/client.common.angular2/routers/user-router.service';
-import Invite           from '@modules/server.common/entities/Invite';
+import { Injectable }     from '@angular/core';
+import { InviteRouter }   from '@modules/client.common.angular2/routers/invite-router.service';
+import { CustomerRouter } from '@modules/client.common.angular2/routers/customer-router.service';
+import Invite             from '@modules/server.common/entities/Invite';
 
 @Injectable()
 export class AuthenticationService
 {
 	constructor(
-			private readonly userRouter: UserRouter,
+			private readonly customerRouter: CustomerRouter,
 			private readonly inviteRouter: InviteRouter
 	)
 	{}
