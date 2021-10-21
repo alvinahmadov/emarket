3af41598637457d1135564c42518a0564e339adf
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-21 23:02:08 +0400

    Reorder imports Reformat Add catch function for module import

--- packages/shop-web-angular/src/app/app.routes.ts
+++ packages/shop-web-angular/src/app/app.routes.ts
@@ -1,64 +1,90 @@
 import { Routes }                 from '@angular/router';
-import { NoContentComponent }     from './no-content';
-import { LoginModuleGuard }       from './+login/login.module.guard';
-import { ProductsModuleGuard }    from './+products/products.module.guard';
+import { environment }            from 'environments/environment'
 import { AppModuleGuard }         from './app.module.guard';
-import { MaintenanceModuleGuard } from './+maintenance-info/maintenance-info.module.guard';
+import { NoContentComponent }     from './no-content';
 import { AuthGuard }              from './authentication/auth.guard';
+import { LoginModuleGuard }       from './+login/login.module.guard';
+import { MaintenanceModuleGuard } from './+maintenance-info/maintenance-info.module.guard';
+import { ProductsModuleGuard }    from './+products/products.module.guard';
+import { WarehousesModuleGuard }  from './+warehouses/warehouses.module.guard';
+
+function catchError(error)
+{
+	if(!environment.production)
+	{
+		console.error(error?.message)
+	}
+}
 
 export const ROUTES: Routes = [
 	{
-		path: '',
-		children: [
+		path:        '',
+		children:    [
 			{
-				path: '',
+				path:       '',
 				redirectTo: '/login',
-				pathMatch: 'full',
+				pathMatch:  'full',
 			},
 			{
-				path: 'login',
+				path:         'login',
 				loadChildren: () =>
-						import('./+login').then((m) => m.LoginModule),
-				canActivate: [LoginModuleGuard],
+						              import('./+login')
+								              .then((m) => m.LoginModule)
+								              .catch(err => catchError(err)),
+				canActivate:  [LoginModuleGuard],
 			},
 			{
-				path: 'products',
+				path:         'products',
 				loadChildren: () =>
-						import('./+products').then((m) => m.ProductsModule),
-				canActivate: [ProductsModuleGuard, AuthGuard],
+						              import('./+products')
+								              .then((m) => m.ProductsModule)
+								              .catch(err => catchError(err)),
+				canActivate:  [ProductsModuleGuard, AuthGuard],
 			},
 			{
-				path: 'orders',
+				path:         'orders',
 				loadChildren: () =>
-						import('./+orders').then((m) => m.OrdersModule),
-				canActivate: [AuthGuard],
+						              import('./+orders')
+								              .then((m) => m.OrdersModule)
+								              .catch(err => catchError(err)),
+				canActivate:  [AuthGuard],
 			},
 			{
-				path: 'settings',
+				path:         'warehouses',
 				loadChildren: () =>
-						import('./+settings').then((m) => m.SettingsModule),
-				canActivate: [AuthGuard],
+						              import('./+warehouses')
+								              .then((m) => m.WarehousesModule)
+								              .catch(err => catchError(err)),
+				canActivate:  [WarehousesModuleGuard, AuthGuard],
+			},
+			{
+				path:         'settings',
+				loadChildren: () =>
+						              import('./+settings')
+								              .then((m) => m.SettingsModule)
+								              .catch(err => catchError(err)),
+				canActivate:  [AuthGuard],
 			},
 		],
 		canActivate: [AppModuleGuard],
 	},
 	{
-		path: 'maintenance-info',
+		path:         'maintenance-info',
 		loadChildren: () =>
-				import('./+maintenance-info/maintenance-info.module').then(
-						(m) => m.MaintenanceInfoModule
-				),
-		canActivate: [MaintenanceModuleGuard],
+				              import('./+maintenance-info/maintenance-info.module')
+						              .then((m) => m.MaintenanceInfoModule)
+						              .catch(err => catchError(err)),
+		canActivate:  [MaintenanceModuleGuard],
 	},
 	{
-		path: 'server-down',
+		path:         'server-down',
 		loadChildren: () =>
-				import('./+server-down/server-down.module').then(
-						(m) => m.ServerDownModule
-				),
+				              import('./+server-down/server-down.module')
+						              .then((m) => m.ServerDownModule)
+						              .catch(err => catchError(err)),
 	},
 	{
-		path: '**',
+		path:      '**',
 		component: NoContentComponent,
 	},
 ];
