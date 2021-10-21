fed21712d72194b6dba1da3c0c8e161e3755a8f3
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-21 09:44:42 +0400

    Reorder imports Extend from ApolloService

--- packages/merchant-tablet-ionic/src/services/warehouse-products.service.ts
+++ packages/merchant-tablet-ionic/src/services/warehouse-products.service.ts
@@ -4,40 +4,47 @@ import { Observable }   from 'rxjs';
 import { map }          from 'rxjs/operators';
 import IPagingOptions   from '@modules/server.common/interfaces/IPagingOptions';
 import WarehouseProduct from '@modules/server.common/entities/WarehouseProduct';
-import { GQLQueries }   from '@modules/server.common/utilities/graphql';
+import ApolloService    from '@modules/client.common.angular2/services/apollo.service';
+import { GQLQuery }     from 'graphql/definitions';
 
 @Injectable()
-export class WarehouseProductsService
+export class WarehouseProductsService extends ApolloService
 {
-	constructor(private readonly _apollo: Apollo) {}
+	constructor(apollo: Apollo)
+	{
+		super(apollo, {
+			serviceName:  "Merchant::WarehouseProductsService",
+			pollInterval: 4000
+		})
+	}
 	
-	getProductsWithPagination(
+	public getProductsWithPagination(
 			storeId: string,
 			pagingOptions?: IPagingOptions
 	): Observable<WarehouseProduct[]>
 	{
-		return this._apollo
+		return this.apollo
 		           .watchQuery<{
 			           warehouseProducts: WarehouseProduct[]
 		           }>({
-			              query:        GQLQueries.WarehouseProductProductsWithPagination,
-			              pollInterval: 2000,
+			              query:        GQLQuery.Store.Product.GetWithPagination,
+			              pollInterval: this.pollInterval,
 			              variables:    { storeId, pagingOptions },
 		              })
-		           .valueChanges.pipe(
-						map((res) => res.data['getWarehouseProductsWithPagination'])
-				);
+		           .valueChanges
+		           .pipe(map((result) => this.get(result)));
 	}
 	
-	async getProductsCount(storeId: string)
+	async getProductsCount(storeId: string): Promise<number>
 	{
-		const res = await this._apollo
-		                      .query({
-			                             query:     GQLQueries.WarehouseProductProductsCount,
-			                             variables: { storeId },
-		                             })
-		                      .toPromise();
-		
-		return res.data['getWarehouseProductsCount'];
+		return this.apollo
+		           .query<{
+			           count: number
+		           }>({
+			              query:     GQLQuery.Store.Product.Count,
+			              variables: { storeId },
+		              })
+		           .pipe(map((result) => this.get(result)))
+		           .toPromise();
 	}
 }
