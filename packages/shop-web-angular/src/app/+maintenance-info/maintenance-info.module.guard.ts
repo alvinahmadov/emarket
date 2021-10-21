adbb0b6a073e9b0f9fa400c256f38c53a5071854
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-21 23:02:50 +0400

    Reformat Rename Store to StorageService

--- packages/shop-web-angular/src/app/+maintenance-info/maintenance-info.module.guard.ts
+++ packages/shop-web-angular/src/app/+maintenance-info/maintenance-info.module.guard.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable }     from '@angular/core';
 import {
 	Router,
 	CanActivate,
 	ActivatedRouteSnapshot,
 	RouterStateSnapshot,
-}                     from '@angular/router';
-import { Store }      from 'app/services/store';
+}                         from '@angular/router';
+import { StorageService } from 'app/services/storage';
 
 @Injectable()
 export class MaintenanceModuleGuard implements CanActivate
 {
-	constructor(private readonly router: Router, private store: Store) {}
+	constructor(private readonly router: Router, private store: StorageService) {}
 	
 	canActivate(
 			route: ActivatedRouteSnapshot,
