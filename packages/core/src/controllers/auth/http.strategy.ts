a38887ecfd9eeb97fae5b0760b969711291b38ee
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-21 10:13:50 +0400

    Reorder imports Add jwt secret from environment

--- packages/core/src/controllers/auth/http.strategy.ts
+++ packages/core/src/controllers/auth/http.strategy.ts
@@ -1,8 +1,9 @@
-import { Strategy }         from 'passport-http-bearer';
-import { PassportStrategy } from '@nestjs/passport';
-import { Injectable }       from '@nestjs/common';
-import { AuthService }      from './auth.service';
-import { ExtractJwt }       from 'passport-jwt';
+import { PassportStrategy }                  from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Strategy }                          from 'passport-http-bearer';
+import { ExtractJwt }                        from 'passport-jwt';
+import { AuthService, JwtPayload }           from './auth.service';
+import { env }                               from '../../env';
 
 @Injectable()
 export class HttpStrategy extends PassportStrategy(Strategy)
@@ -11,15 +12,17 @@ export class HttpStrategy extends PassportStrategy(Strategy)
 	{
 		super({
 			      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			      secretOrKey: 'secretKey'
+			      secretOrKey:    env.JWT_SECRET
 		      });
 	}
 	
-	// async validate(payload: JwtPayload, done: Function) {
-	//   const user = await this.authService.validateUser(payload);
-	//   if (!user) {
-	//     return done(new UnauthorizedException(), false);
-	//   }
-	//   done(null, user);
-	// }
+	async validate(payload: JwtPayload, done: Function)
+	{
+		const user = await this.authService.validateUser(payload);
+		if(!user)
+		{
+			return done(new UnauthorizedException(), false);
+		}
+		done(null, user);
+	}
 }
