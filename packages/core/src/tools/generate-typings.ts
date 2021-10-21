257d1e91005a910e7551be7455f5d4943c01a059
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-21 10:23:19 +0400

    Add helper tool for definitions generation in typescript

--- packages/core/src/tools/generate-typings.ts
+++ packages/core/src/tools/generate-typings.ts
@@ -1,11 +1,19 @@
 import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
 import { join }                      from 'path';
 
-const definitionsFactory = new GraphQLDefinitionsFactory();
-definitionsFactory.generate({
-	                            typePaths: ['./src/**/*.graphql'],
-	                            path: join(process.cwd(), 'src/graphql.schema.ts'),
-	                            outputAs: 'class',
-                            })
-                  .then(console.log)
-                  .catch(console.error);
+export function generateDefinitions()
+{
+	const definitionsFactory = new GraphQLDefinitionsFactory();
+	definitionsFactory.generate({
+		                            typePaths: ['./src/**/*.graphql'],
+		                            path:      join(process.cwd(), 'src/graphql.schema.ts'),
+		                            outputAs:  'interface',
+	                            })
+	                  .then(console.log)
+	                  .catch(console.error);
+}
+
+(() =>
+{
+	generateDefinitions();
+})()
