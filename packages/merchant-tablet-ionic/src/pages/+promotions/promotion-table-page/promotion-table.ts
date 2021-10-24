814e417f4dec8c6f5724e1e5fe9ca51274a2ba0a
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-25 00:24:59 +0400

    Refactor: Store to StorageService Reorder imports Reorder

--- packages/merchant-tablet-ionic/src/pages/+promotions/promotion-table-page/promotion-table.ts
+++ packages/merchant-tablet-ionic/src/pages/+promotions/promotion-table-page/promotion-table.ts
@@ -1,35 +1,37 @@
 import { OnInit, OnDestroy, Component }     from '@angular/core';
-import { LocalDataSource }                  from 'ng2-smart-table';
-import { IPromotion }                       from '@modules/server.common/interfaces/IPromotion';
-import { Subject }                          from 'rxjs';
-import { PromotionService }                 from 'services/promotion.service';
+import { DatePipe }                         from '@angular/common';
 import { ModalController, ToastController } from '@ionic/angular';
 import { TranslateService }                 from '@ngx-translate/core';
+import { LocalDataSource }                  from 'ng2-smart-table';
+import { Subject }                          from 'rxjs';
 import { takeUntil }                        from 'rxjs/operators';
-import { PromotionMutation }                from '../promotion-mutation-popup/promotion-mutation';
+import { IPromotion }                       from '@modules/server.common/interfaces/IPromotion';
 import { ProductLocalesService }            from '@modules/client.common.angular2/locale/product-locales.service';
 import { ConfirmDeletePopupPage }           from 'components/confirm-delete-popup/confirm-delete-popup';
 import { ImageTableComponent }              from 'components/table-components/image-table';
-import { DatePipe }                         from '@angular/common';
+import { PromotionService }                 from 'services/promotion.service';
+import { StorageService }                   from 'services/storage.service';
+import { PromotionMutation }                from '../promotion-mutation-popup/promotion-mutation';
 
 @Component({
-	           selector: 'promotion-table',
+	           selector:    'promotion-table',
 	           templateUrl: 'promotion-table.html',
-	           styleUrls: ['./promotion-table.scss'],
+	           styleUrls:   ['./promotion-table.scss'],
            })
 export class PromotionTable implements OnInit, OnDestroy
 {
-	settingsSmartTable: object;
-	showNoPromotionsIcon: boolean = false;
+	public settingsSmartTable: object;
+	public showNoPromotionsIcon: boolean = false;
 	
-	promotions: IPromotion[];
+	public promotions: IPromotion[];
 	
-	sourceSmartTable = new LocalDataSource();
-	selectedPromotions: IPromotion[];
+	public sourceSmartTable = new LocalDataSource();
+	public selectedPromotions: IPromotion[];
 	
 	private _ngDestroy$ = new Subject<void>();
 	
 	constructor(
+			private readonly storage: StorageService,
 			private readonly promotionsService: PromotionService,
 			private readonly productLocaleService: ProductLocalesService,
 			private readonly translateService: TranslateService,
@@ -38,26 +40,90 @@ export class PromotionTable implements OnInit, OnDestroy
 	)
 	{}
 	
-	ngOnInit(): void
+	public ngOnInit(): void
 	{
 		this._loadPromotions();
 		this._applyTranslationOnSmartTable();
 	}
 	
-	ngOnDestroy()
+	public ngOnDestroy()
 	{
 		this._ngDestroy$.next();
 		this._ngDestroy$.complete();
 	}
 	
+	public async openAddPromotion()
+	{
+		const addPromotionPopup = await this.modalCtrl.create({
+			                                                      component: PromotionMutation,
+		                                                      });
+		
+		await addPromotionPopup.present();
+		
+		await addPromotionPopup.onDidDismiss();
+		
+		this._loadPromotions();
+	}
+	
+	public async editPromotion(event: any)
+	{
+		const modal = await this.modalCtrl.create({
+			                                          component:      PromotionMutation,
+			                                          componentProps: { promotion: event.data.promotion },
+		                                          });
+		
+		await modal.present();
+		
+		await modal.onDidDismiss();
+		
+		this._loadPromotions();
+	}
+	
+	public async deletePromotion(event: any)
+	{
+		const modal = await this.modalCtrl.create({
+			                                          component:      ConfirmDeletePopupPage,
+			                                          componentProps: {
+				                                          data:       event.data,
+				                                          customText: `Promotion: ${event.data.title || '-'}`,
+			                                          },
+			                                          cssClass:       'confirm-delete-wrapper',
+		                                          });
+		
+		await modal.present();
+		
+		const res = await modal.onDidDismiss();
+		
+		if(res.data)
+		{
+			const promotionId = event.data.id;
+			
+			this.promotionsService.removeByIds([promotionId]).subscribe(
+					() =>
+					{
+						this.promotions = this.promotions.filter(
+								(p) => ![promotionId].includes(p._id)
+						);
+						
+						this._loadDataSmartTable();
+						this.presentToast('Successfully deleted promotion!');
+					},
+					(err) =>
+					{
+						this.presentToast(err.message || 'Something went wrong!');
+					}
+			);
+		}
+	}
+	
 	private _loadPromotions()
 	{
 		this.promotionsService
-		    .getAll({ warehouse: localStorage.getItem('_warehouseId') || null })
+		    .getPromotions({ warehouse: this.storage.warehouseId })
 		    .pipe(takeUntil(this._ngDestroy$))
-		    .subscribe((promotionsRes) =>
+		    .subscribe((promotions) =>
 		               {
-			               this.promotions = promotionsRes.promotions || [];
+			               this.promotions = promotions || [];
 			
 			               this._loadSettingsSmartTable();
 			               this._loadDataSmartTable();
@@ -76,64 +142,51 @@ export class PromotionTable implements OnInit, OnDestroy
 		    .subscribe((TRANSLATE_DATA) =>
 		               {
 			               this.settingsSmartTable = {
-				               mode: 'external',
-				               edit: {
+				               mode:    'external',
+				               edit:    {
 					               editButtonContent: '<i class="fa fa-edit"></i>',
-					               confirmEdit: true,
+					               confirmEdit:       true,
 				               },
-				               delete: {
+				               delete:  {
 					               deleteButtonContent: '<i class="fa fa-trash"></i>',
-					               confirmDelete: true,
+					               confirmDelete:       true,
 				               },
 				               columns: {
-					               image: {
-						               title: TRANSLATE_DATA.IMAGE,
-						               type: 'custom',
+					               image:          {
+						               title:           TRANSLATE_DATA.IMAGE,
+						               type:            'custom',
 						               renderComponent: ImageTableComponent,
-						               filter: false,
+						               filter:          false,
 					               },
-					               title: {
+					               title:          {
 						               title: TRANSLATE_DATA.TITLE,
-						               type: 'string',
+						               type:  'string',
 					               },
-					               active: {
-						               title: TRANSLATE_DATA.STATUS,
-						               type: 'html',
+					               active:         {
+						               title:                TRANSLATE_DATA.STATUS,
+						               type:                 'html',
 						               valuePrepareFunction: (active) =>
-						               {
-							               return `<span>${active ? '✔' : '✘'}</span>`;
-						               },
+						                                     {
+							                                     return `<span>${active ? '✔' : '✘'}</span>`;
+						                                     },
 					               },
-					               activeFrom: {
-						               title: TRANSLATE_DATA.ACTIVE_FROM,
-						               type: 'html',
-						               valuePrepareFunction: this.formatTableDate,
+					               activeFrom:     {
+						               title:                TRANSLATE_DATA.ACTIVE_FROM,
+						               type:                 'html',
+						               valuePrepareFunction: PromotionTable.formatTableDate,
 					               },
-					               activeTo: {
-						               title: TRANSLATE_DATA.ACTIVE_TO,
-						               type: 'html',
-						               valuePrepareFunction: this.formatTableDate,
+					               activeTo:       {
+						               title:                TRANSLATE_DATA.ACTIVE_TO,
+						               type:                 'html',
+						               valuePrepareFunction: PromotionTable.formatTableDate,
 					               },
 					               purchasesCount: {
 						               title: TRANSLATE_DATA.PURCHASES_COUNT,
-						               type: 'number',
+						               type:  'number',
 					               },
 				               },
 			               };
 		               });
-	}
-	
-	async openAddPromotion()
-	{
-		const addPromotionPopup = await this.modalCtrl.create({
-			                                                      component: PromotionMutation,
-		                                                      });
-		
-		await addPromotionPopup.present();
-		
-		await addPromotionPopup.onDidDismiss();
-		
-		this._loadPromotions();
 	}
 	
 	private _applyTranslationOnSmartTable()
@@ -149,72 +202,23 @@ export class PromotionTable implements OnInit, OnDestroy
 	
 	private _loadDataSmartTable()
 	{
-		const promotionsVM = this.promotions.map((promotion) =>
-		                                         {
-			                                         return {
-				                                         id: promotion._id,
-				                                         image: promotion.image,
-				                                         title: this.productLocaleService.getTranslate(promotion.title),
-				                                         active: promotion.active,
-				                                         activeFrom: promotion.activeFrom,
-				                                         activeTo: promotion.activeTo,
-				                                         purchasesCount: promotion.purchasesCount,
-				                                         promotion: promotion,
-			                                         };
-		                                         });
+		const promotionsVM = this.promotions.map(
+				(promotion) =>
+				{
+					return {
+						id:             promotion._id,
+						image:          promotion.image,
+						title:          this.productLocaleService.getTranslate(promotion.title),
+						active:         promotion.active,
+						activeFrom:     promotion.activeFrom,
+						activeTo:       promotion.activeTo,
+						purchasesCount: promotion.purchasesCount,
+						promotion:      promotion,
+					};
+				}
+		);
 		
 		this.sourceSmartTable.load(promotionsVM);
-	}
-	
-	async editPromotion(event: any)
-	{
-		const modal = await this.modalCtrl.create({
-			                                          component: PromotionMutation,
-			                                          componentProps: { promotion: event.data.promotion },
-		                                          });
-		
-		await modal.present();
-		
-		await modal.onDidDismiss();
-		
-		this._loadPromotions();
-	}
-	
-	async deletePromotion(event: any)
-	{
-		const modal = await this.modalCtrl.create({
-			                                          component: ConfirmDeletePopupPage,
-			                                          componentProps: {
-				                                          data: event.data,
-				                                          customText: `Promotion: ${event.data.title || '-'}`,
-			                                          },
-			                                          cssClass: 'confirm-delete-wrapper',
-		                                          });
-		
-		await modal.present();
-		
-		const res = await modal.onDidDismiss();
-		
-		if(res.data)
-		{
-			const promotionId = event.data.id;
-			
-			this.promotionsService.removeByIds([promotionId]).subscribe(
-					(data) =>
-					{
-						this.promotions = this.promotions.filter(
-								(p) => ![promotionId].includes(p._id)
-						);
-						
-						this._loadDataSmartTable();
-						this.presentToast('Successfully deleted promotion!');
-					},
-					(err) =>
-					{
-						this.presentToast(err.message || 'Something went wrong!');
-					}
-			);
-		}
 	}
 	
 	private presentToast(message: string)
@@ -230,12 +234,13 @@ export class PromotionTable implements OnInit, OnDestroy
 		          });
 	}
 	
-	private formatTableDate(date: string)
+	private static formatTableDate(date: string)
 	{
 		const raw: Date = new Date(date);
 		
 		const formatted: string = date
-		                          ? new DatePipe('en-EN').transform(raw, 'dd-MMM-yyyy hh:mm:ss')
+		                          ? new DatePipe('en-GB')
+				                          .transform(raw, 'dd/MM/yy hh:mm')
 		                          : '-';
 		
 		return `<p>${formatted}</p>`;
