6b9e780c03c7a428c2e47e2474a1760c263d960f
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-25 01:12:12 +0400

    Refactor: Store to StorageService Add explicit types

--- packages/merchant-tablet-ionic/src/pages/+warehouse/warehouse.module.guard.ts
+++ packages/merchant-tablet-ionic/src/pages/+warehouse/warehouse.module.guard.ts
@@ -1,25 +1,22 @@
 import { Injectable }             from '@angular/core';
 import { CanLoad, Route, Router } from '@angular/router';
-import { Store }                  from '../../services/store.service';
+import { StorageService }         from 'services/storage.service';
 
 @Injectable()
 export class WarehouseModuleGuard implements CanLoad
 {
 	constructor(
-			private readonly store: Store,
+			private readonly storageService: StorageService,
 			private readonly router: Router
 	)
 	{}
 	
-	async canLoad(route: Route)
+	public async canLoad(route: Route): Promise<boolean>
 	{
-		const isLogged = await this.store.isLogged();
-		
-		const token = this.store.token;
-		
+		const isLogged = await this.storageService.isLogged();
 		if(!isLogged)
 		{
-			await this.router.navigateByUrl('/auth');
+			this.router.navigateByUrl('/auth');
 			return false;
 		}
 		return true;
