11f6f7f5617a1f3a181a2707bcb0b8dae65bf852
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-26 03:47:50 +0400

    Reorder imports User to Customer

--- packages/merchant-tablet-ionic/src/@shared/user/mutation/user-mutation.module.ts
+++ packages/merchant-tablet-ionic/src/@shared/user/mutation/user-mutation.module.ts
@@ -1,15 +1,15 @@
+import { CommonModule }          from '@angular/common';
 import { NgModule }              from '@angular/core';
-import { UserMutationComponent } from './user-mutation.component';
+import { FormsModule }           from '@angular/forms';
 import { TranslateModule }       from '@ngx-translate/core';
+import { IonicModule }           from '@ionic/angular';
+import { CustomersService }      from 'services/customers.service';
+import { UserMutationComponent } from './user-mutation.component';
 import { UserFormsModule }       from '../forms/user-forms.module';
 import { GoogleMapModule }       from '../../google-map/google-map.module';
-import { UsersService }          from '../../../services/users.service';
-import { IonicModule }           from '@ionic/angular';
-import { CommonModule }          from '@angular/common';
-import { FormsModule }           from '@angular/forms';
 
 @NgModule({
-	          imports: [
+	          imports:         [
 		          TranslateModule.forChild(),
 		          UserFormsModule,
 		          GoogleMapModule,
@@ -17,9 +17,9 @@ import { FormsModule }           from '@angular/forms';
 		          CommonModule,
 		          FormsModule,
 	          ],
-	          providers: [UsersService],
-	          exports: [UserMutationComponent],
-	          declarations: [UserMutationComponent],
+	          providers:       [CustomersService],
+	          exports:         [UserMutationComponent],
+	          declarations:    [UserMutationComponent],
 	          entryComponents: [UserMutationComponent],
           })
 export class UserMutationModule {}
