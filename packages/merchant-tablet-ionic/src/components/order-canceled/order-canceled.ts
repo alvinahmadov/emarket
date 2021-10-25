5771b8a0f9a922a6cad44aaa17bb590f978cc997
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-26 03:37:08 +0400

    Reorder imports Reformat

--- packages/merchant-tablet-ionic/src/components/order-canceled/order-canceled.ts
+++ packages/merchant-tablet-ionic/src/components/order-canceled/order-canceled.ts
@@ -1,11 +1,11 @@
 import { Component, Input }      from '@angular/core';
+import { ILocaleMember }         from '@modules/server.common/interfaces/ILocale';
 import Order                     from '@modules/server.common/entities/Order';
 import { ProductLocalesService } from '@modules/client.common.angular2/locale/product-locales.service';
-import { ILocaleMember }         from '@modules/server.common/interfaces/ILocale';
 
 @Component({
 	           selector: 'order-canceled',
-	           templateUrl: 'order-canceled.html',
+	           templateUrl: './order-canceled.html',
            })
 export class OrderCanceledComponent
 {
