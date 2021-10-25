24ec213b2cc9bc4147c562152979e2313bdfa4ff
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-25 13:03:05 +0400

    Reorder imports Reorder

--- packages/merchant-tablet-ionic/src/pages/+carriers/carrier-edit-popup/location/location-form.component.ts
+++ packages/merchant-tablet-ionic/src/pages/+carriers/carrier-edit-popup/location/location-form.component.ts
@@ -1,71 +1,71 @@
 import {
 	Component,
 	Input,
-	OnDestroy,
 	ViewChild,
 	ElementRef,
 	EventEmitter,
 	Output,
 	OnInit,
 	OnChanges,
-} from '@angular/core';
+	OnDestroy
+}                           from '@angular/core';
 import {
 	FormBuilder,
 	FormGroup,
 	Validators,
 	AbstractControl,
-} from '@angular/forms';
-
+}                           from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
-import GeoLocation, {
-	countriesIdsToNamesArray,
-	CountryName,
-	Country,
-	getCountryName,
-}                           from '@modules/server.common/entities/GeoLocation';
 import { isEmpty }          from 'lodash';
+import {
+	countriesIdsToNamesArray,
+	TCountryData,
+	getDefaultCountryName
+}                           from '@modules/server.common/data/countries';
+import Country              from '@modules/server.common/enums/Country';
+import GeoLocation          from '@modules/server.common/entities/GeoLocation';
 import Carrier              from '@modules/server.common/entities/Carrier';
 
 @Component({
-	           selector: 'carrier-location-form',
-	           styleUrls: ['./location-form.component.scss'],
+	           selector:    'carrier-location-form',
+	           styleUrls:   ['./location-form.component.scss'],
 	           templateUrl: 'location-form.component.html',
            })
 export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 {
 	@ViewChild('autocomplete', { static: true })
-	searchElement: ElementRef;
+	public searchElement: ElementRef;
 	
 	@Input()
-	carrier: Carrier;
+	public carrier: Carrier;
 	
 	@Output()
-	buttonClickEventComplete = new EventEmitter();
+	public buttonClickEventComplete = new EventEmitter();
 	@Output()
-	backToStep2event = new EventEmitter();
+	public backToStep2event = new EventEmitter();
 	
-	mapCoordEmitter = new EventEmitter<google.maps.LatLng>();
+	public mapCoordEmitter = new EventEmitter<google.maps.LatLng>();
 	
-	mapGeometryEmitter = new EventEmitter<google.maps.GeocoderGeometry | google.maps.places.PlaceGeometry>();
+	public mapGeometryEmitter = new EventEmitter<google.maps.GeocoderGeometry | google.maps.places.PlaceGeometry>();
 	
 	private _lastUsedAddress: string;
 	
-	form: FormGroup;
+	public form: FormGroup;
 	
-	showCoordinates: boolean = false;
+	public showCoordinates: boolean = false;
 	
-	city: AbstractControl;
-	street: AbstractControl;
-	house: AbstractControl;
-	country: AbstractControl;
-	lng: AbstractControl;
-	lat: AbstractControl;
+	public city: AbstractControl;
+	public street: AbstractControl;
+	public house: AbstractControl;
+	public country: AbstractControl;
+	public lng: AbstractControl;
+	public lat: AbstractControl;
 	// public apartment: AbstractControl;
-	postcode: AbstractControl;
+	public postcode: AbstractControl;
 	
-	OK: string = 'OK';
-	CANCEL: string = 'CANCEL';
-	PREFIX: string = 'CARRIERS_VIEW.ADD_CARRIER.';
+	public OK: string = 'OK';
+	public CANCEL: string = 'CANCEL';
+	public PREFIX: string = 'CARRIERS_VIEW.ADD_CARRIER.';
 	
 	constructor(
 			private formBuilder: FormBuilder,
@@ -73,7 +73,7 @@ export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 	)
 	{}
 	
-	ngOnInit()
+	public ngOnInit()
 	{
 		this._initGoogleAutocompleteApi();
 		// this._tryFindNewCoordinates();
@@ -83,33 +83,21 @@ export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 		this.loadData();
 	}
 	
-	ngOnChanges(): void {}
+	public ngOnChanges(): void {}
 	
-	get buttonOK()
+	public ngOnDestroy(): void {}
+	
+	public get buttonOK()
 	{
 		return this._translate(this.PREFIX + this.OK);
 	}
 	
-	get buttonCancel()
+	public get buttonCancel()
 	{
 		return this._translate(this.PREFIX + this.CANCEL);
 	}
 	
-	buildForm(formBuilder: FormBuilder)
-	{
-		this.form = formBuilder.group({
-			                              city: ['', Validators.required],
-			                              street: ['', Validators.required],
-			                              house: ['', Validators.required],
-			                              lat: ['', Validators.required],
-			                              lng: ['', Validators.required],
-			                              country: ['', Validators.required],
-			                              postcode: [''],
-			                              // apartment: ['']
-		                              });
-	}
-	
-	bindFormControls()
+	public bindFormControls()
 	{
 		this.city = this.form.get('city');
 		this.street = this.form.get('street');
@@ -121,18 +109,31 @@ export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 		this.postcode = this.form.get('postcode');
 	}
 	
-	get countries(): Array<{ id: Country; name: CountryName }>
+	public get countries(): TCountryData[]
 	{
 		return countriesIdsToNamesArray;
 	}
 	
-	toggleCoordinates()
+	public buildForm(formBuilder: FormBuilder)
 	{
-		this.showCoordinates = !this.showCoordinates;
-		console.log('Toggle Cordinates');
+		this.form = formBuilder.group({
+			                              city:     ['', Validators.required],
+			                              street:   ['', Validators.required],
+			                              house:    ['', Validators.required],
+			                              lat:      ['', Validators.required],
+			                              lng:      ['', Validators.required],
+			                              country:  ['', Validators.required],
+			                              postcode: [''],
+			                              // apartment: ['']
+		                              });
 	}
 	
-	textInputChange(val, input)
+	public toggleCoordinates()
+	{
+		this.showCoordinates = !this.showCoordinates;
+	}
+	
+	public textInputChange(val, input)
 	{
 		if(input === 'lat' || input === 'lng')
 		{
@@ -144,15 +145,14 @@ export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 		}
 	}
 	
-	toStep2()
+	public toStep2()
 	{
 		this.backToStep2event.emit();
 	}
 	
-	clickComplete()
+	public clickComplete()
 	{
 		// let prevOrComplete = data;
-		console.log('send event to parent');
 		this.buttonClickEventComplete.emit('complete');
 	}
 	
@@ -215,14 +215,14 @@ export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 		const shortName = 'short_name';
 		
 		const neededAddressTypes = {
-			country: shortName,
+			country:  shortName,
 			locality: longName,
 			// 'neighborhood' is not need for now
 			// neighborhood: longName,
-			route: longName,
-			intersection: longName,
-			street_number: longName,
-			postal_code: longName,
+			route:                       longName,
+			intersection:                longName,
+			street_number:               longName,
+			postal_code:                 longName,
 			administrative_area_level_1: shortName,
 			administrative_area_level_2: shortName,
 			administrative_area_level_3: shortName,
@@ -236,44 +236,46 @@ export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 		let postcode = '';
 		let city = '';
 		
-		locationResult.address_components.forEach((address) =>
-		                                          {
-			                                          const addressType = address.types[0];
-			                                          const addressTypeKey = neededAddressTypes[addressType];
-			
-			                                          const val = address[addressTypeKey];
-			
-			                                          switch(addressType)
-			                                          {
-				                                          case 'country':
-					                                          country = val;
-					                                          break;
-				                                          case 'locality':
-				                                          case 'administrative_area_level_1':
-				                                          case 'administrative_area_level_2':
-				                                          case 'administrative_area_level_3':
-				                                          case 'administrative_area_level_4':
-				                                          case 'administrative_area_level_5':
-					                                          if(city === '')
-					                                          {
-						                                          city = val;
-					                                          }
-					                                          break;
-				                                          case 'route':
-				                                          case 'intersection':
-					                                          if(streetName === '')
-					                                          {
-						                                          streetName = val;
-					                                          }
-					                                          break;
-				                                          case 'street_number':
-					                                          streetNumber = val;
-					                                          break;
-				                                          case 'postal_code':
-					                                          postcode = val;
-					                                          break;
-			                                          }
-		                                          });
+		locationResult.address_components.forEach(
+				(address) =>
+				{
+					const addressType = address.types[0];
+					const addressTypeKey = neededAddressTypes[addressType];
+					
+					const val = address[addressTypeKey];
+					
+					switch(addressType)
+					{
+						case 'country':
+							country = val;
+							break;
+						case 'locality':
+						case 'administrative_area_level_1':
+						case 'administrative_area_level_2':
+						case 'administrative_area_level_3':
+						case 'administrative_area_level_4':
+						case 'administrative_area_level_5':
+							if(city === '')
+							{
+								city = val;
+							}
+							break;
+						case 'route':
+						case 'intersection':
+							if(streetName === '')
+							{
+								streetName = val;
+							}
+							break;
+						case 'street_number':
+							streetNumber = val;
+							break;
+						case 'postal_code':
+							postcode = val;
+							break;
+					}
+				}
+		);
 		
 		this._setFormLocationValues(
 				country,
@@ -287,21 +289,80 @@ export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 	private _translate(key: string): string
 	{
 		let translationResult = '';
-		
-		this.translate.get(key).subscribe((res) =>
-		                                  {
-			                                  translationResult = res;
-		                                  });
-		
+		this.translate.get(key).subscribe((res) => translationResult = res);
 		return translationResult;
 	}
 	
+	private static _setupGoogleAutocompleteOptions(
+			autocomplete: google.maps.places.Autocomplete
+	)
+	{
+		autocomplete.setComponentRestrictions({ country: ['us', 'bg', 'il'] });
+		autocomplete['setFields'](['address_components', 'geometry']);
+	}
+	
+	private _listenForGoogleAutocompleteAddressChanges(
+			autocomplete: google.maps.places.Autocomplete
+	)
+	{
+		autocomplete.addListener('place_changed', (_) =>
+		{
+			const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+			this._applyNewPlaceOnTheMap(place);
+		});
+	}
+	
+	private _tryFindNewAddress()
+	{
+		const house = this.house.value;
+		const city = this.city.value;
+		const streetAddress = this.street.value;
+		const countryName = getDefaultCountryName(+this.country.value);
+		
+		if(
+				isEmpty(streetAddress) ||
+				isEmpty(house) ||
+				isEmpty(city) ||
+				isEmpty(countryName)
+		)
+		{
+			return;
+		}
+		
+		const newAddress = `${house}${streetAddress}${city}${countryName}`;
+		
+		if(newAddress !== this._lastUsedAddress)
+		{
+			this._lastUsedAddress = newAddress;
+			
+			const geocoder = new google.maps.Geocoder();
+			
+			geocoder.geocode(
+					{
+						address:               `${streetAddress} ${house}, ${city}`,
+						componentRestrictions: { country: countryName },
+					},
+					(results, status) =>
+					{
+						if(status === google.maps.GeocoderStatus.OK)
+						{
+							const formattedAddress = results[0].formatted_address;
+							const place: google.maps.GeocoderResult = results[0];
+							
+							this._applyNewPlaceOnTheMap(place);
+							this._applyFormattedAddress(formattedAddress);
+						}
+					}
+			);
+		}
+	}
+	
 	private _setFormLocationValues(
-			country,
-			city,
-			streetName,
-			streetNumber,
-			postcode
+			country: string,
+			city: string,
+			streetName: string,
+			streetNumber: string,
+			postcode: string
 	)
 	{
 		if(!isEmpty(country))
@@ -335,72 +396,8 @@ export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 					inputElement
 			);
 			
-			this._setupGoogleAutocompleteOptions(autocomplete);
+			LocationFormComponent._setupGoogleAutocompleteOptions(autocomplete);
 			this._listenForGoogleAutocompleteAddressChanges(autocomplete);
-		}
-	}
-	
-	private _setupGoogleAutocompleteOptions(
-			autocomplete: google.maps.places.Autocomplete
-	)
-	{
-		autocomplete.setComponentRestrictions({ country: ['us', 'bg', 'il'] });
-		autocomplete['setFields'](['address_components', 'geometry']);
-	}
-	
-	private _listenForGoogleAutocompleteAddressChanges(
-			autocomplete: google.maps.places.Autocomplete
-	)
-	{
-		autocomplete.addListener('place_changed', (_) =>
-		{
-			const place: google.maps.places.PlaceResult = autocomplete.getPlace();
-			this._applyNewPlaceOnTheMap(place);
-		});
-	}
-	
-	private _tryFindNewAddress()
-	{
-		const house = this.house.value;
-		const city = this.city.value;
-		const streetAddress = this.street.value;
-		const countryName = getCountryName(+this.country.value);
-		
-		if(
-				isEmpty(streetAddress) ||
-				isEmpty(house) ||
-				isEmpty(city) ||
-				isEmpty(countryName)
-		)
-		{
-			return;
-		}
-		
-		const newAddress = `${house}${streetAddress}${city}${countryName}`;
-		
-		if(newAddress !== this._lastUsedAddress)
-		{
-			this._lastUsedAddress = newAddress;
-			
-			const geocoder = new google.maps.Geocoder();
-			
-			geocoder.geocode(
-					{
-						address: `${streetAddress} ${house}, ${city}`,
-						componentRestrictions: { country: countryName },
-					},
-					(results, status) =>
-					{
-						if(status === google.maps.GeocoderStatus.OK)
-						{
-							const formattedAddress = results[0].formatted_address;
-							const place: google.maps.GeocoderResult = results[0];
-							
-							this._applyNewPlaceOnTheMap(place);
-							this._applyFormattedAddress(formattedAddress);
-						}
-					}
-			);
 		}
 	}
 	
@@ -429,6 +426,4 @@ export class LocationFormComponent implements OnDestroy, OnInit, OnChanges
 			this._tryFindNewCoordinates();
 		}
 	}
-	
-	ngOnDestroy(): void {}
 }
