02d76850ce28c3a1cef73d19df1222af42c4c457
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-28 05:33:14 +0400

    Reorder imports Add chat popup

--- packages/shop-web-angular/src/app/app.component.ts
+++ packages/shop-web-angular/src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation, } from '@angular/core';
-import { RouterOutlet }                                     from '@angular/router';
-import { TranslateService }                                 from '@ngx-translate/core';
-import { StorageService }                                   from 'app/services/storage';
-import { AppState }                                         from './app.service';
-import { environment }                                      from '../environments/environment'
+import {
+	Component,
+	ViewChild,
+	ViewEncapsulation,
+	ElementRef,
+	OnInit
+}                           from '@angular/core';
+import { RouterOutlet }     from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { v4 as uuid }       from 'uuid'
+import IUser                from '@modules/server.common/interfaces/IUser';
+import Admin                from '@modules/server.common/entities/Admin';
+import {
+	ChatPopup,
+	ChatService,
+	ChatSession
+}                           from '@modules/client.common.angular2/services/chat.service';
+import { AppState }         from './app.service';
+import { AdminsService }    from './services/admins.service';
+import { environment }      from 'environments/environment'
+import { CustomersService } from 'app/services/customer.service';
+import { StorageService }   from 'app/services/storage';
 
 export interface ToolbarController
 {
@@ -18,49 +34,29 @@ export const ROOT_SELECTOR = 'app';
  */
 @Component({
 	           selector:      'app',
+	           styleUrls:     ['./app.component.scss'],
+	           templateUrl:   './app.component.html',
 	           encapsulation: ViewEncapsulation.None,
-	           styles:        [
-		           `
-                       html,
-                       body,
-                       app,
-                       mat-sidenav-container {
-                           margin: 0;
-                           width: 100%;
-                           height: 100%;
-                           background-color: #eeeeee;
-                       }
-
-                       .app-content {
-                           width: 100%;
-                           height: 100%;
-                           background-color: #eeeeee;
-                       }
-
-                       .app-content.toolbar-enabled {
-                           height: 100%;
-                       }
-		           `,
-	           ],
-	           template:      `
-		                          <toolbar *ngIf="!isToolbarDisabled" class="navbar navbar-expand-lg"></toolbar>
-		                          <div
-				                          class="app-content"
-				                          [ngClass]="{ 'toolbar-enabled': !isToolbarDisabled }"
-		                          >
-			                          <router-outlet class="container-fluid"></router-outlet>
-		                          </div>
-	                          `,
            })
 export class AppComponent implements OnInit
 {
 	@ViewChild(RouterOutlet)
 	public routerOutlet: RouterOutlet;
 	
+	@ViewChild('chatpopupContainer')
+	public chatpopupContainer: ElementRef;
+	
+	private session: ChatSession;
+	
+	private chatPopup: ChatPopup;
+	
 	constructor(
 			public appState: AppState,
 			private translateService: TranslateService,
-			private storageService: StorageService
+			private storageService: StorageService,
+			private customersService: CustomersService,
+			private adminsService: AdminsService,
+			private chatService: ChatService
 	)
 	{
 		// Here we initialize translates for the all app, when loads for the first time. Do not remove it
@@ -84,6 +80,58 @@ export class AppComponent implements OnInit
 					: defaultLanguage
 			);
 		}
+		
+	}
+	
+	public ngOnInit()
+	{
+		this.initializeChat()
+	}
+	
+	private async initializeChat()
+	{
+		let customer: IUser;
+		let admin: Admin;
+		const isLogged = this.storageService.isLogged();
+		
+		admin = await this.adminsService.getAdmins().toPromise();
+		if(isLogged)
+		{
+			customer = await this.customersService
+			                     .getCustomerById(this.storageService.userId)
+			                     .toPromise();
+		}
+		else
+		{
+			customer = {
+				_id:      uuid(),
+				username: "Customer",
+				role:     "guest",
+				email:    "customer@emarket.com"
+			};
+		}
+		if(customer)
+		{
+			if(!customer.firstName)
+				customer.firstName = customer.username;
+			this.session = await this.chatService
+			                         .createCurrentSession(customer);
+			
+			if(this.chatpopupContainer)
+			{
+				await this.chatService.getOrCreateConversation(this.session, {
+					_id:      admin._id,
+					username: "Emarket Admin",
+					email:    admin.email,
+					avatar:   admin.avatar,
+					role:     "admin"
+				});
+				this.chatPopup = await this.chatService.createPopup(
+						this.session,
+						this.chatpopupContainer
+				);
+			}
+		}
 	}
 	
 	public get isToolbarDisabled()
@@ -98,6 +146,13 @@ export class AppComponent implements OnInit
 		);
 	}
 	
-	ngOnInit()
-	{}
+	public get isChatDisabled()
+	{
+		const serverConnection = Number(this.storageService.serverConnection);
+		return (
+				this.routerOutlet == null ||
+				serverConnection === 0 ||
+				!this.routerOutlet.isActivated
+		);
+	}
 }
