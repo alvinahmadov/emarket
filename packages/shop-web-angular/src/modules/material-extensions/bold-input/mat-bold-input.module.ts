1119300f346a0ea1996e4268bfdccbb295858911
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-10-29 00:14:59 +0400

    Add MatInputModule

--- packages/shop-web-angular/src/modules/material-extensions/bold-input/mat-bold-input.module.ts
+++ packages/shop-web-angular/src/modules/material-extensions/bold-input/mat-bold-input.module.ts
@@ -2,11 +2,12 @@ import { CommonModule }          from '@angular/common';
 import { NgModule }              from '@angular/core';
 import { FormsModule }           from '@angular/forms';
 import { MatRippleModule }       from '@angular/material/core';
+import { MatInputModule }        from '@angular/material/input';
 import { MatBoldInputComponent } from './mat-bold-input.component';
 
 @NgModule({
-	          imports: [CommonModule, FormsModule, MatRippleModule],
+	          imports:      [CommonModule, FormsModule, MatRippleModule, MatInputModule],
 	          declarations: [MatBoldInputComponent],
-	          exports: [MatBoldInputComponent],
+	          exports:      [MatBoldInputComponent],
           })
 export class MatBoldInputModule {}
