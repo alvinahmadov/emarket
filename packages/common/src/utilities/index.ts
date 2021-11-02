271df6fb8ebf49a4f1c3fcb4d58f08f6e41dd5f7
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-11-02 05:48:53 +0400

    Remove common query and mutation constants

--- packages/common/src/utilities/index.ts
+++ packages/common/src/utilities/index.ts
@@ -1,13 +1,9 @@
 import CommonUtils   from './common';
 import GeoUtils      from './geolocation';
 import FakeDataUtils from './fake-data';
-import GQLQueries    from './graphql-defs/Queries';
-import GQLMutations  from './graphql-defs/Mutations';
 
 export {
 	CommonUtils,
 	GeoUtils,
-	FakeDataUtils,
-	GQLQueries,
-	GQLMutations
+	FakeDataUtils
 }
