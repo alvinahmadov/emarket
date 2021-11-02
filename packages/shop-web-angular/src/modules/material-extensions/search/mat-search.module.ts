5101329ad5eee51d391bf34aedb158a8eb616cbd
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-11-02 05:35:47 +0400

    Reformat Add MatInputModule

--- packages/shop-web-angular/src/modules/material-extensions/search/mat-search.module.ts
+++ packages/shop-web-angular/src/modules/material-extensions/search/mat-search.module.ts
@@ -2,17 +2,19 @@ import { CommonModule }       from '@angular/common';
 import { NgModule }           from '@angular/core';
 import { MatButtonModule }    from '@angular/material/button';
 import { MatRippleModule }    from '@angular/material/core';
-import { MatSearchComponent } from './mat-search.component';
+import { MatInputModule }     from '@angular/material/input';
 import { TranslateModule }    from '@ngx-translate/core';
+import { MatSearchComponent } from './mat-search.component';
 
 @NgModule({
-	          imports: [
+	          imports:      [
 		          CommonModule,
 		          MatButtonModule,
 		          MatRippleModule,
 		          TranslateModule.forChild(),
+		          MatInputModule,
 	          ],
 	          declarations: [MatSearchComponent],
-	          exports: [MatSearchComponent],
+	          exports:      [MatSearchComponent],
           })
 export class MatSearchModule {}
