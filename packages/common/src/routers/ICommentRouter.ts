3ef1c336a74b2d8d43e63ce566e7af4fd211d380
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-11-05 04:09:40 +0400

    Refactor

--- packages/common/src/routers/ICommentRouter.ts
+++ packages/common/src/routers/ICommentRouter.ts
@@ -1,8 +1,9 @@
-import { Observable }                     from 'rxjs';
-import { UpdateObject }                   from '../@pyro/db/db-update-object';
-import IComment, { ICommentCreateObject } from '../interfaces/IComment';
-import IPagingOptions                     from '../interfaces/IPagingOptions';
-import Comment                            from '../entities/Comment';
+import { Observable }           from 'rxjs';
+import { UpdateObject }         from '../@pyro/db/db-update-object';
+import { ICommentCreateObject } from '../interfaces/IComment';
+import IPagingOptions           from '../interfaces/IPagingOptions';
+import Comment                  from '../entities/Comment';
+import { CreateObject } from '@pyro/db/db-create-object';
 
 interface ICommentsRouter
 {
@@ -31,18 +32,33 @@ interface ICommentsRouter
 			updateObject: UpdateObject<Comment>
 	): Promise<Comment>;
 	
-	save(
+	increaseLikes(
 			storeId: string,
 			storeProductId: string,
-			commentId: string,
-			updatedComment: IComment
+			userId: string,
+			commentId: string
 	): Promise<Comment>;
+	
+	increaseDislikes(
+			storeId: string,
+			storeProductId: string,
+			userId: string,
+			commentId: string
+	): Promise<Comment>;
+	
+	replyTo(
+			storeId: string,
+			storeProductId: string,
+			replyCommentId: string,
+			comment: CreateObject<Comment>
+	): Promise<void>;
 	
 	delete(
 			storeId: string,
 			storeProductId: string,
 			commentIds: string[]
 	): Promise<Comment[]>;
+	
 }
 
 export default ICommentsRouter;
