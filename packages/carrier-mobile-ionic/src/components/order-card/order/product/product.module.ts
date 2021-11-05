9deb01d18d24ae5e74ca6293ae54762f9e34e7aa
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-11-05 04:25:14 +0400

    Reformat and rename Store to StorageService

--- packages/carrier-mobile-ionic/src/components/order-card/order/product/product.module.ts
+++ packages/carrier-mobile-ionic/src/components/order-card/order/product/product.module.ts
@@ -1,13 +1,13 @@
 import { NgModule }         from '@angular/core';
 import { CommonModule }     from '@angular/common';
 import { TranslateModule }  from '@ngx-translate/core';
-import { ProductComponent } from './product.component';
-import { Store }            from '../../../../services/store.service';
 import { IonicModule }      from '@ionic/angular';
+import { StorageService }   from 'services/storage.service';
+import { ProductComponent } from './product.component';
 
 @NgModule({
 	          imports:      [IonicModule, CommonModule, TranslateModule.forChild()],
-	          providers:    [Store],
+	          providers:    [StorageService],
 	          exports:      [ProductComponent],
 	          declarations: [ProductComponent],
           })
