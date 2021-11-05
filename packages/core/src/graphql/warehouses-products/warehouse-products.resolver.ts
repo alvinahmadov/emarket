ad747492886785e2978c9cf59540363fb5ea81b0
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-11-05 04:12:06 +0400

    Add interface arguments and missing properties from router

--- packages/core/src/graphql/warehouses-products/warehouse-products.resolver.ts
+++ packages/core/src/graphql/warehouses-products/warehouse-products.resolver.ts
@@ -6,34 +6,74 @@ import WarehouseProduct                  from '@modules/server.common/entities/W
 import ProductInfo                       from '@modules/server.common/entities/ProductInfo';
 import { WarehousesProductsService }     from '../../services/warehouses';
 
-export type TWarehouseProductInput = {
-	warehouseId: string;
-	warehouseProductId: string;
+export interface IStoreInput
+{
+	storeId: string
 }
 
-export type TWarehouseProductsInput = {
-	warehouseId: string;
-	products: IWarehouseProductCreateObject[];
+export interface IWarehouseProductInput extends IStoreInput
+{
+	storeProductId: string;
 }
 
-export type TWarehouseProductIdsInput = {
-	warehouseId: string;
-	productsIds: string[];
+export interface IWarehouseProductCountInput extends IWarehouseProductInput
+{
+	count: number
 }
 
-export type TQuantityUpdateInput = {
+export interface IWarehouseProductsInput extends IStoreInput
+{
+	storeProducts: IWarehouseProductCreateObject[];
+}
+
+export interface IWarehouseProductIdsInput extends IStoreInput
+{
+	storeProductsIds: string[];
+}
+
+export interface IWarehouseProductAvailableInput extends IWarehouseProductInput
+{
+	isAvailable: boolean;
+}
+
+export interface IWarehouseProductTakeawayInput extends IWarehouseProductInput
+{
+	isTakeaway: boolean;
+}
+
+export interface IWarehouseProductDeliveryInput extends IWarehouseProductInput
+{
+	isDelivery: boolean;
+}
+
+export interface IWarehouseProductsTopInput extends IStoreInput
+{
+	quantity: number;
+}
+
+export interface IWarehouseProductPriceInput extends IWarehouseProductInput
+{
+	price: number
+}
+
+export interface IWarehouseProductRatingInput extends IWarehouseProductCountInput
+{
+	customerId: string,
+}
+
+export interface IWarehouseProductUpdateInput extends IWarehouseProductInput
+{
+	updateInput: {
+		quantity: IQuantityUpdateInput;
+		price: number;
+	};
+}
+
+export interface IQuantityUpdateInput
+{
 	increase: number;
 	decrease: number;
 	to: number;
-}
-
-export type TWarehouseProductUpdateInput = {
-	warehouseId: string;
-	productId: string;
-	updateInput: {
-		quantity: TQuantityUpdateInput;
-		price: number;
-	};
 }
 
 @Resolver('Warehouse-products')
@@ -45,54 +85,32 @@ export class WarehouseProductsResolver
 	{}
 	
 	@Query()
-	async getWarehouseProductsWithPagination(_, { storeId, pagingOptions = {} }): Promise<WarehouseProduct[]>
-	{
-		const warehouseProducts = await this._warehousesProductsService.getProductsWithPagination(
-				storeId,
-				pagingOptions
-		);
-		
-		return warehouseProducts.map((p) => new WarehouseProduct(p));
-	}
-	
-	@Query()
-	async getWarehouseProductsCount(_, { id }: { id: string }): Promise<number>
-	{
-		return this._warehousesProductsService.getProductsCount(id);
-	}
-	
-	@Query()
-	async getWarehouseProduct(
+	public async getWarehouseProduct(
 			_,
 			{
-				warehouseId,
-				warehouseProductId
-			}: TWarehouseProductInput
+				storeId,
+				storeProductId
+			}: IWarehouseProductInput
 	): Promise<WarehouseProduct>
 	{
 		return await this._warehousesProductsService
-		                 .getProduct(warehouseId, warehouseProductId)
+		                 .getProduct(storeId, storeProductId)
 		                 .pipe(first())
 		                 .toPromise();
 	}
 	
 	@Query()
-	getWarehouseProductInfo(
-			_,
-			{
-				warehouseId
-			}: { warehouseId: string }
-	): Observable<ProductInfo>
+	public getWarehouseProductInfo(_, { storeId }: IStoreInput): Observable<ProductInfo>
 	{
 		return this._warehousesProductsService
-		           .getAvailable(warehouseId)
+		           .getAvailable(storeId)
 		           .pipe(
 				           map(warehouseProducts =>
 				               {
 					               return _.map(warehouseProducts, (warehouseProduct: WarehouseProduct) =>
 					               {
 						               return new ProductInfo({
-							                                      warehouseId:   warehouseId,
+							                                      warehouseId:   storeId,
 							                                      warehouseLogo: '',
 							                                      warehouseProduct,
 							                                      distance:      100000
@@ -103,38 +121,81 @@ export class WarehouseProductsResolver
 		           )
 	}
 	
-	@Mutation()
-	async addWarehouseProducts(
+	@Query()
+	public async getWarehouseProductsWithPagination(
 			_,
 			{
-				warehouseId,
-				products
-			}: TWarehouseProductsInput
+				storeId,
+				pagingOptions = {}
+			}
 	): Promise<WarehouseProduct[]>
 	{
-		return this._warehousesProductsService.add(warehouseId, products);
+		const warehouseProducts = await this._warehousesProductsService.getProductsWithPagination(
+				storeId,
+				pagingOptions
+		);
+		
+		return warehouseProducts.map((p) => new WarehouseProduct(p));
+	}
+	
+	@Query()
+	public async getWarehouseProductsCount(_, { storeId }: IStoreInput): Promise<number>
+	{
+		return this._warehousesProductsService.getProductsCount(storeId);
+	}
+	
+	@Query()
+	public async getWarehouseProductsAvailable(_, { storeId }: IStoreInput): Promise<WarehouseProduct[]>
+	{
+		return this._warehousesProductsService.getAvailable(storeId).toPromise();
+	}
+	
+	@Query()
+	public async getWarehouseProductsTop(
+			_,
+			{
+				storeId,
+				quantity
+			}: IWarehouseProductsTopInput
+	): Promise<WarehouseProduct[]>
+	{
+		return this._warehousesProductsService
+		           .getTopProducts(storeId, quantity)
+		           .toPromise();
 	}
 	
 	@Mutation()
-	async removeWarehouseProducts(
+	public async addWarehouseProducts(
 			_,
 			{
-				warehouseId,
-				productsIds
-			}: TWarehouseProductIdsInput
+				storeId,
+				storeProducts
+			}: IWarehouseProductsInput
 	): Promise<WarehouseProduct[]>
 	{
-		return this._warehousesProductsService.remove(warehouseId, productsIds);
+		return this._warehousesProductsService.add(storeId, storeProducts);
 	}
 	
 	@Mutation()
-	async updateWarehouseProduct(
+	public async removeWarehouseProducts(
 			_,
 			{
-				warehouseId,
-				productId,
+				storeId,
+				storeProductsIds
+			}: IWarehouseProductIdsInput
+	): Promise<WarehouseProduct[]>
+	{
+		return this._warehousesProductsService.remove(storeId, storeProductsIds);
+	}
+	
+	@Mutation()
+	public async updateWarehouseProduct(
+			_,
+			{
+				storeId,
+				storeProductId,
 				updateInput
-			}: TWarehouseProductUpdateInput
+			}: IWarehouseProductUpdateInput
 	): Promise<void>
 	{
 		if(updateInput.quantity)
@@ -146,13 +207,168 @@ export class WarehouseProductsResolver
 			
 			await this._warehousesProductsService
 			          .increaseCount(
-					          warehouseId,
-					          productId,
+					          storeId,
+					          storeProductId,
 					          updateInput.quantity.increase
 			          );
 		}
 		
 		await this._warehousesProductsService
-		          .changePrice(warehouseId, productId, updateInput.price);
+		          .changePrice(storeId, storeProductId, updateInput.price);
+	}
+	
+	@Mutation()
+	public async changeWarehouseProductAvailability(
+			_,
+			{
+				storeId,
+				storeProductId,
+				isAvailable
+			}: IWarehouseProductAvailableInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .changeProductAvailability(storeId, storeProductId, isAvailable);
+	}
+	
+	@Mutation()
+	public async changeWarehouseProductTakeaway(
+			_,
+			{
+				storeId,
+				storeProductId,
+				isTakeaway
+			}: IWarehouseProductTakeawayInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .changeProductTakeaway(storeId, storeProductId, isTakeaway);
+	}
+	
+	@Mutation()
+	public async changeWarehouseProductDelivery(
+			_,
+			{
+				storeId,
+				storeProductId,
+				isDelivery
+			}: IWarehouseProductDeliveryInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .changeProductDelivery(storeId, storeProductId, isDelivery);
+	}
+	
+	@Mutation()
+	public changeWarehouseProductRating(
+			_,
+			{
+				storeId,
+				storeProductId,
+				customerId,
+				count
+			}: IWarehouseProductRatingInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .changeRate(storeId, storeProductId, customerId, count);
+	}
+	
+	@Mutation()
+	public changeWarehouseProductPrice(
+			_,
+			{
+				storeId,
+				storeProductId,
+				price
+			}: IWarehouseProductPriceInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .changePrice(storeId, storeProductId, price);
+	}
+	
+	@Mutation()
+	public async increaseWarehouseProductCount(
+			_,
+			{
+				storeId,
+				storeProductId,
+				count
+			}: IWarehouseProductCountInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .increaseCount(storeId, storeProductId, count);
+	}
+	
+	@Mutation()
+	public async increaseWarehouseProductSoldCount(
+			_,
+			{
+				storeId,
+				storeProductId,
+				count
+			}: IWarehouseProductCountInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .increaseSoldCount(storeId, storeProductId, count);
+	}
+	
+	@Mutation()
+	public async increaseWarehouseProductViewsCount(
+			_,
+			{
+				storeId,
+				storeProductId,
+				count
+			}: IWarehouseProductCountInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .increaseViewsCount(storeId, storeProductId, count);
+	}
+	
+	@Mutation()
+	public async decreaseWarehouseProductCount(
+			_,
+			{
+				storeId,
+				storeProductId,
+				count
+			}: IWarehouseProductCountInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .decreaseCount(storeId, storeProductId, count);
+	}
+	
+	@Mutation()
+	public async decreaseWarehouseProductSoldCount(
+			_,
+			{
+				storeId,
+				storeProductId,
+				count
+			}: IWarehouseProductCountInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .decreaseSoldCount(storeId, storeProductId, count);
+	}
+	
+	@Mutation()
+	public async decreaseWarehouseProductViewsCount(
+			_,
+			{
+				storeId,
+				storeProductId,
+				count
+			}: IWarehouseProductCountInput
+	): Promise<WarehouseProduct>
+	{
+		return this._warehousesProductsService
+		           .decreaseViewsCount(storeId, storeProductId, count);
 	}
 }
