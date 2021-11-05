2f48b143aa8da897093e3498aad37f98669c36ef
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-11-05 21:09:33 +0400

    Add comment notes Remove TODO

--- packages/common/src/interfaces/ILanguage.ts
+++ packages/common/src/interfaces/ILanguage.ts
@@ -1,14 +1,25 @@
-// TODO: add other locales
+/**
+ * @note Don't forget to add language locales here
+ * after adding new language
+ * */
 type ILanguage = 'en-US' | 'ru-RU';
 
 export default ILanguage;
 
+/**
+ * @note Don't forget to add language locales here
+ * after adding new language
+ * */
 export enum LanguageCodesEnum
 {
 	ENGLISH = 'en-US',
 	RUSSIAN = 'ru-RU',
 }
 
+/**
+ * @note Don't forget to add language locales here
+ * after adding new language
+ * */
 export enum LanguagesEnum
 {
 	ENGLISH = 'English',
