50e015efaff0d5ccc270a641581448afd8fdd5b3
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-11-08 06:32:27 +0400

    Simplify code by using Customer entities fullName getter

--- packages/merchant-tablet-ionic/src/components/carrier-deliveries-table/customer.ts
+++ packages/merchant-tablet-ionic/src/components/carrier-deliveries-table/customer.ts
@@ -1,42 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ViewCell }                 from 'ng2-smart-table';
-import Order                        from '@modules/server.common/entities/Order';
-import User                         from '@modules/server.common/entities/User';
-import UserOrder                    from '@modules/server.common/entities/UserOrder';
+import { Component, OnInit } from '@angular/core';
+import { ViewCell }          from 'ng2-smart-table';
+import Order                 from '@modules/server.common/entities/Order';
 
 @Component({
 	           template: `
-		<span class="nameAddress">
-			<strong *ngIf="getUserName(order)">{{ getUserName(order) }}</strong>
-			<div class="address">{{ order.user.fullAddress }}</div>
-		</span>
-	`,
+		                     <span class="nameAddress">
+			                     <strong *ngIf="getFullName(order)">
+				                     {{ getFullName(order) }}
+			                     </strong>
+			                     <div class="address">
+				           {{ order.customer.fullAddress }}
+			           </div>
+		                     </span>
+	                     `,
            })
 export class CustomerComponent implements ViewCell, OnInit
 {
-	value: string | number;
-	rowData: any;
-	order: Order;
+	public value: string | number;
+	public rowData: any;
+	public order: Order;
 	
 	constructor() {}
 	
-	ngOnInit(): void
+	public ngOnInit(): void
 	{
 		this.order = this.rowData.order;
 	}
 	
-	getUserName(order: Order)
+	public getFullName(order: Order)
 	{
-		const user: UserOrder = order.user;
-		
-		if(user.firstName)
-		{
-			if(user.lastName)
-			{
-				return user.firstName + ' ' + user.lastName;
-			}
-			
-			return user.firstName;
-		}
+		return order.customer.fullName
 	}
 }
