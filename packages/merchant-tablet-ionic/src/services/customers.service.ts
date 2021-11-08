752f34601692ba567c4827a5cb48254603fdb327
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-11-08 06:28:51 +0400

    Fix customer search

--- packages/merchant-tablet-ionic/src/services/customers.service.ts
+++ packages/merchant-tablet-ionic/src/services/customers.service.ts
@@ -5,7 +5,7 @@ import { Observable }                    from 'rxjs';
 import ICustomer, { ICustomerFindInput } from '@modules/server.common/interfaces/ICustomer';
 import Customer                          from '@modules/server.common/entities/Customer';
 import ApolloService                     from '@modules/client.common.angular2/services/apollo.service';
-import { GQLQuery }                      from '../graphql/definitions';
+import { GQLQuery }                      from 'graphql/definitions';
 
 @Injectable()
 export class CustomersService extends ApolloService
@@ -47,16 +47,15 @@ export class CustomersService extends ApolloService
 		           );
 	}
 	
-	public findCustomer(findInput: ICustomerFindInput): Observable<Customer>
+	public findCustomers(findInput: ICustomerFindInput): Observable<Customer[] | Customer>
 	{
 		return this.apollo
 		           .query<{
-			           customer: ICustomer
+			           customers: Customer[]
 		           }>({
 			              query:     GQLQuery.Customer.Find,
 			              variables: { findInput }
 		              })
-		           .pipe(map((result) => <Customer>
-				           this.factory(result, Customer)));
+		           .pipe(map((result) => this.factory(result, Customer)));
 	}
 }
