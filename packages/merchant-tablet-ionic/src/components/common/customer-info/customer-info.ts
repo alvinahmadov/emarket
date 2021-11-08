bbc0b40f126d9c520675667717e58a4d080135ed
Alvin Ahmadov <alvin.dev.ahmadov@yandex.com>
2021-11-08 06:37:29 +0400

    Add getters to check customer validity Add casting getter for another component's input field

--- packages/merchant-tablet-ionic/src/components/common/customer-info/customer-info.ts
+++ packages/merchant-tablet-ionic/src/components/common/customer-info/customer-info.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import CustomerOrder        from '@modules/server.common/entities/CustomerOrder';
+import Customer             from '@modules/server.common/entities/Customer';
 
 @Component({
 	           selector:    'customer-info',
@@ -11,5 +12,18 @@ export class CustomerInfoComponent
 	@Input()
 	public customer: CustomerOrder;
 	
-	constructor() {}
+	public get fullName(): string
+	{
+		return this.customer ? this.customer.fullName : "No customer name";
+	}
+	
+	public get fullAddress(): string
+	{
+		return this.customer ? this.customer.fullAddress : "No address";
+	}
+	
+	public get asCustomer(): Customer
+	{
+		return <Customer> this.customer;
+	}
 }
