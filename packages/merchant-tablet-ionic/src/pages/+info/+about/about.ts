3c388d1d462002493775601b3e3d5f56dd1196cc
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-12 17:44:11 +0400

    Reorder and reformat

--- packages/merchant-tablet-ionic/src/pages/+info/+about/about.ts
+++ packages/merchant-tablet-ionic/src/pages/+info/+about/about.ts
@@ -2,39 +2,42 @@ import { Component, OnDestroy } from '@angular/core';
 import { Subscription }         from 'rxjs';
 import { CustomerRouter }       from '@modules/client.common.angular2/routers/customer-router.service';
 import { environment }          from 'environment';
+import { AppSettingsService }   from 'services/app-settings.service';
+import { StorageService }       from 'services/storage.service';
 
 @Component({
-	           selector: 'page-about',
+	           selector:    'page-about',
 	           templateUrl: 'about.html',
            })
 export class AboutPage implements OnDestroy
 {
 	public useAboutHtml: string = '<h1>Loading...</h1>';
 	public selectedLanguage: string;
-	private sub: Subscription;
 	public deviceId: string;
 	public userId: string;
 	public appVersion: string;
+	private sub: Subscription;
 	
-	constructor(private userRouter: CustomerRouter)
+	constructor(
+			private userRouter: CustomerRouter,
+			private appSettingsService: AppSettingsService,
+			private storageService: StorageService
+	)
 	{
-		this.selectedLanguage = localStorage.getItem('_language') || 'en-US';
-		this.deviceId = localStorage.getItem('_deviceId');
-		this.userId = localStorage.getItem('_userId');
+		this.selectedLanguage = this.storageService.locale || 'en-US';
+		this.deviceId = this.storageService.deviceId;
+		this.userId = this.storageService.merchantId;
 		this.appVersion = environment.APP_VERSION;
 	}
 	
-	ngOnInit()
+	public ngOnInit()
 	{
 		this.sub = this.userRouter
 		               .getAboutUs(this.userId, this.deviceId, this.selectedLanguage)
-		               .subscribe((html) =>
-		                          {
-			                          this.useAboutHtml = html;
-		                          });
+		               .subscribe((html) => this.useAboutHtml = html);
 	}
 	
-	ngOnDestroy()
+	public ngOnDestroy()
 	{
 		this.sub.unsubscribe();
 	}
