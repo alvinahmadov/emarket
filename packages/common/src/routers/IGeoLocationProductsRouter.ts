fac3ed3caea1ac4570cc4c2123ed242862f0b7b7
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-12 20:22:32 +0400

    Add input types

--- packages/common/src/routers/IGeoLocationProductsRouter.ts
+++ packages/common/src/routers/IGeoLocationProductsRouter.ts
@@ -2,6 +2,32 @@ import { Observable } from 'rxjs';
 import IGeoLocation   from '../interfaces/IGeoLocation';
 import ProductInfo    from '../entities/ProductInfo';
 
+interface IGetGeoLocationProductsOptions
+{
+	isDeliveryRequired?: boolean;
+	isTakeaway?: boolean;
+	trackingDistance?: number;
+	merchantIds?: string[];
+	imageOrientation?: number;
+	locale?: string;
+	withoutCount?: boolean;
+}
+
+export interface IGeoLocationProductsInput
+{
+	geoLocation;
+	options?: IGetGeoLocationProductsOptions;
+	pagingOptions;
+	searchText?: string;
+}
+
+export interface ICountOfGeoLocationProducts
+{
+	geoLocation: IGeoLocation;
+	options?: IGetGeoLocationProductsOptions;
+	searchText?: string;
+}
+
 interface IGeoLocationProductsRouter
 {
 	get(geoLocation: IGeoLocation): Observable<ProductInfo[]>;
