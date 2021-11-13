b8b4761d2c6c27708ccff4b5683f4bbd1ab9e9e8
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-13 04:52:51 +0400

    Reformat Add explicit types Add explicit access specifiers

--- packages/shop-web-angular/src/app/shared/message-pop-up/message-pop-up.component.ts
+++ packages/shop-web-angular/src/app/shared/message-pop-up/message-pop-up.component.ts
@@ -1,9 +1,16 @@
-import { Component, Input, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef }    from '@angular/material/dialog';
+import { Component, Inject, OnInit }     from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+type TMessagePopUpData = {
+	confirmButton: string;
+	cancelButton: string;
+	modalTitle: string;
+	commonText: string;
+}
 
 @Component({
-	           selector: 'message-pop-up',
-	           styleUrls: ['./message-pop-up.component.scss'],
+	           selector:    'message-pop-up',
+	           styleUrls:   ['./message-pop-up.component.scss'],
 	           templateUrl: './message-pop-up.component.html',
            })
 export class MessagePopUpComponent implements OnInit
@@ -16,16 +23,16 @@ export class MessagePopUpComponent implements OnInit
 	
 	constructor(
 			public dialogRef: MatDialogRef<MessagePopUpComponent>,
-			@Inject(MAT_DIALOG_DATA) public data: any
+			@Inject(MAT_DIALOG_DATA) public data: TMessagePopUpData
 	)
 	{}
 	
-	ngOnInit()
+	public ngOnInit()
 	{
 		this.initializeText();
 	}
 	
-	initializeText()
+	public initializeText()
 	{
 		this.modalTitle = this.PREFIX + this.data.modalTitle;
 		this.commonText = this.PREFIX + this.data.commonText;
@@ -33,7 +40,7 @@ export class MessagePopUpComponent implements OnInit
 		this.confirmButton = this.PREFIX + this.data.confirmButton;
 	}
 	
-	onNoClick(): void
+	public onNoClick(): void
 	{
 		this.dialogRef.close();
 	}
