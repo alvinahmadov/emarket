c4d5df936b9595ec258298474a08e9800de38e08
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-14 19:43:19 +0400

    Add StorageService Add specifiers

--- packages/carrier-mobile-ionic/src/pages/product/return-product/return-product.ts
+++ packages/carrier-mobile-ionic/src/pages/product/return-product/return-product.ts
@@ -1,15 +1,16 @@
 import { Component, OnDestroy }  from '@angular/core';
-import { ILocaleMember }         from '@modules/server.common/interfaces/ILocale';
-import { ProductLocalesService } from '@modules/client.common.angular2/locale/product-locales.service';
+import { Subject }               from 'rxjs';
+import { first, takeUntil }      from 'rxjs/operators';
 import ICarrier                  from '@modules/server.common/interfaces/ICarrier';
+import { ILocaleMember }         from '@modules/server.common/interfaces/ILocale';
 import IOrder                    from '@modules/server.common/interfaces/IOrder';
+import IOrderProduct             from "@modules/server.common/interfaces/IOrderProduct";
+import OrderCarrierStatus        from '@modules/server.common/enums/OrderCarrierStatus';
+import { ProductLocalesService } from '@modules/client.common.angular2/locale/product-locales.service';
 import { CarrierRouter }         from '@modules/client.common.angular2/routers/carrier-router.service';
 import { OrderRouter }           from '@modules/client.common.angular2/routers/order-router.service';
-import { first, takeUntil }      from 'rxjs/operators';
-import { Store }                 from 'services/store.service';
-import OrderCarrierStatus        from '@modules/server.common/enums/OrderCarrierStatus';
-import { Subject }               from 'rxjs';
 import { NavController }         from '@ionic/angular';
+import { StorageService }        from 'services/storage.service';
 
 @Component({
 	           selector:    'page-return-product',
@@ -17,8 +18,8 @@ import { NavController }         from '@ionic/angular';
            })
 export class ReturnProductPage implements OnDestroy
 {
-	carrier: ICarrier;
-	selectedOrder: IOrder;
+	public carrier: ICarrier;
+	public selectedOrder: IOrder;
 	
 	private destroy$ = new Subject<void>();
 	
@@ -26,14 +27,14 @@ export class ReturnProductPage implements OnDestroy
 			private translateProductLocales: ProductLocalesService,
 			private carrierRouter: CarrierRouter,
 			private orderRouter: OrderRouter,
-			private store: Store,
+			private storageService: StorageService,
 			private navCtrl: NavController
 	)
 	{}
 	
-	get allowCancel()
+	public get allowCancel()
 	{
-		switch(this.store.returnProductFrom)
+		switch(this.storageService.returnProductFrom)
 		{
 			case 'driveToWarehouse':
 				return false;
@@ -44,29 +45,34 @@ export class ReturnProductPage implements OnDestroy
 		}
 	}
 	
-	ionViewWillEnter()
+	public ionViewWillEnter()
 	{
 		this.loadData();
 	}
 	
-	localeTranslate(member: ILocaleMember[])
+	public localeTranslate(member: ILocaleMember[]): string
 	{
 		return this.translateProductLocales.getTranslate(member);
 	}
 	
-	cancelReturn()
+	public get products(): IOrderProduct[]
+	{
+		return this.selectedOrder?.products ?? [];
+	}
+	
+	public cancelReturn()
 	{
 		if(!this.allowCancel) throw new Error('Cancel not allowed!');
 		
 		this.navCtrl.navigateRoot('/main/starting-delivery');
 	}
 	
-	async returnProduct()
+	public async returnProduct()
 	{
 		if(!this.selectedOrder.isCancelled)
 		{
 			await this.orderRouter.updateCarrierStatus(
-					this.store.orderId,
+					this.storageService.orderId,
 					OrderCarrierStatus.IssuesDuringDelivery
 			);
 			
@@ -81,12 +87,12 @@ export class ReturnProductPage implements OnDestroy
 		}
 	}
 	
-	ionViewWillLeave()
+	public ionViewWillLeave()
 	{
-		localStorage.removeItem('returnProductFrom');
+		this.storageService.returnProductFrom = null;
 	}
 	
-	ngOnDestroy(): void
+	public ngOnDestroy(): void
 	{
 		this.destroy$.next();
 		this.destroy$.complete();
@@ -95,25 +101,25 @@ export class ReturnProductPage implements OnDestroy
 	private async loadData()
 	{
 		this.carrier = await this.carrierRouter
-		                         .get(this.store.carrierId)
+		                         .get(this.storageService.carrierId)
 		                         .pipe(first())
 		                         .toPromise();
 		
 		this.orderRouter
-		    .get(this.store.orderId, {
+		    .get(this.storageService.orderId, {
 			    populateWarehouse: true,
 		    })
 		    .pipe(takeUntil(this.destroy$))
 		    .subscribe((o) =>
 		               {
 			               this.selectedOrder = o;
-			               this.store.selectedOrder = o;
+			               this.storageService.selectedOrder = o;
 		               });
 	}
 	
 	private unselectOrder()
 	{
-		localStorage.removeItem('orderId');
-		this.store.selectedOrder = null;
+		this.storageService.orderId = null;
+		this.storageService.selectedOrder = null;
 	}
 }
