16ae7d9a4bf4521bb8388030aa7a4b1c99b0c89b
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-14 19:16:43 +0400

    Rename Store to StorageService

--- packages/carrier-mobile-ionic/src/pages/login/login.module.ts
+++ packages/carrier-mobile-ionic/src/pages/login/login.module.ts
@@ -1,12 +1,12 @@
 import { NgModule }             from '@angular/core';
-import { LoginPage }            from './login';
-import { TranslateModule }      from '@ngx-translate/core';
-import { AuthService }          from '../../services/auth.service';
-import { Store }                from '../../services/store.service';
-import { Routes, RouterModule } from '@angular/router';
-import { IonicModule }          from '@ionic/angular';
-import { FormsModule }          from '@angular/forms';
 import { CommonModule }         from '@angular/common';
+import { FormsModule }          from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { TranslateModule }      from '@ngx-translate/core';
+import { IonicModule }          from '@ionic/angular';
+import { LoginPage }            from './login';
+import { AuthService }          from '../../services/auth.service';
+import { StorageService }       from '../../services/storage.service';
 
 const routes: Routes = [
 	{
@@ -24,6 +24,9 @@ const routes: Routes = [
 		          TranslateModule.forChild(),
 		          RouterModule.forChild(routes),
 	          ],
-	          providers:    [AuthService, Store],
+	          providers:    [
+	          		AuthService,
+		            StorageService
+	          ],
           })
 export class LoginPageModule {}
