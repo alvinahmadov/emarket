82369297e63537cdfe174b72ea449216044213df
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-14 20:40:43 +0400

    Add additional interfaces

--- packages/common/src/routers/IGeoLocationOrdersRouter.ts
+++ packages/common/src/routers/IGeoLocationOrdersRouter.ts
@@ -1,11 +1,42 @@
-import { Observable } from 'rxjs';
-import Order          from '../entities/Order';
-import IGeoLocation   from '../interfaces/IGeoLocation';
+import { Observable }                      from 'rxjs';
+import Order                               from '../entities/Order';
+import IGeoLocation                        from '../interfaces/IGeoLocation';
+import { IGeoLocationOrdersPagingOptions } from "@ever-platform/core/build/src/services/geo-locations";
 
 export interface IGeoLocationOrdersRouterOptions
 {
 	populateWarehouse?: boolean;
 	populateCarrier?: boolean;
+}
+
+interface IGeoLocationOrdersSearchRecord
+{
+	key: string;
+	value: string;
+}
+
+export interface IGeoLocationOrdersSearchObject
+{
+	isCancelled?: boolean;
+	byRegex: Array<IGeoLocationOrdersSearchRecord>;
+}
+
+export interface IGeoLocationWorkOrderSearchInput extends IGeoLocationOrdersSearchObject
+{
+	isCancelled?: boolean;
+}
+
+export interface IGeoLocationWorkOrderInput
+{
+	geoLocation: IGeoLocation;
+	skippedOrderIds: string[];
+	options: IGeoLocationOrdersPagingOptions;
+	searchObj?: IGeoLocationOrdersSearchObject;
+}
+
+export interface IGeoLocationWorkOrdersInput extends IGeoLocationWorkOrderInput
+{
+	searchObj: IGeoLocationWorkOrderSearchInput;
 }
 
 interface IGeoLocationOrdersRouter
