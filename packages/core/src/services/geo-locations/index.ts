99ac3053bdfe519b07adea298a5ea730dd472058
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-14 22:35:19 +0400

    Remove removed file

--- packages/core/src/services/geo-locations/index.ts
+++ packages/core/src/services/geo-locations/index.ts
@@ -1,5 +1,4 @@
 export * from './GeoLocationsOrdersService';
-export * from './GeoLocationOrdersOptions';
 export * from './GeoLocationsProductsService';
 export * from './GeoLocationsWarehousesService';
 export * from './GeoLocationsService';
