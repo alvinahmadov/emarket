eb23c6b0be9f59d623fcf6f9a6aa8e8975505ec0
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-14 19:12:43 +0400

    Rename Store to StorageService

--- packages/carrier-mobile-ionic/src/pages/login/login.module.guard.ts
+++ packages/carrier-mobile-ionic/src/pages/login/login.module.guard.ts
@@ -1,19 +1,21 @@
+// noinspection ES6MissingAwait
+
 import { Injectable }             from '@angular/core';
 import { CanLoad, Route, Router } from '@angular/router';
-import { Store }                  from '../../services/store.service';
+import { StorageService }         from 'services/storage.service';
 
 @Injectable()
 export class LoginModuleGuard implements CanLoad
 {
 	constructor(
-			private readonly store: Store,
+			private readonly storageService: StorageService,
 			private readonly router: Router
 	)
 	{}
 	
 	async canLoad(route: Route)
 	{
-		const isLogged = await this.store.isLogged();
+		const isLogged = await this.storageService.isLogged();
 		if(isLogged)
 		{
 			this.router.navigateByUrl('main');
