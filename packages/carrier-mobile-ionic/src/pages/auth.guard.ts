70affadd4f87b8cfa1eb135f30784de8f7bdc19a
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-14 19:40:27 +0400

    Rename Store to StorageService Add specifiers

--- packages/carrier-mobile-ionic/src/pages/auth.guard.ts
+++ packages/carrier-mobile-ionic/src/pages/auth.guard.ts
@@ -1,19 +1,19 @@
 import { Injectable }             from '@angular/core';
 import { CanLoad, Route, Router } from '@angular/router';
-import { Store }                  from 'services/store.service';
+import { StorageService }         from 'services/storage.service';
 
 @Injectable()
 export class AuthGuard implements CanLoad
 {
 	constructor(
-			private readonly store: Store,
+			private readonly storageService: StorageService,
 			private readonly router: Router
 	)
 	{}
 	
-	async canLoad(route: Route)
+	public async canLoad(route: Route)
 	{
-		const isLogged = await this.store.isLogged();
+		const isLogged = await this.storageService.isLogged();
 		if(!isLogged)
 		{
 			this.router.navigateByUrl('/login');
