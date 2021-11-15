b2b6ac9dab34485728f919175248ca3cfb1a7ca9
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-15 08:21:09 +0400

    Remove warehouse name Add storage service Add translation

--- packages/merchant-tablet-ionic/src/@shared/common/product-type-popup.ts
+++ packages/merchant-tablet-ionic/src/@shared/common/product-type-popup.ts
@@ -1,22 +1,47 @@
-import { ModalController } from "@ionic/angular";
-import { IProductImage }   from "@modules/server.common/interfaces/IProduct";
-import { ComponentRef }    from "@ionic/core";
-import Warehouse           from "@modules/server.common/entities/Warehouse";
-import { first }           from "rxjs/operators";
-import { WarehouseRouter } from "@modules/client.common.angular2/routers/warehouse-router.service";
+import { ComponentRef }                      from '@ionic/core';
+import { ModalController }                   from '@ionic/angular';
+import { first }                             from 'rxjs/operators';
+import { IProductImage }                     from '@modules/server.common/interfaces/IProduct';
+import Warehouse                             from '@modules/server.common/entities/Warehouse';
+import { WarehouseRouter }                   from '@modules/client.common.angular2/routers/warehouse-router.service';
+import { StorageService }                    from 'services/storage.service';
+import { environment }                       from "environment";
 
-abstract class ProductTypePopup
+class ProductTypePopup
 {
-	warehouseName: string;
+	public readonly OK: string = 'OK';
+	public readonly CANCEL: string = 'CANCEL';
+	public readonly SELECT_CATEGORIES: string = 'SELECT_CATEGORIES';
+	public readonly PREFIX: string = 'WAREHOUSE_VIEW.SELECT_POP_UP.';
+	
+	protected locale: string;
+	public selectedProductCategories: string[] = [];
+	public hasImage: boolean;
+	protected imagesData: IProductImage[];
+	
+	public productDelivery: boolean;
+	public productTakeaway: boolean;
+	
+	public readonly descriptionMaxLength = 255;
+	public readonly locales: string[] = environment.AVAILABLE_LOCALES.split('|');
+	protected static cssClass = "mutation-product-images-modal";
+	
+	public isAvailable: boolean;
 	
 	protected constructor(
-			protected modalController: ModalController
+			protected modalController: ModalController,
+			protected storageService: StorageService
 	)
 	{}
 	
-	get warehouseId()
+	public get warehouseId()
 	{
-		return localStorage.getItem('_warehouseId');
+		return this.storageService.warehouseId;
+	}
+	
+	public get isBrowser()
+	{
+		return this.storageService.platform === 'browser';
 	}
 	
 	protected async createModal<T extends ComponentRef>(
@@ -26,12 +51,12 @@ abstract class ProductTypePopup
 	)
 	{
 		return await this.modalController.create({
-			                                         component: component,
-			                                         componentProps: {
+			                                         component:       component,
+			                                         componentProps:  {
 				                                         images,
 			                                         },
 			                                         backdropDismiss: false,
-			                                         cssClass: cssClass,
+			                                         cssClass:        cssClass,
 		                                         });
 	}
 	
@@ -44,10 +69,6 @@ abstract class ProductTypePopup
 			warehouse = warehouseRouter.get(this.warehouseId, false)
 			                           .pipe(first())
 			                           .toPromise();
-			if(warehouse)
-			{
-				this.warehouseName = (await warehouse).name;
-			}
 		}
 		
 		return warehouse;
