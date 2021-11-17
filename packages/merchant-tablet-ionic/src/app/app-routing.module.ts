7a8f012d09b4202433e7b4e31a2c1052fcde1942
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-17 06:09:40 +0400

    Reformat Add config

--- packages/merchant-tablet-ionic/src/app/app-routing.module.ts
+++ packages/merchant-tablet-ionic/src/app/app-routing.module.ts
@@ -2,44 +2,44 @@ import { NgModule }                           from '@angular/core';
 import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { PagesModuleGuard }                   from '../pages/pages.module.guard';
 import { MaintenanceModuleGuard }             from './+maintenance-info/maintenance-info.module.guard';
+import { environment }                        from 'environments/environment';
 
 const routes: Routes = [
 	{
-		path: '',
+		path:         '',
 		loadChildren: () =>
-				import('../pages/pages.module').then((m) => m.PagesModule),
-		canActivate: [PagesModuleGuard],
+				              import('../pages/pages.module').then((m) => m.PagesModule),
+		canActivate:  [PagesModuleGuard],
 	},
 	{
-		path: 'maintenance-info',
+		path:         'maintenance-info',
 		loadChildren: () =>
-				import('./+maintenance-info/maintenance-info.module').then(
-						(m) => m.MaintenanceInfoPageModule
-				),
-		canActivate: [MaintenanceModuleGuard],
+				              import('./+maintenance-info/maintenance-info.module').then(
+						              (m) => m.MaintenanceInfoPageModule
+				              ),
+		canActivate:  [MaintenanceModuleGuard],
 	},
 	{
-		path: 'server-down',
+		path:         'server-down',
 		loadChildren: () =>
-				import('./+server-down/server-down.module').then(
-						(m) => m.ServerDownPageModule
-				),
+				              import('./+server-down/server-down.module').then(
+						              (m) => m.ServerDownPageModule
+				              ),
 	},
 	{
-		path: '**',
-		pathMatch: 'full',
+		path:       '**',
+		pathMatch:  'full',
 		redirectTo: '',
 	},
 ];
 
 const config: ExtraOptions = {
-	useHash: true,
-	enableTracing: true,
+	useHash:       true,
+	enableTracing: !!environment.production,
 };
 
 @NgModule({
-	          // imports: [RouterModule.forRoot(routes, config)],
-	          imports: [RouterModule.forRoot(routes)],
+	          imports: [RouterModule.forRoot(routes, config)],
 	          exports: [RouterModule],
           })
 export class AppRoutingModule {}
