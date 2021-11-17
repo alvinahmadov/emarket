076afb30c72eecd38e07484a511f64fc11c89ea7
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-17 20:11:12 +0400

    Add productId and productTitle for routing to comments page Add discount info Add open comments button Reorder

--- packages/shop-web-angular/src/app/+products/product-details/statistics/statistics.component.ts
+++ packages/shop-web-angular/src/app/+products/product-details/statistics/statistics.component.ts
@@ -1,20 +1,21 @@
 import {
 	Component, Input,
 	OnInit, OnDestroy
-}                                  from '@angular/core';
-import { Subject }                 from 'rxjs';
-import { map }                     from 'rxjs/operators';
-import { ILocaleMember }           from '@modules/server.common/interfaces/ILocale';
-import { IProductsCategory }       from '@modules/server.common/interfaces/IProductsCategory';
-import IWarehouse                  from '@modules/server.common/interfaces/IWarehouse';
-import Currency                    from '@modules/server.common/entities/Currency';
-import Product                     from '@modules/server.common/entities/Product';
-import WarehouseProduct            from '@modules/server.common/entities/WarehouseProduct';
-import { ProductLocalesService }   from '@modules/client.common.angular2/locale/product-locales.service';
-import { WarehouseProductsRouter } from '@modules/client.common.angular2/routers/warehouse-products-router.service';
-import { CurrenciesService }       from 'app/services/currencies.service';
-import { StorageService }          from 'app/services/storage';
-import { ProductCategoryService }  from 'app/services/product-category.service';
+}                                 from '@angular/core';
+import { Router }                 from '@angular/router';
+import { Subject }                from 'rxjs';
+import { map }                    from 'rxjs/operators';
+import { ILocaleMember }          from '@modules/server.common/interfaces/ILocale';
+import { IProductsCategory }      from '@modules/server.common/interfaces/IProductsCategory';
+import IWarehouse                 from '@modules/server.common/interfaces/IWarehouse';
+import Currency                   from '@modules/server.common/entities/Currency';
+import Product                    from '@modules/server.common/entities/Product';
+import WarehouseProduct           from '@modules/server.common/entities/WarehouseProduct';
+import { ProductLocalesService }  from '@modules/client.common.angular2/locale/product-locales.service';
+import { CurrenciesService }      from 'app/services/currencies.service';
+import { ProductCommentService }  from 'app/services/product-comment.service';
+import { StorageService }         from 'app/services/storage';
+import { ProductCategoryService } from 'app/services/product-category.service';
 
 @Component({
 	           selector:    'es-statistics',
@@ -29,9 +30,16 @@ export class StatisticsComponent implements OnInit, OnDestroy
 	@Input()
 	public warehouseId: string;
 	
+	@Input()
+	public productId: string;
+	
+	@Input()
+	public productTitle: string;
+	
 	public customerId: string;
 	
 	public categories: IProductsCategory[] = null;
+	public commentsCount: number = 0;
 	
 	public warehouse: IWarehouse;
 	public currency: Currency;
@@ -39,10 +47,11 @@ export class StatisticsComponent implements OnInit, OnDestroy
 	private _ngDestroy$: Subject<void> = new Subject<void>();
 	
 	constructor(
+			private readonly router: Router,
 			private readonly _productLocalesService: ProductLocalesService,
 			private readonly _currenciesService: CurrenciesService,
 			private readonly _productCategoryService: ProductCategoryService,
-			private readonly _warehouseProductRouter: WarehouseProductsRouter,
+			private readonly _productCommentsService: ProductCommentService,
 			private readonly storage: StorageService
 	)
 	{
@@ -76,6 +85,11 @@ export class StatisticsComponent implements OnInit, OnDestroy
 						         })
 				
 			}
+			this._productCommentsService.getCountOfComments(
+					    this.warehouseId,
+					    this.warehouseProduct.id
+			    )
+			    .toPromise().then(c => this.commentsCount = c);
 		}
 	}
 	
@@ -85,14 +99,28 @@ export class StatisticsComponent implements OnInit, OnDestroy
 		this._ngDestroy$.complete();
 	}
 	
-	public localeTranslate(member: ILocaleMember[]): string
-	{
-		return this._productLocalesService.getTranslate(member);
-	}
-	
 	public get hasDiscount(): boolean
 	{
 		return this.getDiscount() > 0;
+	}
+	
+	public async openComments()
+	{
+		await this.router.navigate(
+				['products/comments', this.warehouseId, this.warehouseProduct.id],
+				{
+					state:              {
+						productId:    this.productId,
+						productTitle: this.productTitle
+					},
+					skipLocationChange: true
+				}
+		);
+	}
+	
+	public localeTranslate(member: ILocaleMember[]): string
+	{
+		return this._productLocalesService.getTranslate(member);
 	}
 	
 	public getDiscount()
