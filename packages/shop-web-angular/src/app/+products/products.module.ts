7e817b322818d03d9ab55b13627ce0c3bfed9f67
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-17 19:11:25 +0400

    Add services, modules and new comments component

--- packages/shop-web-angular/src/app/+products/products.module.ts
+++ packages/shop-web-angular/src/app/+products/products.module.ts
@@ -1,29 +1,39 @@
-import { NgModule }                         from '@angular/core';
-import { CommonModule }                     from '@angular/common';
-import { HttpClient }                       from '@angular/common/http';
-import { RouterModule }                     from '@angular/router';
-import { FormsModule }                      from '@angular/forms';
-import { MatButtonModule }                  from "@angular/material/button";
-import { MatCardModule }                    from '@angular/material/card';
-import { MatFormFieldModule }               from '@angular/material/form-field';
-import { MatIconModule }                    from '@angular/material/icon';
-import { MatInputModule }                   from '@angular/material/input';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader }              from '@ngx-translate/http-loader';
-import { LazyLoadImageModule }              from 'ng-lazyload-image';
-import { DragScrollModule }                 from 'ngx-drag-scroll';
-import { CurrenciesService }                from 'app/services/currencies.service';
-import { GeoLocationService }               from 'app/services/geo-location';
-import { GeoLocationProductsService }       from 'app/services/geo-location-products';
-import { ProductsService }                  from 'app/services/products';
-import { WarehouseProductsService }         from 'app/services/warehouse-products';
-import { ProductsCommonModule }             from './common/common.module';
-import { ProductDetailsComponent }          from './product-details';
-import { ProductsComponent }                from './products.component';
-import { routes }                           from './products.routes';
-import { CarouselViewModule }               from './views/carousel/carousel-view.module';
-import { ListViewModule }                   from './views/list/list-view.module';
-import { WarehouseLogoModule }              from '../warehouse-logo';
+import { NgModule }                                      from '@angular/core';
+import { CommonModule }                                  from '@angular/common';
+import { HttpClient }                                    from '@angular/common/http';
+import { RouterModule }                                  from '@angular/router';
+import { FormsModule, ReactiveFormsModule }              from '@angular/forms';
+import { MatButtonModule }                               from '@angular/material/button';
+import { MatRippleModule }                               from '@angular/material/core';
+import { MatCardModule }                                 from '@angular/material/card';
+import { MatFormFieldModule }                            from '@angular/material/form-field';
+import { MatIconModule }                                 from '@angular/material/icon';
+import { MatInputModule }                                from '@angular/material/input';
+import { MatGridListModule }                             from '@angular/material/grid-list';
+import { MatProgressSpinnerModule }                      from '@angular/material/progress-spinner';
+import { MatListModule }                                 from '@angular/material/list';
+import { TranslateModule, TranslateLoader }              from '@ngx-translate/core';
+import { TranslateHttpLoader }                           from '@ngx-translate/http-loader';
+import { NgbModule, NgbRatingModule }                    from '@ng-bootstrap/ng-bootstrap';
+import { LazyLoadImageModule }                           from 'ng-lazyload-image';
+import { DragScrollModule }                              from 'ngx-drag-scroll';
+import { CurrenciesService }                             from 'app/services/currencies.service';
+import { GeoLocationService }                            from 'app/services/geo-location';
+import { GeoLocationProductsService }                    from 'app/services/geo-location-products';
+import { ProductsService }                               from 'app/services/products.service';
+import { WarehouseProductsService }                      from 'app/services/warehouse-products.service';
+import { ProductCategoryService }                        from 'app/services/product-category.service';
+import { PipesModule }                                   from 'pipes/pipes.module';
+import { ProductsCommonModule }                          from './common/common.module';
+import { ProductDetailsComponent }                       from './product-details';
+import { CommentListComponent, ProductCommentComponent } from './product-comments';
+import { ProductsComponent }                             from './products.component';
+import { routes }                                        from './products.routes';
+import { StatisticsComponent }                           from './product-details/statistics/statistics.component';
+import { ProductImageComponent }                         from './product-details/product-image/product-image.component';
+import { CarouselViewModule }                            from './views/carousel/carousel-view.module';
+import { ListViewModule }                                from './views/list/list-view.module';
+import { WarehouseLogoModule }                           from '../warehouse-logo';
 
 export function HttpLoaderFactory(http: HttpClient)
 {
@@ -31,9 +41,17 @@ export function HttpLoaderFactory(http: HttpClient)
 }
 
 @NgModule({
-	          declarations: [ProductsComponent, ProductDetailsComponent],
+	          declarations: [
+		          ProductsComponent,
+		          ProductDetailsComponent,
+		          StatisticsComponent,
+		          ProductImageComponent,
+		          ProductCommentComponent,
+		          CommentListComponent
+	          ],
 	          imports:      [
 		          CommonModule,
+		          ProductsCommonModule,
 		          TranslateModule.forChild({
 			                                   loader: {
 				                                   provide:    TranslateLoader,
@@ -45,7 +63,6 @@ export function HttpLoaderFactory(http: HttpClient)
 		          LazyLoadImageModule,
 		          WarehouseLogoModule,
 		          MatCardModule,
-		          ProductsCommonModule,
 		          MatIconModule,
 		          DragScrollModule,
 		          CarouselViewModule,
@@ -54,12 +71,21 @@ export function HttpLoaderFactory(http: HttpClient)
 		          MatFormFieldModule,
 		          MatInputModule,
 		          MatButtonModule,
+		          MatGridListModule,
+		          NgbRatingModule,
+		          NgbModule,
+		          MatListModule,
+		          PipesModule,
+		          MatProgressSpinnerModule,
+		          ReactiveFormsModule,
+		          MatRippleModule,
 	          ],
 	          providers:    [
 		          CurrenciesService,
 		          GeoLocationService,
 		          GeoLocationProductsService,
 		          ProductsService,
+		          ProductCategoryService,
 		          WarehouseProductsService
 	          ],
           })
