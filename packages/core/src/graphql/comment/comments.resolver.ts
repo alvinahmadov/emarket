2bbefe4aeb96c96c12594d1ccfe56d7410986040
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-11-17 14:31:17 +0400

    Add input interfaces Make getComments observable Remove logger

--- packages/core/src/graphql/comment/comments.resolver.ts
+++ packages/core/src/graphql/comment/comments.resolver.ts
@@ -1,108 +1,114 @@
-// import { UseGuards }                 from '@nestjs/common';
-// import { AuthGuard }                 from '@nestjs/passport';
+import { UseGuards }                 from '@nestjs/common';
+import { AuthGuard }                 from '@nestjs/passport';
 import { Mutation, Query, Resolver } from '@nestjs/graphql';
-import Logger                        from 'bunyan';
-import { map }                       from 'rxjs/operators';
+import { Observable }                from 'rxjs';
+import {
+	ICommentInput,
+	ICommentsRetrieveInput,
+	ICommentRetrieveInput,
+	ICommentCreateInput,
+	ICommentUpdateInput,
+	ICommentDeleteInput,
+	ICommentRateInput,
+}                                    from '@modules/server.common/routers/ICommentRouter';
 import Comment                       from '@modules/server.common/entities/Comment';
 import { CommentsService }           from '../../services/comments';
-import { createLogger }              from '../../helpers/Log';
 
 @Resolver('Comment')
 export class CommentsResolver
 {
-	protected readonly logger: Logger = createLogger({
-		                                                 name: 'commentsResolver',
-		                                                 ns:   'graphql'
-	                                                 });
-	
 	constructor(private readonly _commentsService: CommentsService) {}
 	
 	@Query('comment')
-	public async getComment(
-			_,
-			{ storeId, storeProductId, commentId }
-	): Promise<Comment>
+	public async getComment(_, retrieveInput: ICommentRetrieveInput): Promise<Comment>
 	{
-		this.logger.info('.getComment(findParams: ICommentFindParams)');
-		return this._commentsService
-		           .get(
-				           storeId,
-				           storeProductId,
-				           commentId
-		           )
-		           .pipe(map(comment =>
-		                     {
-			                     this.logger.debug('Got comment', { storeId, storeProductId, comment });
-			                     return comment;
-		                     }))
-		           .toPromise();
+		return this._commentsService.get(
+				retrieveInput.storeId,
+				retrieveInput.storeProductId,
+				retrieveInput.commentId
+		).toPromise();
 	}
 	
 	@Query('comments')
-	public async getComments(
-			_,
-			{ storeId, storeProductId, pagingOptions = {} }
-	): Promise<Comment[]>
+	public getComments(_, retrieveInput: ICommentsRetrieveInput): Observable<Comment[]>
 	{
-		if(!pagingOptions || (pagingOptions && !pagingOptions['sort']))
+		let pagingOptions = retrieveInput.pagingOptions ?? {};
+		
+		if(!pagingOptions || (pagingOptions && !pagingOptions.sort))
 		{
-			pagingOptions['sort'] = { field: '_createdAt', sortBy: 'desc' };
+			pagingOptions.sort = { field: '_createdAt', sortBy: 'desc' };
 		}
 		
-		return this._commentsService
-		           .getComments(
-				           storeId,
-				           storeProductId,
-				           pagingOptions
-		           );
+		return this._commentsService.getComments(
+				retrieveInput.storeId,
+				retrieveInput.storeProductId,
+				pagingOptions
+		);
 	}
 	
 	@Query()
-	public async getCountOfComments(
-			_,
-			{ storeId, storeProductId }
-	): Promise<number>
+	public async getCountOfComments(_, countInput: ICommentInput): Promise<number>
 	{
-		const comments = await this._commentsService.getComments(storeId, storeProductId);
+		const comments = await this._commentsService.getComments(
+				countInput.storeId,
+				countInput.storeProductId
+		).toPromise();
+		
 		return comments.length;
 	}
 	
 	@Mutation()
-	// @UseGuards(AuthGuard('jwt'))
-	public async deleteCommentsByIds(
-			_,
-			{
-				storeId,
-				storeProductId,
-				commentIds
-			}
-	): Promise<Comment[]>
+	public async addComment(_, createInput: ICommentCreateInput): Promise<Comment[]>
 	{
-		return await this._commentsService
-		                 .delete(storeId, storeProductId, commentIds)
+		return this._commentsService.add(
+				createInput.storeId,
+				createInput.storeProductId,
+				createInput.comment
+		);
 	}
 	
 	@Mutation()
-	public async addComment(
-			_, { storeId, storeProductId, comment }
-	): Promise<Comment[]>
+	@UseGuards(AuthGuard('jwt'))
+	public async updateComment(_, updateInput: ICommentUpdateInput): Promise<Comment>
 	{
-		return await this._commentsService.add(storeId, storeProductId, comment);
+		return this._commentsService.update(
+				updateInput.storeId,
+				updateInput.storeProductId,
+				updateInput.commentId,
+				updateInput.comment
+		);
+	}
+	
+	@Mutation('increaseCommentLikes')
+	public async increaseLikes(_, rateInput: ICommentRateInput): Promise<Comment>
+	{
+		return this._commentsService.increaseLikes(
+				rateInput.storeId,
+				rateInput.storeProductId,
+				rateInput.userId,
+				rateInput.commentId
+		);
+	}
+	
+	@Mutation('increaseCommentDislikes')
+	public async increaseDislikes(_, rateInput: ICommentRateInput): Promise<Comment>
+	{
+		return this._commentsService.increaseDislikes(
+				rateInput.storeId,
+				rateInput.storeProductId,
+				rateInput.userId,
+				rateInput.commentId
+		);
 	}
 	
 	@Mutation()
-	// @UseGuards(AuthGuard('jwt'))
-	public async updateComment(
-			_,
-			{ storeId, storeProductId, commentId, comment }
-	): Promise<Comment>
+	@UseGuards(AuthGuard('jwt'))
+	public async deleteCommentsByIds(_, deleteInput: ICommentDeleteInput): Promise<Comment[]>
 	{
-		return this._commentsService
-		           .save(
-				           storeId,
-				           storeProductId,
-				           commentId,
-				           comment
-		           );
+		return await this._commentsService.delete(
+				deleteInput.storeId,
+				deleteInput.storeProductId,
+				deleteInput.commentIds
+		);
 	}
 }
